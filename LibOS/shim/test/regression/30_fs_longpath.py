gramineproject/graphene

[LibOS] Fix bug of long relative paths (more than 256B)
Previously, Graphene had a bug when dentry's rel_path was not initialized
to actual relative path if parent + filename length was longer than
256B. The problem was that rel_path was a shim_qstr which could contain
maximum 256 chars. This commit increases the limit to 4096B and adds
checks to fail explicitly if rel_path is still too large. Also, it adds
LibOS regression test.

Note that relative paths greater than 256 chars in length are not
uncommon. Most OSes impose the limit of 4096 chars or no limit at all.

LibOS/shim/test/regression/30_fs_longpath.py
@@ -1,0 +1,13 @@
+import os, sys, mmap
+from regression import Regression
+
+loader = sys.argv[1]
+
+# Running Long Filepath Example
+regression = Regression(loader, "fs_long_path")
+
+regression.add_check(name="FS Long Paths",
+        check=lambda res: "Succesfully read from file: Hello World" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)

gramineproject/gramine

LibOS/shim/test/regression/30_fs_longpath.py
@@ -1,0 +1,13 @@
+import os, sys, mmap
+from regression import Regression
+
+loader = sys.argv[1]
+
+# Running Long Filepath Example
+regression = Regression(loader, "fs_long_path")
+
+regression.add_check(name="FS Long Paths",
+        check=lambda res: "Successfully read from file: Hello World" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)