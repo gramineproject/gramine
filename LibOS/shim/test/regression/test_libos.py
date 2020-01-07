gramineproject/graphene

[LibOS] Comprehensive cleanup of poll()/ppoll()/select()/pselect()
Accurate cleanup of shim_do_poll(), shim_do_ppoll(), shim_do_select(),
and shim_do_pselect6(). This cleanup also adds error handling (missing
previously).

The commit adds four LibOS regression tests on poll, ppoll, select,
and pselect mechanisms. Also, the corresponding LTP tests pass now.

LibOS/shim/test/regression/test_libos.py
@@ -386,6 +386,26 @@ class TC_80_Socket(RegressionTestCase):
         # epoll_wait timeout
         self.assertIn('epoll_wait test passed', stdout)
 
+    def test_020_poll(self):
+        stdout, stderr = self.run_binary(['poll'])
+        self.assertIn('poll(POLLOUT) returned 1 file descriptors', stdout)
+        self.assertIn('poll(POLLIN) returned 1 file descriptors', stdout)
+
+    def test_030_ppoll(self):
+        stdout, stderr = self.run_binary(['ppoll'])
+        self.assertIn('ppoll(POLLOUT) returned 1 file descriptors', stdout)
+        self.assertIn('ppoll(POLLIN) returned 1 file descriptors', stdout)
+
+    def test_040_select(self):
+        stdout, stderr = self.run_binary(['select'])
+        self.assertIn('select() on write event returned 1 file descriptors', stdout)
+        self.assertIn('select() on read event returned 1 file descriptors', stdout)
+
+    def test_050_pselect(self):
+        stdout, stderr = self.run_binary(['pselect'])
+        self.assertIn('pselect() on write event returned 1 file descriptors', stdout)
+        self.assertIn('pselect() on read event returned 1 file descriptors', stdout)
+
     def test_100_socket_unix(self):
         stdout, stderr = self.run_binary(['unix'])
         self.assertIn('Data: This is packet 0', stdout)

gramineproject/gramine

LibOS/shim/test/regression/test_libos.py
@@ -411,6 +411,26 @@ class TC_80_Socket(RegressionTestCase):
         # epoll_wait timeout
         self.assertIn('epoll_wait test passed', stdout)
 
+    def test_020_poll(self):
+        stdout, _ = self.run_binary(['poll'])
+        self.assertIn('poll(POLLOUT) returned 1 file descriptors', stdout)
+        self.assertIn('poll(POLLIN) returned 1 file descriptors', stdout)
+
+    def test_030_ppoll(self):
+        stdout, _ = self.run_binary(['ppoll'])
+        self.assertIn('ppoll(POLLOUT) returned 1 file descriptors', stdout)
+        self.assertIn('ppoll(POLLIN) returned 1 file descriptors', stdout)
+
+    def test_040_select(self):
+        stdout, _ = self.run_binary(['select'])
+        self.assertIn('select() on write event returned 1 file descriptors', stdout)
+        self.assertIn('select() on read event returned 1 file descriptors', stdout)
+
+    def test_050_pselect(self):
+        stdout, _ = self.run_binary(['pselect'])
+        self.assertIn('pselect() on write event returned 1 file descriptors', stdout)
+        self.assertIn('pselect() on read event returned 1 file descriptors', stdout)
+
     def test_100_socket_unix(self):
         stdout, stderr = self.run_binary(['unix'])
         self.assertIn('Data: This is packet 0', stdout)