gramineproject/graphene

[{LibOS,Pal}/regression] Increase timeout for multi-process tests
Multi-process tests create new enclaves and require significant time for
initialization. Previously, tests ran for 8-20 seconds. This is barely
enough for slow machines.

LibOS/shim/test/regression/test_libos.py
@@ -86,14 +86,14 @@ class TC_01_Bootstrap(RegressionTestCase):
             stdout)
 
     def test_201_fork_and_exec(self):
-        stdout, stderr = self.run_binary(['fork_and_exec'])
+        stdout, stderr = self.run_binary(['fork_and_exec'], timeout=60)
 
         # fork and exec 2 page child binary
         self.assertIn('child exited with status: 0', stdout)
         self.assertIn('test completed successfully', stdout)
 
     def test_202_vfork_and_exec(self):
-        stdout, stderr = self.run_binary(['vfork_and_exec'])
+        stdout, stderr = self.run_binary(['vfork_and_exec'], timeout=60)
 
         # vfork and exec 2 page child binary
         self.assertIn('child exited with status: 0', stdout)

Pal/regression/test_pal.py
@@ -507,7 +507,7 @@ class TC_20_SingleProcess(RegressionTestCase):
 
 class TC_21_ProcessCreation(RegressionTestCase):
     def test_100_process(self):
-        stdout, stderr = self.run_binary(['Process'], timeout=8)
+        stdout, stderr = self.run_binary(['Process'], timeout=60)
         counter = collections.Counter(stderr.split('\n'))
         # Process Creation
         self.assertEqual(counter['Child Process Created'], 3)
@@ -523,7 +523,7 @@ class TC_21_ProcessCreation(RegressionTestCase):
         self.assertEqual(counter['Process Read 2: Hello World 2'], 3)
 
     def test_110_process_broadcast(self):
-        stdout, stderr = self.run_binary(['Process'], timeout=8)
+        stdout, stderr = self.run_binary(['Process'], timeout=60)
         counter = collections.Counter(stderr.split('\n'))
 
         # Multi-Process Broadcast Channel Transmission

gramineproject/gramine

LibOS/shim/test/regression/test_libos.py
@@ -86,14 +86,14 @@ class TC_01_Bootstrap(RegressionTestCase):
         self.assertIn('hello from execv process', stdout)
 
     def test_202_fork_and_exec(self):
-        stdout, _ = self.run_binary(['fork_and_exec'])
+        stdout, _ = self.run_binary(['fork_and_exec'], timeout=60)
 
         # fork and exec 2 page child binary
         self.assertIn('child exited with status: 0', stdout)
         self.assertIn('test completed successfully', stdout)
 
     def test_203_vfork_and_exec(self):
-        stdout, _ = self.run_binary(['vfork_and_exec'])
+        stdout, _ = self.run_binary(['vfork_and_exec'], timeout=60)
 
         # vfork and exec 2 page child binary
         self.assertIn('child exited with status: 0', stdout)

Pal/regression/test_pal.py
@@ -512,7 +512,7 @@ class TC_20_SingleProcess(RegressionTestCase):
 
 class TC_21_ProcessCreation(RegressionTestCase):
     def test_100_process(self):
-        _, stderr = self.run_binary(['Process'], timeout=8)
+        _, stderr = self.run_binary(['Process'], timeout=60)
         counter = collections.Counter(stderr.split('\n'))
         # Process Creation
         self.assertEqual(counter['Child Process Created'], 3)