gramineproject/graphene

[LibOS] Add getters and setters for shim context's syscall number

LibOS/shim/include/arch/x86_64/shim_tcb-arch.h
@@ -32,4 +32,12 @@ static inline void shim_regs_set_sp(struct shim_regs* sr, uint64_t sp) {
     sr->rsp = sp;
 }
 
+static inline uint64_t shim_regs_get_syscallnr(struct shim_regs* sr) {
+    return sr->orig_rax;
+}
+
+static inline void shim_regs_set_syscallnr(struct shim_regs* sr, uint64_t orig_reg) {
+    sr->orig_rax = orig_reg;
+}
+
 #endif /* _SHIM_TCB_ARCH_H_ */

LibOS/shim/include/shim_tcb.h
@@ -24,6 +24,14 @@ static inline void shim_context_set_sp(struct shim_context* sc, unsigned long sp
     shim_regs_set_sp(sc->regs, sp);
 }
 
+static inline unsigned long shim_context_get_syscallnr(struct shim_context* sc) {
+    return shim_regs_get_syscallnr(sc->regs);
+}
+
+static inline void shim_context_set_syscallnr(struct shim_context* sc, unsigned long orig_reg) {
+    shim_regs_set_syscallnr(sc->regs, orig_reg);
+}
+
 struct debug_buf;
 
 typedef struct shim_tcb shim_tcb_t;

gramineproject/gramine

LibOS/shim/include/arch/x86_64/shim_tcb-arch.h
@@ -32,4 +32,12 @@ static inline void shim_regs_set_sp(struct shim_regs* sr, uint64_t sp) {
     sr->rsp = sp;
 }
 
+static inline uint64_t shim_regs_get_syscallnr(struct shim_regs* sr) {
+    return sr->orig_rax;
+}
+
+static inline void shim_regs_set_syscallnr(struct shim_regs* sr, uint64_t sc_num) {
+    sr->orig_rax = sc_num;
+}
+
 #endif /* _SHIM_TCB_ARCH_H_ */

LibOS/shim/include/shim_tcb.h
@@ -24,6 +24,14 @@ static inline void shim_context_set_sp(struct shim_context* sc, unsigned long sp
     shim_regs_set_sp(sc->regs, sp);
 }
 
+static inline unsigned long shim_context_get_syscallnr(struct shim_context* sc) {
+    return shim_regs_get_syscallnr(sc->regs);
+}
+
+static inline void shim_context_set_syscallnr(struct shim_context* sc, unsigned long sc_num) {
+    shim_regs_set_syscallnr(sc->regs, sc_num);
+}
+
 struct debug_buf;
 
 typedef struct shim_tcb shim_tcb_t;