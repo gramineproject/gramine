OAKO-UCONN/graphene

[LibOS] Rework threads implementation
Most important differences from the old version:
- strip global process information from the thread struct into a
  dedicated one,
- a parent is informed about the child death when the whole process
  (the last thread) dies (not on each thread exit),
- all threads have the same parent (spawning thread is NOT the parent of
  the spawned thread),
- a thread is able to wait on children created by another thread,
- a process is able to wait for exited children after execve,
- rewritten `waitid` implementation (no more gotos, supports __WCLONE
  and friends flags),
- added option for syscall restarting, for now used only in `waitid`.

Additionally various bugfixes, cleanups and missing locks added.

LibOS/shim/src/sys/shim_wait.c
@@ -1,42 +1,123 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
-/* Copyright (C) 2014 Stony Brook University */
-
-/*
- * Implementation of system calls "waitid" and "wait4".
+/* Copyright (C) 2020 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
  */
 
-#include <errno.h>
-#include <linux/wait.h>
-#include <sys/mman.h>
-#include <sys/syscall.h>
+#include <stddef.h> // to get missing size_t definition
 
-#include "pal.h"
-#include "pal_error.h"
+#include <linux/signal.h>
+#include <linux/wait.h>
+#include <stdbool.h>
+
+#include "assert.h"
+#include "api.h"
 #include "shim_internal.h"
 #include "shim_lock.h"
+#include "shim_process.h"
 #include "shim_signal.h"
 #include "shim_table.h"
 #include "shim_thread.h"
-#include "shim_utils.h"
-
+#include "shim_types.h"
 
 /* For wait4() return value */
 #define WCOREFLAG 0x80
 
+static bool child_matches_flags(struct shim_child_process* child, int flags) {
+    if (flags & __WALL) {
+        return true;
+    }
+
+    return (!!(flags & __WCLONE)) ^ (child->death_notification_signal == SIGCHLD);
+}
+
+static bool child_matches(struct shim_child_process* child, int which, IDTYPE id, int flags) {
+    if (!child_matches_flags(child, flags)) {
+        return false;
+    }
+
+    bool ret = false;
+    switch (which) {
+        case P_PID:
+            if (child->pid == id) {
+                ret = true;
+            }
+            break;
+        case P_PGID:
+            /* TODO: this makes no sense, until we implement IPC pgid change. */
+            break;
+        case P_ALL:
+            ret = true;
+            break;
+        default:
+            /* Impossible. */
+            BUG();
+            break;
+    }
+
+    return ret;
+}
+
+static void remove_qnode_from_wait_queue(struct shim_thread_queue* qnode) {
+    lock(&g_process.children_lock);
+
+    bool seen = false;
+    if (g_process.wait_queue == qnode) {
+        g_process.wait_queue = qnode->next;
+        seen = true;
+    } else if (g_process.wait_queue) {
+        struct shim_thread_queue* tmp = g_process.wait_queue;
+        while (tmp->next) {
+            if (tmp->next == qnode) {
+                tmp->next = qnode->next;
+                seen = true;
+                break;
+            }
+            tmp = tmp->next;
+        }
+    }
+
+    unlock(&g_process.children_lock);
+
+    while (!seen) {
+        DkEventClear(get_cur_thread()->scheduler_event);
+        COMPILER_BARRIER();
+        /* Check if `qnode` is no longer used. */
+        if (!__atomic_load_n(&qnode->in_use, __ATOMIC_ACQUIRE)) {
+            break;
+        }
+        /* We cannot handle any errors here. */
+        (void)thread_sleep(NO_TIMEOUT);
+    }
+}
 
 long shim_do_waitid(int which, pid_t id, siginfo_t* infop, int options, struct __kernel_rusage* ru) {
-    struct shim_thread* cur    = get_cur_thread();
-    struct shim_thread* thread = NULL;
-    int ret;
     __UNUSED(ru);
 
-    /* Note that we don't support WSTOPPED or WCONTINUED correctly. */
     if (options & ~(WNOHANG | WNOWAIT | WEXITED | WSTOPPED | WCONTINUED |
-                    __WNOTHREAD| __WCLONE | __WALL))
+                    __WNOTHREAD | __WCLONE | __WALL))
         return -EINVAL;
+
+    if (options & __WALL) {
+        options &= ~__WCLONE;
+    }
 
     if (!(options & (WEXITED | WSTOPPED | WCONTINUED)))
         return -EINVAL;
+
+    if (options & WSTOPPED) {
+        debug("Ignoring unsuported WSTOPPED flag to wait4\n");
+        options &= ~WSTOPPED;
+    }
+    if (options & WCONTINUED) {
+        debug("Ignoring unsuported WCONTINUED flag to wait4\n");
+        options &= ~WCONTINUED;
+    }
+    assert(options & WEXITED);
+
+    if (options & __WNOTHREAD) {
+        debug("Ignoring unsuported __WNOTHREAD flag to wait4\n");
+        options &= ~__WNOTHREAD;
+    }
 
     if (!(which == P_PGID || which == P_ALL || which == P_PID))
         return -EINVAL;
@@ -44,138 +125,99 @@ long shim_do_waitid(int which, pid_t id, siginfo_t* infop, int options, struct _
     if (infop && test_user_memory(infop, sizeof(*infop), /*write=*/true))
         return -EFAULT;
 
-    if (which == P_PID) {
-        pid_t pid = id;
+    long ret = 0;
 
-        if (!(thread = lookup_thread(pid)))
-            return -ECHILD;
+    lock(&g_process.children_lock);
 
-        if (!(options & WNOHANG)) {
-        block_pid:
-            object_wait_with_retry(thread->exit_event);
+    while (1) {
+        struct shim_child_process* child;
+        /* First search already exited children. */
+        LISTP_FOR_EACH_ENTRY(child, &g_process.zombies, list) {
+            if (child_matches(child, which, id, options)) {
+                /* We have a match! */
+                if (infop) {
+                    infop->si_pid = child->pid;
+                    infop->si_uid = child->uid;
+                    infop->si_signo = SIGCHLD;
+
+                    fill_siginfo_code_and_status(infop, child->term_signal, child->exit_code);
+                }
+
+                if (!(options & WNOWAIT)) {
+                    LISTP_DEL_INIT(child, &g_process.zombies, list);
+                    destroy_child_process(child);
+                }
+                ret = 0;
+                goto out;
+            }
         }
 
-        lock(&thread->lock);
+        /* Do we have any non-exited child to wait for? */
+        bool have_waitable_child = false;
+        LISTP_FOR_EACH_ENTRY(child, &g_process.children, list) {
+            if (child_matches(child, which, id, options)) {
+                have_waitable_child = true;
+                break;
+            }
+        }
 
-        if (thread->is_alive) {
-            unlock(&thread->lock);
-            if (!(options & WNOHANG))
-                goto block_pid;
-            put_thread(thread);
+        if (!have_waitable_child) {
+            ret = -ECHILD;
+            goto out;
+        }
 
+        /* There are some children we can wait for. */
+        if (options & WNOHANG) {
             if (infop) {
                 infop->si_pid = 0;
                 infop->si_signo = 0;
             }
-            return 0;
+            ret = 0;
+            goto out;
         }
 
-        if (!(options & WNOWAIT) && !LIST_EMPTY(thread, siblings)) {
-            debug("reaping thread %p\n", thread);
-            struct shim_thread* parent = thread->parent;
-            assert(parent);
+        /* Ok, let's wait. */
+        struct shim_thread* self = get_cur_thread();
+        struct shim_thread_queue qnode = {
+            .thread = self,
+            .next = g_process.wait_queue,
+        };
+        g_process.wait_queue = &qnode;
+        __atomic_store_n(&qnode.in_use, true, __ATOMIC_RELEASE);
 
-            lock(&parent->lock);
-            /* DEP 5/15/17: These threads are exited */
-            assert(!thread->is_alive);
-            LISTP_DEL_INIT(thread, &parent->exited_children, siblings);
-            unlock(&parent->lock);
+        __atomic_store_n(&self->signal_handled, SIGNAL_NOT_HANDLED, __ATOMIC_RELEASE);
 
-            put_thread(parent);
-            thread->parent = NULL;
-            put_thread(thread);
-        }
+        unlock(&g_process.children_lock);
 
-        unlock(&thread->lock);
-        goto found;
-    }
-
-    lock(&cur->lock);
-
-    if (LISTP_EMPTY(&cur->children) && LISTP_EMPTY(&cur->exited_children)) {
-        unlock(&cur->lock);
-        return -ECHILD;
-    }
-
-    if (!(options & WNOHANG)) {
-    block:
-        if (cur->child_exit_event)
-            while (LISTP_EMPTY(&cur->exited_children)) {
-                unlock(&cur->lock);
-                object_wait_with_retry(cur->child_exit_event);
-                lock(&cur->lock);
+        while (1) {
+            if (__atomic_load_n(&self->signal_handled, __ATOMIC_ACQUIRE) == SIGNAL_HANDLED) {
+                remove_qnode_from_wait_queue(&qnode);
+                return -EINTR;
             }
-    }
 
-    if (which == P_PGID) {
-        IDTYPE pgid;
-        if (id == 0)
-            pgid = cur->pgid;
-        else
-            pgid = id;
-
-        LISTP_FOR_EACH_ENTRY(thread, &cur->exited_children, siblings) {
-            if (thread->pgid == pgid) {
-                get_thread(thread);
-                goto found_child;
+            DkEventClear(self->scheduler_event);
+            COMPILER_BARRIER();
+            /* Check that we are still supposed to sleep. */
+            if (!__atomic_load_n(&qnode.in_use, __ATOMIC_ACQUIRE)) {
+                break;
             }
+            ret = thread_sleep(NO_TIMEOUT);
+            if (ret < 0 && ret != -EINTR && ret != -EAGAIN && ret != -EWOULDBLOCK) {
+                debug("thread_sleep failed in waitid\n");
+
+                remove_qnode_from_wait_queue(&qnode);
+                /* `ret` is already set. */
+                return ret;
+            }
+
+            handle_signals();
         }
 
-        if (!(options & WNOHANG))
-            goto block;
-    } else {
-        assert(which == P_ALL);
-
-        if (!LISTP_EMPTY(&cur->exited_children)) {
-            thread = LISTP_FIRST_ENTRY(&cur->exited_children, struct shim_thread, siblings);
-            get_thread(thread);
-            goto found_child;
-        }
+        lock(&g_process.children_lock);
     }
 
-    unlock(&cur->lock);
-
-    if (infop) {
-        infop->si_pid = 0;
-        infop->si_signo = 0;
-    }
-    return 0;
-
-found_child:
-    if (!(options & WNOWAIT)) {
-        LISTP_DEL_INIT(thread, &cur->exited_children, siblings);
-        put_thread(cur);
-        thread->parent = NULL;
-
-        if (LISTP_EMPTY(&cur->exited_children))
-            DkEventClear(cur->child_exit_event);
-    }
-
-    unlock(&cur->lock);
-
-found:
-    ret = 0;
-    if (infop) {
-        infop->si_pid = thread->tid;
-        infop->si_uid = thread->uid;
-        infop->si_signo = SIGCHLD;
-
-        if (thread->term_signal == 0) {
-            infop->si_code = CLD_EXITED;
-            infop->si_status = thread->exit_code;
-        } else if (thread->term_signal & __WCOREDUMP_BIT) {
-            infop->si_code = CLD_DUMPED;
-            infop->si_status = thread->term_signal & ~__WCOREDUMP_BIT;
-        } else {
-            infop->si_code = CLD_KILLED;
-            infop->si_status = thread->term_signal;
-        }
-    }
-
-    if (!(options & WNOWAIT))
-        del_thread(thread);
-
-    put_thread(thread);
+out:
+    unlock(&g_process.children_lock);
     return ret;
 }
 
@@ -184,7 +226,6 @@ long shim_do_wait4(pid_t pid, int* status, int options, struct __kernel_rusage* 
     pid_t id;
     siginfo_t info;
 
-    /* Note that only WNOHANG is handled correctly. */
     if (options & ~(WNOHANG | WUNTRACED | WCONTINUED | __WNOTHREAD | __WCLONE | __WALL)) {
         return -EINVAL;
     }
@@ -194,8 +235,10 @@ long shim_do_wait4(pid_t pid, int* status, int options, struct __kernel_rusage* 
 
     /* Prepare options for shim_do_waitid(). */
     options |= WEXITED;
-    if (options & WUNTRACED)
+    if (options & WUNTRACED) {
         options &= ~WUNTRACED;
+        options |= WSTOPPED;
+    }
 
     if (pid < -1) {
         which = P_PGID;

gramineproject/gramine

LibOS/shim/src/sys/shim_wait.c
@@ -1,42 +1,126 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
-/* Copyright (C) 2014 Stony Brook University */
-
-/*
- * Implementation of system calls "waitid" and "wait4".
+/* Copyright (C) 2020 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
  */
 
-#include <errno.h>
-#include <linux/wait.h>
-#include <sys/mman.h>
-#include <sys/syscall.h>
+/* We need to include this to get size_t definition, which otherwise is missing in <linux/signal.h>
+ * (at least on Ubuntu16.04) */
+#include <stddef.h>
 
-#include "pal.h"
-#include "pal_error.h"
+#include <linux/signal.h>
+#include <linux/wait.h>
+#include <stdbool.h>
+
+#include "assert.h"
+#include "api.h"
 #include "shim_internal.h"
 #include "shim_lock.h"
+#include "shim_process.h"
 #include "shim_signal.h"
 #include "shim_table.h"
 #include "shim_thread.h"
-#include "shim_utils.h"
-
+#include "shim_types.h"
 
 /* For wait4() return value */
 #define WCOREFLAG 0x80
 
+static bool child_matches_flags(struct shim_child_process* child, int flags) {
+    if (flags & __WALL) {
+        return true;
+    }
+
+    return (!!(flags & __WCLONE)) ^ (child->child_termination_signal == SIGCHLD);
+}
+
+static bool child_matches(struct shim_child_process* child, int which, IDTYPE id, int flags) {
+    if (!child_matches_flags(child, flags)) {
+        return false;
+    }
+
+    bool ret = false;
+    switch (which) {
+        case P_PID:
+            if (child->pid == id) {
+                ret = true;
+            }
+            break;
+        case P_PGID:
+            /* TODO: this makes no sense, until we implement IPC pgid change. */
+            break;
+        case P_ALL:
+            ret = true;
+            break;
+        default:
+            /* Impossible. */
+            BUG();
+            break;
+    }
+
+    return ret;
+}
+
+static void remove_qnode_from_wait_queue(struct shim_thread_queue* qnode) {
+    lock(&g_process.children_lock);
+
+    bool seen = false;
+    if (g_process.wait_queue == qnode) {
+        g_process.wait_queue = qnode->next;
+        seen = true;
+    } else if (g_process.wait_queue) {
+        struct shim_thread_queue* tmp = g_process.wait_queue;
+        while (tmp->next) {
+            if (tmp->next == qnode) {
+                tmp->next = qnode->next;
+                seen = true;
+                break;
+            }
+            tmp = tmp->next;
+        }
+    }
+
+    unlock(&g_process.children_lock);
+
+    while (!seen) {
+        DkEventClear(get_cur_thread()->scheduler_event);
+        /* Check `mark_child_exited` for explanation why we might need this compiler barrier. */
+        COMPILER_BARRIER();
+        /* Check if `qnode` is no longer used. */
+        if (!__atomic_load_n(&qnode->in_use, __ATOMIC_ACQUIRE)) {
+            break;
+        }
+        /* We cannot handle any errors here. */
+        (void)thread_sleep(NO_TIMEOUT);
+    }
+}
 
 long shim_do_waitid(int which, pid_t id, siginfo_t* infop, int options, struct __kernel_rusage* ru) {
-    struct shim_thread* cur    = get_cur_thread();
-    struct shim_thread* thread = NULL;
-    int ret;
     __UNUSED(ru);
 
-    /* Note that we don't support WSTOPPED or WCONTINUED correctly. */
     if (options & ~(WNOHANG | WNOWAIT | WEXITED | WSTOPPED | WCONTINUED |
-                    __WNOTHREAD| __WCLONE | __WALL))
+                    __WNOTHREAD | __WCLONE | __WALL))
         return -EINVAL;
+
+    if (options & __WALL) {
+        options &= ~__WCLONE;
+    }
 
     if (!(options & (WEXITED | WSTOPPED | WCONTINUED)))
         return -EINVAL;
+
+    if (options & WSTOPPED) {
+        debug("Ignoring unsupported WSTOPPED flag to wait4\n");
+        options &= ~WSTOPPED;
+    }
+    if (options & WCONTINUED) {
+        debug("Ignoring unsupported WCONTINUED flag to wait4\n");
+        options &= ~WCONTINUED;
+    }
+    assert(options & WEXITED);
+
+    if (options & __WNOTHREAD) {
+        debug("Ignoring unsupported __WNOTHREAD flag to wait4\n");
+        options &= ~__WNOTHREAD;
+    }
 
     if (!(which == P_PGID || which == P_ALL || which == P_PID))
         return -EINVAL;
@@ -44,138 +128,100 @@ long shim_do_waitid(int which, pid_t id, siginfo_t* infop, int options, struct _
     if (infop && test_user_memory(infop, sizeof(*infop), /*write=*/true))
         return -EFAULT;
 
-    if (which == P_PID) {
-        pid_t pid = id;
+    long ret = 0;
 
-        if (!(thread = lookup_thread(pid)))
-            return -ECHILD;
+    lock(&g_process.children_lock);
 
-        if (!(options & WNOHANG)) {
-        block_pid:
-            object_wait_with_retry(thread->exit_event);
+    while (1) {
+        struct shim_child_process* child;
+        /* First search already exited children. */
+        LISTP_FOR_EACH_ENTRY(child, &g_process.zombies, list) {
+            if (child_matches(child, which, id, options)) {
+                /* We have a match! */
+                if (infop) {
+                    infop->si_pid = child->pid;
+                    infop->si_uid = child->uid;
+                    infop->si_signo = SIGCHLD;
+
+                    fill_siginfo_code_and_status(infop, child->term_signal, child->exit_code);
+                }
+
+                if (!(options & WNOWAIT)) {
+                    LISTP_DEL_INIT(child, &g_process.zombies, list);
+                    destroy_child_process(child);
+                }
+                ret = 0;
+                goto out;
+            }
         }
 
-        lock(&thread->lock);
+        /* Do we have any non-exited child to wait for? */
+        bool have_waitable_child = false;
+        LISTP_FOR_EACH_ENTRY(child, &g_process.children, list) {
+            if (child_matches(child, which, id, options)) {
+                have_waitable_child = true;
+                break;
+            }
+        }
 
-        if (thread->is_alive) {
-            unlock(&thread->lock);
-            if (!(options & WNOHANG))
-                goto block_pid;
-            put_thread(thread);
+        if (!have_waitable_child) {
+            ret = -ECHILD;
+            goto out;
+        }
+        /* There are some children we can wait for. */
 
+        if (options & WNOHANG) {
             if (infop) {
                 infop->si_pid = 0;
                 infop->si_signo = 0;
             }
-            return 0;
+            ret = 0;
+            goto out;
         }
 
-        if (!(options & WNOWAIT) && !LIST_EMPTY(thread, siblings)) {
-            debug("reaping thread %p\n", thread);
-            struct shim_thread* parent = thread->parent;
-            assert(parent);
+        /* Ok, let's wait. */
+        struct shim_thread* self = get_cur_thread();
+        struct shim_thread_queue qnode = {
+            .thread = self,
+            .next = g_process.wait_queue,
+        };
+        g_process.wait_queue = &qnode;
+        __atomic_store_n(&qnode.in_use, true, __ATOMIC_RELEASE);
 
-            lock(&parent->lock);
-            /* DEP 5/15/17: These threads are exited */
-            assert(!thread->is_alive);
-            LISTP_DEL_INIT(thread, &parent->exited_children, siblings);
-            unlock(&parent->lock);
+        __atomic_store_n(&self->signal_handled, SIGNAL_NOT_HANDLED, __ATOMIC_RELEASE);
 
-            put_thread(parent);
-            thread->parent = NULL;
-            put_thread(thread);
-        }
+        unlock(&g_process.children_lock);
 
-        unlock(&thread->lock);
-        goto found;
-    }
-
-    lock(&cur->lock);
-
-    if (LISTP_EMPTY(&cur->children) && LISTP_EMPTY(&cur->exited_children)) {
-        unlock(&cur->lock);
-        return -ECHILD;
-    }
-
-    if (!(options & WNOHANG)) {
-    block:
-        if (cur->child_exit_event)
-            while (LISTP_EMPTY(&cur->exited_children)) {
-                unlock(&cur->lock);
-                object_wait_with_retry(cur->child_exit_event);
-                lock(&cur->lock);
+        while (1) {
+            if (__atomic_load_n(&self->signal_handled, __ATOMIC_ACQUIRE) == SIGNAL_HANDLED) {
+                remove_qnode_from_wait_queue(&qnode);
+                return -EINTR;
             }
-    }
 
-    if (which == P_PGID) {
-        IDTYPE pgid;
-        if (id == 0)
-            pgid = cur->pgid;
-        else
-            pgid = id;
-
-        LISTP_FOR_EACH_ENTRY(thread, &cur->exited_children, siblings) {
-            if (thread->pgid == pgid) {
-                get_thread(thread);
-                goto found_child;
+            DkEventClear(self->scheduler_event);
+            /* Check `mark_child_exited` for explanation why we might need this compiler barrier. */
+            COMPILER_BARRIER();
+            /* Check that we are still supposed to sleep. */
+            if (!__atomic_load_n(&qnode.in_use, __ATOMIC_ACQUIRE)) {
+                break;
             }
+            ret = thread_sleep(NO_TIMEOUT);
+            if (ret < 0 && ret != -EINTR && ret != -EAGAIN && ret != -EWOULDBLOCK) {
+                debug("thread_sleep failed in waitid\n");
+
+                remove_qnode_from_wait_queue(&qnode);
+                /* `ret` is already set. */
+                return ret;
+            }
+
+            handle_signals();
         }
 
-        if (!(options & WNOHANG))
-            goto block;
-    } else {
-        assert(which == P_ALL);
-
-        if (!LISTP_EMPTY(&cur->exited_children)) {
-            thread = LISTP_FIRST_ENTRY(&cur->exited_children, struct shim_thread, siblings);
-            get_thread(thread);
-            goto found_child;
-        }
+        lock(&g_process.children_lock);
     }
 
-    unlock(&cur->lock);
-
-    if (infop) {
-        infop->si_pid = 0;
-        infop->si_signo = 0;
-    }
-    return 0;
-
-found_child:
-    if (!(options & WNOWAIT)) {
-        LISTP_DEL_INIT(thread, &cur->exited_children, siblings);
-        put_thread(cur);
-        thread->parent = NULL;
-
-        if (LISTP_EMPTY(&cur->exited_children))
-            DkEventClear(cur->child_exit_event);
-    }
-
-    unlock(&cur->lock);
-
-found:
-    ret = 0;
-    if (infop) {
-        infop->si_pid = thread->tid;
-        infop->si_uid = thread->uid;
-        infop->si_signo = SIGCHLD;
-
-        if (thread->term_signal == 0) {
-            infop->si_code = CLD_EXITED;
-            infop->si_status = thread->exit_code;
-        } else if (thread->term_signal & __WCOREDUMP_BIT) {
-            infop->si_code = CLD_DUMPED;
-            infop->si_status = thread->term_signal & ~__WCOREDUMP_BIT;
-        } else {
-            infop->si_code = CLD_KILLED;
-            infop->si_status = thread->term_signal;
-        }
-    }
-
-    if (!(options & WNOWAIT))
-        del_thread(thread);
-
-    put_thread(thread);
+out:
+    unlock(&g_process.children_lock);
     return ret;
 }
 
@@ -184,7 +230,6 @@ long shim_do_wait4(pid_t pid, int* status, int options, struct __kernel_rusage* 
     pid_t id;
     siginfo_t info;
 
-    /* Note that only WNOHANG is handled correctly. */
     if (options & ~(WNOHANG | WUNTRACED | WCONTINUED | __WNOTHREAD | __WCLONE | __WALL)) {
         return -EINVAL;
     }
@@ -194,8 +239,10 @@ long shim_do_wait4(pid_t pid, int* status, int options, struct __kernel_rusage* 
 
     /* Prepare options for shim_do_waitid(). */
     options |= WEXITED;
-    if (options & WUNTRACED)
+    if (options & WUNTRACED) {
         options &= ~WUNTRACED;
+        options |= WSTOPPED;
+    }
 
     if (pid < -1) {
         which = P_PGID;