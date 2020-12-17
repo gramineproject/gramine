gramineproject/graphene

[Pal] Fix semantics of DkSegmentRegister
On x64 DkSegmentRegister had weird semantics which also disabled some
usages like `0` as fsbase.

LibOS/shim/include/arch/x86_64/shim_tcb-arch.h
@@ -187,7 +187,7 @@ static inline void shim_regs_set_syscallnr(struct shim_regs* sr, uint64_t sc_num
     } while (0)
 
 static inline void shim_arch_update_fs_base(unsigned long fs_base) {
-    DkSegmentRegister(PAL_SEGMENT_FS, (PAL_PTR)fs_base);
+    DkSegmentRegister(PAL_SET_SEGMENT_FS, (PAL_PTR)fs_base);
 }
 
 /* On x86_64 the fs_base is the same as the tls parameter to 'clone' */

LibOS/shim/src/shim_syscalls.c
@@ -505,7 +505,7 @@ void* shim_do_arch_prctl(int code, void* addr) {
             return NULL;
 
         case ARCH_GET_FS:
-            return (void*)DkSegmentRegister(PAL_SEGMENT_FS, NULL) ?: (void*)-PAL_ERRNO();
+            return DkSegmentRegister(PAL_GET_SEGMENT_FS, addr) ? NULL : (void*)-PAL_ERRNO();
     }
 
     return (void*)-ENOSYS;

Pal/src/db_misc.c
@@ -32,23 +32,17 @@ PAL_NUM DkRandomBitsRead(PAL_PTR buffer, PAL_NUM size) {
 }
 
 #if defined(__x86_64__)
-PAL_PTR DkSegmentRegister(PAL_FLG reg, PAL_PTR addr) {
+PAL_BOL DkSegmentRegister(PAL_FLG reg, PAL_PTR addr) {
     ENTER_PAL_CALL(DkSegmentRegister);
-    void* seg_addr = (void*)addr;
-    int ret;
 
-    if (addr) {
-        ret = _DkSegmentRegisterSet(reg, seg_addr);
-    } else {
-        ret = _DkSegmentRegisterGet(reg, &seg_addr);
-    }
+    int ret = _DkSegmentRegister(reg, addr);
 
     if (ret < 0) {
         _DkRaiseFailure(-ret);
-        seg_addr = NULL;
+        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
     }
 
-    LEAVE_PAL_CALL_RETURN((PAL_PTR)seg_addr);
+    LEAVE_PAL_CALL_RETURN(PAL_TRUE);
 }
 #endif
 

Pal/src/host/Linux/db_main-x86_64.c
@@ -230,40 +230,26 @@ int _DkRandomBitsRead(void* buffer, size_t size) {
 }
 #endif
 
-int _DkSegmentRegisterSet(int reg, const void* addr) {
+int _DkSegmentRegister(int reg, void* addr) {
     int ret = 0;
 
-    if (reg == PAL_SEGMENT_FS) {
-        ret = INLINE_SYSCALL(arch_prctl, 2, ARCH_SET_FS, addr);
-    } else if (reg == PAL_SEGMENT_GS) {
-        return -PAL_ERROR_DENIED;
-    } else {
-        return -PAL_ERROR_INVAL;
-    }
-    if (IS_ERR(ret))
-        return -PAL_ERROR_DENIED;
-
-    return 0;
-}
-
-int _DkSegmentRegisterGet(int reg, void** addr) {
-    int ret;
-    unsigned long ret_addr;
-
-    if (reg == PAL_SEGMENT_FS) {
-        ret = INLINE_SYSCALL(arch_prctl, 2, ARCH_GET_FS, &ret_addr);
-    } else if (reg == PAL_SEGMENT_GS) {
-        // The GS segment is used for the internal TCB of PAL
-        return -PAL_ERROR_DENIED;
-    } else {
-        return -PAL_ERROR_INVAL;
+    switch (reg) {
+        case PAL_GET_SEGMENT_FS:
+            ret = INLINE_SYSCALL(arch_prctl, 2, ARCH_GET_FS, addr);
+            break;
+        case PAL_SET_SEGMENT_FS:
+            ret = INLINE_SYSCALL(arch_prctl, 2, ARCH_SET_FS, addr);
+            break;
+        case PAL_GET_SEGMENT_GS:
+            /* Fallthrough */
+        case PAL_SET_SEGMENT_GS:
+            // The GS segment is used for the internal TCB of PAL
+            return -PAL_ERROR_DENIED;
+        default:
+            return -PAL_ERROR_INVAL;
     }
 
-    if (IS_ERR(ret))
-        return -PAL_ERROR_DENIED;
-
-    *addr = (void*)ret_addr;
-    return 0;
+    return ret;
 }
 
 int _DkCpuIdRetrieve(unsigned int leaf, unsigned int subleaf, unsigned int values[4]) {

Pal/src/host/Skeleton/db_misc.c
@@ -32,11 +32,7 @@ size_t _DkRandomBitsRead(void* buffer, size_t size) {
     return -PAL_ERROR_NOTIMPLEMENTED;
 }
 
-int _DkSegmentRegisterSet(int reg, const void* addr) {
-    return -PAL_ERROR_NOTIMPLEMENTED;
-}
-
-int _DkSegmentRegisterGet(int reg, void** addr) {
+int _DkSegmentRegister(int reg, void* addr) {
     return -PAL_ERROR_NOTIMPLEMENTED;
 }
 

Pal/src/pal_internal.h
@@ -277,8 +277,7 @@ int _DkSystemTimeQuery(uint64_t* out_usec);
  * 0 on success, negative on failure.
  */
 size_t _DkRandomBitsRead(void* buffer, size_t size);
-int _DkSegmentRegisterSet(int reg, const void* addr);
-int _DkSegmentRegisterGet(int reg, void** addr);
+int _DkSegmentRegister(int reg, void* addr);
 int _DkInstructionCacheFlush(const void* addr, int size);
 int _DkCpuIdRetrieve(unsigned int leaf, unsigned int subleaf, unsigned int values[4]);
 int _DkAttestationReport(PAL_PTR user_report_data, PAL_NUM* user_report_data_size,

gramineproject/gramine

LibOS/shim/include/arch/x86_64/shim_tcb-arch.h
@@ -187,7 +187,7 @@ static inline void shim_regs_set_syscallnr(struct shim_regs* sr, uint64_t sc_num
     } while (0)
 
 static inline void shim_arch_update_fs_base(unsigned long fs_base) {
-    DkSegmentRegister(PAL_SEGMENT_FS, (PAL_PTR)fs_base);
+    DkSegmentRegisterSet(PAL_SEGMENT_FS, (PAL_PTR)fs_base);
 }
 
 /* On x86_64 the fs_base is the same as the tls parameter to 'clone' */

LibOS/shim/src/shim_syscalls.c
@@ -505,7 +505,7 @@ void* shim_do_arch_prctl(int code, void* addr) {
             return NULL;
 
         case ARCH_GET_FS:
-            return (void*)DkSegmentRegister(PAL_SEGMENT_FS, NULL) ?: (void*)-PAL_ERRNO();
+            return DkSegmentRegisterGet(PAL_SEGMENT_FS, addr) ? NULL : (void*)-PAL_ERRNO();
     }
 
     return (void*)-ENOSYS;

Pal/src/db_misc.c
@@ -32,23 +32,30 @@ PAL_NUM DkRandomBitsRead(PAL_PTR buffer, PAL_NUM size) {
 }
 
 #if defined(__x86_64__)
-PAL_PTR DkSegmentRegister(PAL_FLG reg, PAL_PTR addr) {
-    ENTER_PAL_CALL(DkSegmentRegister);
-    void* seg_addr = (void*)addr;
-    int ret;
+PAL_BOL DkSegmentRegisterGet(PAL_FLG reg, PAL_PTR* addr) {
+    ENTER_PAL_CALL(DkSegmentRegisterGet);
 
-    if (addr) {
-        ret = _DkSegmentRegisterSet(reg, seg_addr);
-    } else {
-        ret = _DkSegmentRegisterGet(reg, &seg_addr);
-    }
+    int ret = _DkSegmentRegisterGet(reg, addr);
 
     if (ret < 0) {
         _DkRaiseFailure(-ret);
-        seg_addr = NULL;
+        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
     }
 
-    LEAVE_PAL_CALL_RETURN((PAL_PTR)seg_addr);
+    LEAVE_PAL_CALL_RETURN(PAL_TRUE);
+}
+
+PAL_BOL DkSegmentRegisterSet(PAL_FLG reg, PAL_PTR addr) {
+    ENTER_PAL_CALL(DkSegmentRegisterSet);
+
+    int ret = _DkSegmentRegisterSet(reg, addr);
+
+    if (ret < 0) {
+        _DkRaiseFailure(-ret);
+        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
+    }
+
+    LEAVE_PAL_CALL_RETURN(PAL_TRUE);
 }
 #endif
 

Pal/src/host/Linux/db_main-x86_64.c
@@ -230,40 +230,28 @@ int _DkRandomBitsRead(void* buffer, size_t size) {
 }
 #endif
 
-int _DkSegmentRegisterSet(int reg, const void* addr) {
-    int ret = 0;
-
-    if (reg == PAL_SEGMENT_FS) {
-        ret = INLINE_SYSCALL(arch_prctl, 2, ARCH_SET_FS, addr);
-    } else if (reg == PAL_SEGMENT_GS) {
-        return -PAL_ERROR_DENIED;
-    } else {
-        return -PAL_ERROR_INVAL;
+int _DkSegmentRegisterGet(int reg, void** addr) {
+    switch (reg) {
+        case PAL_SEGMENT_FS:
+            return INLINE_SYSCALL(arch_prctl, 2, ARCH_GET_FS, addr);
+        case PAL_SEGMENT_GS:
+            // The GS segment is used for the internal TCB of PAL
+            return -PAL_ERROR_DENIED;
+        default:
+            return -PAL_ERROR_INVAL;
     }
-    if (IS_ERR(ret))
-        return -PAL_ERROR_DENIED;
-
-    return 0;
 }
 
-int _DkSegmentRegisterGet(int reg, void** addr) {
-    int ret;
-    unsigned long ret_addr;
-
-    if (reg == PAL_SEGMENT_FS) {
-        ret = INLINE_SYSCALL(arch_prctl, 2, ARCH_GET_FS, &ret_addr);
-    } else if (reg == PAL_SEGMENT_GS) {
-        // The GS segment is used for the internal TCB of PAL
-        return -PAL_ERROR_DENIED;
-    } else {
-        return -PAL_ERROR_INVAL;
+int _DkSegmentRegisterSet(int reg, void* addr) {
+    switch (reg) {
+        case PAL_SEGMENT_FS:
+            return INLINE_SYSCALL(arch_prctl, 2, ARCH_SET_FS, addr);
+        case PAL_SEGMENT_GS:
+            // The GS segment is used for the internal TCB of PAL
+            return -PAL_ERROR_DENIED;
+        default:
+            return -PAL_ERROR_INVAL;
     }
-
-    if (IS_ERR(ret))
-        return -PAL_ERROR_DENIED;
-
-    *addr = (void*)ret_addr;
-    return 0;
 }
 
 int _DkCpuIdRetrieve(unsigned int leaf, unsigned int subleaf, unsigned int values[4]) {

Pal/src/host/Skeleton/db_misc.c
@@ -32,11 +32,11 @@ size_t _DkRandomBitsRead(void* buffer, size_t size) {
     return -PAL_ERROR_NOTIMPLEMENTED;
 }
 
-int _DkSegmentRegisterSet(int reg, const void* addr) {
+int _DkSegmentRegisterGet(int reg, void** addr) {
     return -PAL_ERROR_NOTIMPLEMENTED;
 }
 
-int _DkSegmentRegisterGet(int reg, void** addr) {
+int _DkSegmentRegisterSet(int reg, void* addr) {
     return -PAL_ERROR_NOTIMPLEMENTED;
 }
 

Pal/src/pal_internal.h
@@ -277,8 +277,8 @@ int _DkSystemTimeQuery(uint64_t* out_usec);
  * 0 on success, negative on failure.
  */
 size_t _DkRandomBitsRead(void* buffer, size_t size);
-int _DkSegmentRegisterSet(int reg, const void* addr);
 int _DkSegmentRegisterGet(int reg, void** addr);
+int _DkSegmentRegisterSet(int reg, void* addr);
 int _DkInstructionCacheFlush(const void* addr, int size);
 int _DkCpuIdRetrieve(unsigned int leaf, unsigned int subleaf, unsigned int values[4]);
 int _DkAttestationReport(PAL_PTR user_report_data, PAL_NUM* user_report_data_size,