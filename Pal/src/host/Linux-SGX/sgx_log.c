gramineproject/graphene

[Pal,LibOS] New logging system
Instead of 'loader.debug_type', introduce 'loader.log_level'
and 'loader.log_file', along with a set of definitions for
logging at a chosen level.

For now, the call sites keep using the legacy macros (SGX_DBG and
debug()), because converting them all will conflict with other
big changes in the code base. The existing LibOS calls are
assumed to be at 'info' level.

Pal/src/host/Linux-SGX/sgx_log.c
@@ -1,0 +1,32 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2020 Intel Corporation
+ *                    Paweł Marczewski <pawel@invisiblethingslab.com>
+ */
+
+#include <assert.h>
+
+#include "pal_debug.h"
+#include "pal_internal.h"
+#include "pal_linux.h"
+#include "perm.h"
+#include "sgx_log.h"
+
+int g_sgx_log_level = PAL_LOG_DEFAULT_LEVEL;
+int g_sgx_log_fd = PAL_LOG_DEFAULT_FD;
+
+int sgx_log_init(const char* path) {
+    int ret;
+
+    if (g_sgx_log_fd != PAL_LOG_DEFAULT_FD) {
+        ret = INLINE_SYSCALL(close, 1, g_sgx_log_fd);
+        g_sgx_log_fd = PAL_LOG_DEFAULT_FD;
+        if (ret < 0)
+            return ret;
+    }
+
+    ret = INLINE_SYSCALL(open, 3, path, O_WRONLY | O_APPEND | O_CREAT, PERM_rw_______);
+    if (ret < 0)
+        return ret;
+    g_sgx_log_fd = ret;
+    return 0;
+}

Pal/src/host/Linux-SGX/sgx_log.h
@@ -1,0 +1,34 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2020 Intel Corporation
+ *                    Paweł Marczewski <pawel@invisiblethingslab.com>
+ */
+
+/*
+ * Outer PAL logging interface. This is initialized separately to inner PAL, but (once it's
+ * initialized) should output at the level and to the file specified in manifest.
+ */
+
+#ifndef _SGX_LOG_H
+#define _SGX_LOG_H
+
+#include "pal.h"
+#include "pal_debug.h"
+
+extern int g_sgx_log_level;
+extern int g_sgx_log_fd;
+
+int sgx_log_init(const char* path);
+int sgx_log_printf(const char* fmt, ...) __attribute__((format(printf, 1, 2)));
+
+#define _sgx_log(level, fmt...)                          \
+    do {                                                 \
+        if ((level) <= g_sgx_log_level)                  \
+            pal_fdprintf(g_sgx_log_fd, fmt);             \
+    }  while(0)
+
+#define sgx_log_error(fmt...)    _sgx_log(PAL_LOG_ERROR, fmt)
+#define sgx_log_info(fmt...)     _sgx_log(PAL_LOG_INFO, fmt)
+#define sgx_log_debug(fmt...)    _sgx_log(PAL_LOG_DEBUG, fmt)
+#define sgx_log_trace(fmt...)    _sgx_log(PAL_LOG_TRACE, fmt)
+
+#endif /* _SGX_LOG_H */

Pal/src/host/Linux-SGX/sgx_main.c
@@ -848,6 +848,55 @@ static int parse_loader_config(char* loader_config, struct pal_enclave* enclave_
     }
 #endif
 
+    int log_level = PAL_LOG_ERROR;
+    char* log_level_str = NULL;
+    ret = toml_string_in(manifest_root, "loader.log_level", &log_level_str);
+    if (ret < 0) {
+        SGX_DBG(DBG_E, "Cannot parse \'loader.log_level\'\n");
+        ret = -EINVAL;
+        goto out;
+    }
+    if (log_level_str) {
+        if (!strcmp(log_level_str, "none")) {
+            log_level = PAL_LOG_NONE;
+        } else if (!strcmp(log_level_str, "error")) {
+            log_level = PAL_LOG_ERROR;
+        } else if (!strcmp(log_level_str, "info")) {
+            log_level = PAL_LOG_INFO;
+        } else if (!strcmp(log_level_str, "debug")) {
+            log_level = PAL_LOG_DEBUG;
+        } else if (!strcmp(log_level_str, "trace")) {
+            log_level = PAL_LOG_TRACE;
+        } else if (!strcmp(log_level_str, "all")) {
+            log_level = PAL_LOG_ALL;
+        } else {
+            SGX_DBG(DBG_E, "Unknown 'loader.log_level'\n");
+            ret = -EINVAL;
+            goto out;
+        }
+
+        free(log_level_str);
+    }
+
+    char* log_file = NULL;
+    ret = toml_string_in(manifest_root, "loader.log_file", &log_file);
+    if (ret < 0) {
+        SGX_DBG(DBG_E, "Cannot parse \'loader.log_file\'");
+        ret = -EINVAL;
+        goto out;
+    }
+
+    if (log_level > PAL_LOG_NONE && log_file) {
+        ret = sgx_log_init(log_file);
+        free(log_file);
+
+        if (ret < 0) {
+            SGX_DBG(DBG_E, "Cannot open debug stream: %d\n", ret);
+            goto out;
+        }
+    }
+    g_sgx_log_level = log_level;
+
     ret = 0;
 
 out:

gramineproject/gramine

Pal/src/host/Linux-SGX/sgx_log.c
@@ -1,0 +1,32 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2020 Intel Corporation
+ *                    Paweł Marczewski <pawel@invisiblethingslab.com>
+ */
+
+#include <assert.h>
+
+#include "pal_debug.h"
+#include "pal_internal.h"
+#include "pal_linux.h"
+#include "perm.h"
+#include "sgx_log.h"
+
+int g_urts_log_level = PAL_LOG_DEFAULT_LEVEL;
+int g_urts_log_fd = PAL_LOG_DEFAULT_FD;
+
+int urts_log_init(const char* path) {
+    int ret;
+
+    if (g_urts_log_fd != PAL_LOG_DEFAULT_FD) {
+        ret = INLINE_SYSCALL(close, 1, g_urts_log_fd);
+        g_urts_log_fd = PAL_LOG_DEFAULT_FD;
+        if (ret < 0)
+            return ret;
+    }
+
+    ret = INLINE_SYSCALL(open, 3, path, O_WRONLY | O_APPEND | O_CREAT, PERM_rw_______);
+    if (ret < 0)
+        return ret;
+    g_urts_log_fd = ret;
+    return 0;
+}

Pal/src/host/Linux-SGX/sgx_log.h
@@ -1,0 +1,34 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2020 Intel Corporation
+ *                    Paweł Marczewski <pawel@invisiblethingslab.com>
+ */
+
+/*
+ * Outer PAL logging interface. This is initialized separately to inner PAL, but (once it's
+ * initialized) should output at the level and to the file specified in manifest.
+ */
+
+#ifndef SGX_LOG_H_
+#define SGX_LOG_H_
+
+#include "pal.h"
+#include "pal_debug.h"
+
+extern int g_urts_log_level;
+extern int g_urts_log_fd;
+
+int urts_log_init(const char* path);
+int urts_log_printf(const char* fmt, ...) __attribute__((format(printf, 1, 2)));
+
+#define _urts_log(level, fmt...)                          \
+    do {                                                 \
+        if ((level) <= g_urts_log_level)                  \
+            pal_fdprintf(g_urts_log_fd, fmt);             \
+    }  while(0)
+
+#define urts_log_error(fmt...)    _urts_log(PAL_LOG_ERROR, fmt)
+#define urts_log_info(fmt...)     _urts_log(PAL_LOG_INFO, fmt)
+#define urts_log_debug(fmt...)    _urts_log(PAL_LOG_DEBUG, fmt)
+#define urts_log_trace(fmt...)    _urts_log(PAL_LOG_TRACE, fmt)
+
+#endif /* SGX_LOG_H_ */

Pal/src/host/Linux-SGX/sgx_main.c
@@ -826,6 +826,54 @@ static int parse_loader_config(char* manifest, struct pal_enclave* enclave_info)
     }
 #endif
 
+    int log_level = PAL_LOG_DEFAULT_LEVEL;
+    char* log_level_str = NULL;
+    ret = toml_string_in(manifest_root, "loader.log_level", &log_level_str);
+    if (ret < 0) {
+        SGX_DBG(DBG_E, "Cannot parse 'loader.log_level'\n");
+        ret = -EINVAL;
+        goto out;
+    }
+    if (log_level_str) {
+        if (!strcmp(log_level_str, "none")) {
+            log_level = PAL_LOG_NONE;
+        } else if (!strcmp(log_level_str, "error")) {
+            log_level = PAL_LOG_ERROR;
+        } else if (!strcmp(log_level_str, "info")) {
+            log_level = PAL_LOG_INFO;
+        } else if (!strcmp(log_level_str, "debug")) {
+            log_level = PAL_LOG_DEBUG;
+        } else if (!strcmp(log_level_str, "trace")) {
+            log_level = PAL_LOG_TRACE;
+        } else if (!strcmp(log_level_str, "all")) {
+            log_level = PAL_LOG_ALL;
+        } else {
+            SGX_DBG(DBG_E, "Unknown 'loader.log_level'\n");
+            ret = -EINVAL;
+            goto out;
+        }
+    }
+    free(log_level_str);
+
+    char* log_file = NULL;
+    ret = toml_string_in(manifest_root, "loader.log_file", &log_file);
+    if (ret < 0) {
+        SGX_DBG(DBG_E, "Cannot parse 'loader.log_file'\n");
+        ret = -EINVAL;
+        goto out;
+    }
+    if (log_level > PAL_LOG_NONE && log_file) {
+        ret = urts_log_init(log_file);
+
+        if (ret < 0) {
+            SGX_DBG(DBG_E, "Cannot open log file: %d\n", ret);
+            goto out;
+        }
+    }
+    free(log_file);
+
+    g_urts_log_level = log_level;
+
     ret = 0;
 
 out: