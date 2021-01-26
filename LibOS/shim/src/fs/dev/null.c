gramineproject/graphene

[LibOS] Add dummy lseek emulation for /dev/{zero,random,null}
Some apps (e.g., Python Dill module) perform a dummy lseek() on
these /dev/ files.

LibOS/shim/src/fs/dev/null.c
@@ -24,6 +24,13 @@ static ssize_t dev_null_write(struct shim_handle* hdl, const void* buf, size_t c
     return count;
 }
 
+static off_t dev_null_seek(struct shim_handle* hdl, off_t offset, int wence) {
+    __UNUSED(hdl);
+    __UNUSED(offset);
+    __UNUSED(wence);
+    return 0;
+}
+
 static int dev_null_truncate(struct shim_handle* hdl, uint64_t size) {
     __UNUSED(hdl);
     __UNUSED(size);
@@ -61,6 +68,7 @@ static int dev_null_open(struct shim_handle* hdl, const char* name, int flags) {
 
     struct shim_dev_ops ops = {.read     = &dev_null_read,
                                .write    = &dev_null_write,
+                               .seek     = &dev_null_seek,
                                .truncate = &dev_null_truncate,
                                .mode     = &dev_null_mode,
                                .stat     = &dev_null_stat,

LibOS/shim/src/fs/dev/random.c
@@ -27,6 +27,13 @@ static ssize_t dev_random_write(struct shim_handle* hdl, const void* buf, size_t
     return count;
 }
 
+static off_t dev_random_seek(struct shim_handle* hdl, off_t offset, int wence) {
+    __UNUSED(hdl);
+    __UNUSED(offset);
+    __UNUSED(wence);
+    return 0;
+}
+
 static int dev_random_mode(const char* name, mode_t* mode) {
     __UNUSED(name);
     *mode = FILE_RW_MODE | S_IFCHR;
@@ -52,6 +59,7 @@ static int dev_random_open(struct shim_handle* hdl, const char* name, int flags)
 
     struct shim_dev_ops ops = {.read  = &dev_random_read,
                                .write = &dev_random_write,
+                               .seek  = &dev_random_seek,
                                .mode  = &dev_random_mode,
                                .stat  = &dev_random_stat,
                                .hstat = &dev_random_hstat};

LibOS/shim/src/fs/dev/zero.c
@@ -23,6 +23,13 @@ static ssize_t dev_zero_write(struct shim_handle* hdl, const void* buf, size_t c
     return count;
 }
 
+static off_t dev_zero_seek(struct shim_handle* hdl, off_t offset, int wence) {
+    __UNUSED(hdl);
+    __UNUSED(offset);
+    __UNUSED(wence);
+    return 0;
+}
+
 static int dev_zero_truncate(struct shim_handle* hdl, uint64_t size) {
     __UNUSED(hdl);
     __UNUSED(size);
@@ -54,6 +61,7 @@ static int dev_zero_open(struct shim_handle* hdl, const char* name, int flags) {
 
     struct shim_dev_ops ops = {.read     = &dev_zero_read,
                                .write    = &dev_zero_write,
+                               .seek     = &dev_zero_seek,
                                .truncate = &dev_zero_truncate,
                                .mode     = &dev_zero_mode,
                                .stat     = &dev_zero_stat,

gramineproject/gramine

LibOS/shim/src/fs/dev/null.c
@@ -24,6 +24,13 @@ static ssize_t dev_null_write(struct shim_handle* hdl, const void* buf, size_t c
     return count;
 }
 
+static off_t dev_null_seek(struct shim_handle* hdl, off_t offset, int whence) {
+    __UNUSED(hdl);
+    __UNUSED(offset);
+    __UNUSED(whence);
+    return 0;
+}
+
 static int dev_null_truncate(struct shim_handle* hdl, uint64_t size) {
     __UNUSED(hdl);
     __UNUSED(size);
@@ -61,6 +68,7 @@ static int dev_null_open(struct shim_handle* hdl, const char* name, int flags) {
 
     struct shim_dev_ops ops = {.read     = &dev_null_read,
                                .write    = &dev_null_write,
+                               .seek     = &dev_null_seek,
                                .truncate = &dev_null_truncate,
                                .mode     = &dev_null_mode,
                                .stat     = &dev_null_stat,

LibOS/shim/src/fs/dev/random.c
@@ -27,6 +27,13 @@ static ssize_t dev_random_write(struct shim_handle* hdl, const void* buf, size_t
     return count;
 }
 
+static off_t dev_random_seek(struct shim_handle* hdl, off_t offset, int whence) {
+    __UNUSED(hdl);
+    __UNUSED(offset);
+    __UNUSED(whence);
+    return 0;
+}
+
 static int dev_random_mode(const char* name, mode_t* mode) {
     __UNUSED(name);
     *mode = FILE_RW_MODE | S_IFCHR;
@@ -52,6 +59,7 @@ static int dev_random_open(struct shim_handle* hdl, const char* name, int flags)
 
     struct shim_dev_ops ops = {.read  = &dev_random_read,
                                .write = &dev_random_write,
+                               .seek  = &dev_random_seek,
                                .mode  = &dev_random_mode,
                                .stat  = &dev_random_stat,
                                .hstat = &dev_random_hstat};

LibOS/shim/src/fs/dev/zero.c
@@ -23,6 +23,13 @@ static ssize_t dev_zero_write(struct shim_handle* hdl, const void* buf, size_t c
     return count;
 }
 
+static off_t dev_zero_seek(struct shim_handle* hdl, off_t offset, int whence) {
+    __UNUSED(hdl);
+    __UNUSED(offset);
+    __UNUSED(whence);
+    return 0;
+}
+
 static int dev_zero_truncate(struct shim_handle* hdl, uint64_t size) {
     __UNUSED(hdl);
     __UNUSED(size);
@@ -54,6 +61,7 @@ static int dev_zero_open(struct shim_handle* hdl, const char* name, int flags) {
 
     struct shim_dev_ops ops = {.read     = &dev_zero_read,
                                .write    = &dev_zero_write,
+                               .seek     = &dev_zero_seek,
                                .truncate = &dev_zero_truncate,
                                .mode     = &dev_zero_mode,
                                .stat     = &dev_zero_stat,