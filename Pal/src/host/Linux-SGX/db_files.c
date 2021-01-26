gramineproject/graphene

[Pal/Linux-SGX] Remove SGX_DBG
Replace the old SGX_DBG macro with new subsystem (log_* inside
enclave, urts_log_* outside enclave).

Adjust log levels of some messages, and remove some unnecessary
ones.

Pal/src/host/Linux-SGX/db_files.c
@@ -46,7 +46,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
     char* path = (void*)hdl + HANDLE_SIZE(file);
     int ret;
     if ((ret = get_norm_path(uri, path, &len)) < 0) {
-        SGX_DBG(DBG_E, "Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
+        log_error("Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
         free(hdl);
         return ret;
     }
@@ -72,7 +72,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
     /* check if the file is seekable and get real file size */
     ret = ocall_fstat(fd, &st);
     if (IS_ERR(ret)) {
-        SGX_DBG(DBG_E, "file_open(%s): fstat failed: %d\n", path, ret);
+        log_error("file_open(%s): fstat failed: %d\n", path, ret);
         ret = unix_to_pal_error(ERRNO(ret));
         goto out;
     }
@@ -91,7 +91,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
         /* disallow opening more than one writable handle to a PF */
         if (pf_mode & PF_FILE_MODE_WRITE) {
             if (pf->writable_fd >= 0) {
-                SGX_DBG(DBG_D, "file_open(%s): disallowing concurrent writable handle\n", path);
+                log_debug("file_open(%s): disallowing concurrent writable handle\n", path);
                 ret = -PAL_ERROR_DENIED;
                 goto out;
             }
@@ -101,7 +101,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
 
         /* the protected files should be regular files (seekable) */
         if (!hdl->file.seekable) {
-            SGX_DBG(DBG_E, "file_open(%s): disallowing non-seekable file handle\n", path);
+            log_error("file_open(%s): disallowing non-seekable file handle\n", path);
             goto out;
         }
 
@@ -112,7 +112,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
                 pf->writable_fd = fd;
             }
         } else {
-            SGX_DBG(DBG_E, "load_protected_file(%s, %d) failed\n", path, hdl->file.fd);
+            log_error("load_protected_file(%s, %d) failed\n", path, hdl->file.fd);
             goto out;
         }
     } else {
@@ -121,9 +121,9 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
         void* umem;
         ret = load_trusted_file(hdl, &stubs, &total, create, &umem);
         if (ret < 0) {
-            SGX_DBG(DBG_E, "Accessing file:%s is denied (%s). This file is not trusted or allowed."
-                    " Trusted files should be regular files (seekable).\n", hdl->file.realpath,
-                    pal_strerror(-ret));
+            log_error("Accessing file:%s is denied (%s). This file is not trusted or allowed."
+                      " Trusted files should be regular files (seekable).\n", hdl->file.realpath,
+                      pal_strerror(-ret));
             goto out;
         }
 
@@ -156,7 +156,7 @@ static int64_t pf_file_read(struct protected_file* pf, PAL_HANDLE handle, uint64
     int fd = handle->file.fd;
 
     if (!pf->context) {
-        SGX_DBG(DBG_E, "pf_file_read(PF fd %d): PF not initialized\n", fd);
+        log_error("pf_file_read(PF fd %d): PF not initialized\n", fd);
         return -PAL_ERROR_BADHANDLE;
     }
 
@@ -164,7 +164,7 @@ static int64_t pf_file_read(struct protected_file* pf, PAL_HANDLE handle, uint64
     pf_status_t pfs = pf_read(pf->context, offset, count, buffer, &bytes_read);
 
     if (PF_FAILURE(pfs)) {
-        SGX_DBG(DBG_E, "pf_file_read(PF fd %d): pf_read failed: %d\n", fd, pfs);
+        log_error("pf_file_read(PF fd %d): pf_read failed: %d\n", fd, pfs);
         return -PAL_ERROR_DENIED;
     }
 
@@ -220,14 +220,14 @@ static int64_t pf_file_write(struct protected_file* pf, PAL_HANDLE handle, uint6
     int fd = handle->file.fd;
 
     if (!pf->context) {
-        SGX_DBG(DBG_E, "pf_file_write(PF fd %d): PF not initialized\n", fd);
+        log_error("pf_file_write(PF fd %d): PF not initialized\n", fd);
         return -PAL_ERROR_BADHANDLE;
     }
 
     pf_status_t pf_ret = pf_write(pf->context, offset, count, buffer);
 
     if (PF_FAILURE(pf_ret)) {
-        SGX_DBG(DBG_E, "pf_file_write(PF fd %d): pf_write failed: %d\n", fd, pf_ret);
+        log_error("pf_file_write(PF fd %d): pf_write failed: %d\n", fd, pf_ret);
         return -PAL_ERROR_DENIED;
     }
 
@@ -258,7 +258,7 @@ static int64_t file_write(PAL_HANDLE handle, uint64_t offset, uint64_t count, co
     }
 
     /* case of trusted file: disallow writing completely */
-    SGX_DBG(DBG_E, "Writing to a trusted file (%s) is disallowed!\n", handle->file.realpath);
+    log_error("Writing to a trusted file (%s) is disallowed!\n", handle->file.realpath);
     return -PAL_ERROR_DENIED;
 }
 
@@ -266,7 +266,7 @@ static int pf_file_close(struct protected_file* pf, PAL_HANDLE handle) {
     int fd = handle->file.fd;
 
     if (pf->refcount == 0) {
-        SGX_DBG(DBG_E, "pf_file_close(PF fd %d): refcount == 0\n", fd);
+        log_error("pf_file_close(PF fd %d): refcount == 0\n", fd);
         return -PAL_ERROR_INVAL;
     }
 
@@ -328,12 +328,12 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
 
     assert(WITHIN_MASK(prot, PAL_PROT_MASK));
     if ((prot & PAL_PROT_READ) && (prot & PAL_PROT_WRITE)) {
-        SGX_DBG(DBG_E, "pf_file_map(PF fd %d): trying to map with R+W access\n", fd);
+        log_info("pf_file_map(PF fd %d): trying to map with R+W access\n", fd);
         return -PAL_ERROR_NOTSUPPORT;
     }
 
     if (!pf->context) {
-        SGX_DBG(DBG_E, "pf_file_map(PF fd %d): PF not initialized\n", fd);
+        log_error("pf_file_map(PF fd %d): PF not initialized\n", fd);
         return -PAL_ERROR_BADHANDLE;
     }
 
@@ -342,8 +342,8 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
     __UNUSED(pfs);
     assert(PF_SUCCESS(pfs));
 
-    SGX_DBG(DBG_D, "pf_file_map(PF fd %d): pf %p, addr %p, prot %d, offset %lu, size %lu\n", fd, pf,
-            *addr, prot, offset, size);
+    log_debug("pf_file_map(PF fd %d): pf %p, addr %p, prot %d, offset %lu, size %lu\n", fd, pf,
+              *addr, prot, offset, size);
 
     if (*addr == NULL) {
         /* LibOS didn't provide address at which to map, can happen on sendfile() */
@@ -374,8 +374,8 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
     if (prot & PAL_PROT_READ) {
         /* we don't check this on writes since file size may be extended then */
         if (offset >= pf_size) {
-            SGX_DBG(DBG_E, "pf_file_map(PF fd %d): offset (%lu) >= file size (%lu)\n", fd, offset,
-                    pf_size);
+            log_error("pf_file_map(PF fd %d): offset (%lu) >= file size (%lu)\n", fd, offset,
+                      pf_size);
             ret = -PAL_ERROR_INVAL;
             goto out;
         }
@@ -389,7 +389,7 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
             pf_ret = PF_STATUS_CORRUPTED;
         }
         if (PF_FAILURE(pf_ret)) {
-            SGX_DBG(DBG_E, "pf_file_map(PF fd %d): pf_read failed: %d\n", fd, pf_ret);
+            log_error("pf_file_map(PF fd %d): pf_read failed: %d\n", fd, pf_ret);
             ret = -PAL_ERROR_DENIED;
             goto out;
         }
@@ -433,11 +433,11 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
     }
 
     if (!(prot & PAL_PROT_WRITECOPY) && (prot & PAL_PROT_WRITE)) {
-        SGX_DBG(DBG_E,
-                "file_map does not currently support writable pass-through mappings on SGX.  You "
-                "may add the PAL_PROT_WRITECOPY (MAP_PRIVATE) flag to your file mapping to keep "
-                "the writes inside the enclave but they won't be reflected outside of the "
-                "enclave.\n");
+        log_error(
+            "file_map does not currently support writable pass-through mappings on SGX.  You "
+            "may add the PAL_PROT_WRITECOPY (MAP_PRIVATE) flag to your file mapping to keep "
+            "the writes inside the enclave but they won't be reflected outside of the "
+            "enclave.\n");
         return -PAL_ERROR_DENIED;
     }
 
@@ -466,7 +466,7 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
                                            mem, offset, end - offset,
                                            stubs, handle->file.total);
         if (ret < 0) {
-            SGX_DBG(DBG_E, "file_map - copy & verify on trusted file returned %d\n", ret);
+            log_error("file_map - copy & verify on trusted file returned %d\n", ret);
             goto out;
         }
     } else {
@@ -495,7 +495,7 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
             } else if (ERRNO(bytes) == EINTR || ERRNO(bytes) == EAGAIN) {
                 continue;
             } else {
-                SGX_DBG(DBG_E, "file_map - ocall_pread on allowed file returned %ld\n", bytes);
+                log_error("file_map - ocall_pread on allowed file returned %ld\n", bytes);
                 ret = unix_to_pal_error(ERRNO(bytes));
                 goto out;
             }
@@ -523,8 +523,8 @@ static int64_t pf_file_setlength(struct protected_file* pf, PAL_HANDLE handle, u
 
     pf_status_t pfs = pf_set_size(pf->context, length);
     if (PF_FAILURE(pfs)) {
-        SGX_DBG(DBG_E, "pf_file_setlength(PF fd %d, %lu): pf_set_size returned %d\n", fd, length,
-                pfs);
+        log_error("pf_file_setlength(PF fd %d, %lu): pf_set_size returned %d\n", fd, length,
+                  pfs);
         return -PAL_ERROR_DENIED;
     }
     return length;
@@ -551,12 +551,12 @@ static int file_flush(PAL_HANDLE handle) {
     if (pf) {
         int ret = flush_pf_maps(pf, /*buffer=*/NULL, /*remove=*/false);
         if (ret < 0) {
-            SGX_DBG(DBG_E, "file_flush(PF fd %d): flush_pf_maps returned %d\n", fd, ret);
+            log_error("file_flush(PF fd %d): flush_pf_maps returned %d\n", fd, ret);
             return ret;
         }
         pf_status_t pfs = pf_flush(pf->context);
         if (PF_FAILURE(pfs)) {
-            SGX_DBG(DBG_E, "file_flush(PF fd %d): pf_flush returned %d\n", fd, pfs);
+            log_error("file_flush(PF fd %d): pf_flush returned %d\n", fd, pfs);
             return -PAL_ERROR_DENIED;
         }
     } else {
@@ -597,8 +597,8 @@ static int pf_file_attrquery(struct protected_file* pf, int fd_from_attrquery, c
     pf = load_protected_file(path, &fd_from_attrquery, real_size, PF_FILE_MODE_READ,
                              /*create=*/false, pf);
     if (!pf) {
-        SGX_DBG(DBG_E, "pf_file_attrquery: load_protected_file(%s, %d) failed\n", path,
-                fd_from_attrquery);
+        log_error("pf_file_attrquery: load_protected_file(%s, %d) failed\n", path,
+                  fd_from_attrquery);
         /* The call above will fail for PFs that were tampered with or have a wrong path.
          * glibc kills the process if this fails during directory enumeration, but that
          * should be fine given the scenario.
@@ -651,7 +651,7 @@ static int file_attrquery(const char* type, const char* uri, PAL_STREAM_ATTR* at
     size_t len = URI_MAX;
     ret = get_norm_path(uri, path, &len);
     if (ret < 0) {
-        SGX_DBG(DBG_E, "Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
+        log_error("Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
         goto out;
     }
 

Pal/src/host/Linux-SGX/db_process.c
@@ -89,8 +89,7 @@ static int generate_sign_data(const PAL_SESSION_KEY* session_key, uint64_t encla
     if (ret < 0)
         return ret;
 
-    SGX_DBG(DBG_P | DBG_S, "Enclave identifier: %016lx -> %s\n", enclave_id,
-            ALLOCA_BYTES2HEXSTR(data.eid_mac));
+    log_debug("Enclave identifier: %016lx -> %s\n", enclave_id, ALLOCA_BYTES2HEXSTR(data.eid_mac));
 
     /* Copy proc_data into sgx_sign_data_t */
     assert(sizeof(data) <= sizeof(*sign_data));
@@ -280,7 +279,7 @@ int init_child_process(PAL_HANDLE* parent_handle) {
 
 noreturn void _DkProcessExit(int exitcode) {
     if (exitcode)
-        SGX_DBG(DBG_I, "DkProcessExit: Returning exit code %d\n", exitcode);
+        log_info("DkProcessExit: Returning exit code %d\n", exitcode);
     ocall_exit(exitcode, /*is_exitgroup=*/true);
     /* Unreachable. */
 }

gramineproject/gramine

Pal/src/host/Linux-SGX/db_files.c
@@ -46,7 +46,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
     char* path = (void*)hdl + HANDLE_SIZE(file);
     int ret;
     if ((ret = get_norm_path(uri, path, &len)) < 0) {
-        SGX_DBG(DBG_E, "Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
+        log_error("Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
         free(hdl);
         return ret;
     }
@@ -72,7 +72,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
     /* check if the file is seekable and get real file size */
     ret = ocall_fstat(fd, &st);
     if (IS_ERR(ret)) {
-        SGX_DBG(DBG_E, "file_open(%s): fstat failed: %d\n", path, ret);
+        log_error("file_open(%s): fstat failed: %d\n", path, ret);
         ret = unix_to_pal_error(ERRNO(ret));
         goto out;
     }
@@ -91,7 +91,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
         /* disallow opening more than one writable handle to a PF */
         if (pf_mode & PF_FILE_MODE_WRITE) {
             if (pf->writable_fd >= 0) {
-                SGX_DBG(DBG_D, "file_open(%s): disallowing concurrent writable handle\n", path);
+                log_error("file_open(%s): disallowing concurrent writable handle\n", path);
                 ret = -PAL_ERROR_DENIED;
                 goto out;
             }
@@ -101,7 +101,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
 
         /* the protected files should be regular files (seekable) */
         if (!hdl->file.seekable) {
-            SGX_DBG(DBG_E, "file_open(%s): disallowing non-seekable file handle\n", path);
+            log_error("file_open(%s): disallowing non-seekable file handle\n", path);
             goto out;
         }
 
@@ -112,7 +112,7 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
                 pf->writable_fd = fd;
             }
         } else {
-            SGX_DBG(DBG_E, "load_protected_file(%s, %d) failed\n", path, hdl->file.fd);
+            log_error("load_protected_file(%s, %d) failed\n", path, hdl->file.fd);
             goto out;
         }
     } else {
@@ -121,9 +121,9 @@ static int file_open(PAL_HANDLE* handle, const char* type, const char* uri, int 
         void* umem;
         ret = load_trusted_file(hdl, &stubs, &total, create, &umem);
         if (ret < 0) {
-            SGX_DBG(DBG_E, "Accessing file:%s is denied (%s). This file is not trusted or allowed."
-                    " Trusted files should be regular files (seekable).\n", hdl->file.realpath,
-                    pal_strerror(-ret));
+            log_error("Accessing file:%s is denied (%s). This file is not trusted or allowed."
+                      " Trusted files should be regular files (seekable).\n", hdl->file.realpath,
+                      pal_strerror(-ret));
             goto out;
         }
 
@@ -156,7 +156,7 @@ static int64_t pf_file_read(struct protected_file* pf, PAL_HANDLE handle, uint64
     int fd = handle->file.fd;
 
     if (!pf->context) {
-        SGX_DBG(DBG_E, "pf_file_read(PF fd %d): PF not initialized\n", fd);
+        log_error("pf_file_read(PF fd %d): PF not initialized\n", fd);
         return -PAL_ERROR_BADHANDLE;
     }
 
@@ -164,7 +164,7 @@ static int64_t pf_file_read(struct protected_file* pf, PAL_HANDLE handle, uint64
     pf_status_t pfs = pf_read(pf->context, offset, count, buffer, &bytes_read);
 
     if (PF_FAILURE(pfs)) {
-        SGX_DBG(DBG_E, "pf_file_read(PF fd %d): pf_read failed: %d\n", fd, pfs);
+        log_error("pf_file_read(PF fd %d): pf_read failed: %d\n", fd, pfs);
         return -PAL_ERROR_DENIED;
     }
 
@@ -220,14 +220,14 @@ static int64_t pf_file_write(struct protected_file* pf, PAL_HANDLE handle, uint6
     int fd = handle->file.fd;
 
     if (!pf->context) {
-        SGX_DBG(DBG_E, "pf_file_write(PF fd %d): PF not initialized\n", fd);
+        log_error("pf_file_write(PF fd %d): PF not initialized\n", fd);
         return -PAL_ERROR_BADHANDLE;
     }
 
     pf_status_t pf_ret = pf_write(pf->context, offset, count, buffer);
 
     if (PF_FAILURE(pf_ret)) {
-        SGX_DBG(DBG_E, "pf_file_write(PF fd %d): pf_write failed: %d\n", fd, pf_ret);
+        log_error("pf_file_write(PF fd %d): pf_write failed: %d\n", fd, pf_ret);
         return -PAL_ERROR_DENIED;
     }
 
@@ -258,7 +258,7 @@ static int64_t file_write(PAL_HANDLE handle, uint64_t offset, uint64_t count, co
     }
 
     /* case of trusted file: disallow writing completely */
-    SGX_DBG(DBG_E, "Writing to a trusted file (%s) is disallowed!\n", handle->file.realpath);
+    log_error("Writing to a trusted file (%s) is disallowed!\n", handle->file.realpath);
     return -PAL_ERROR_DENIED;
 }
 
@@ -266,7 +266,7 @@ static int pf_file_close(struct protected_file* pf, PAL_HANDLE handle) {
     int fd = handle->file.fd;
 
     if (pf->refcount == 0) {
-        SGX_DBG(DBG_E, "pf_file_close(PF fd %d): refcount == 0\n", fd);
+        log_error("pf_file_close(PF fd %d): refcount == 0\n", fd);
         return -PAL_ERROR_INVAL;
     }
 
@@ -328,12 +328,12 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
 
     assert(WITHIN_MASK(prot, PAL_PROT_MASK));
     if ((prot & PAL_PROT_READ) && (prot & PAL_PROT_WRITE)) {
-        SGX_DBG(DBG_E, "pf_file_map(PF fd %d): trying to map with R+W access\n", fd);
+        log_error("pf_file_map(PF fd %d): trying to map with R+W access\n", fd);
         return -PAL_ERROR_NOTSUPPORT;
     }
 
     if (!pf->context) {
-        SGX_DBG(DBG_E, "pf_file_map(PF fd %d): PF not initialized\n", fd);
+        log_error("pf_file_map(PF fd %d): PF not initialized\n", fd);
         return -PAL_ERROR_BADHANDLE;
     }
 
@@ -342,8 +342,8 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
     __UNUSED(pfs);
     assert(PF_SUCCESS(pfs));
 
-    SGX_DBG(DBG_D, "pf_file_map(PF fd %d): pf %p, addr %p, prot %d, offset %lu, size %lu\n", fd, pf,
-            *addr, prot, offset, size);
+    log_debug("pf_file_map(PF fd %d): pf %p, addr %p, prot %d, offset %lu, size %lu\n", fd, pf,
+              *addr, prot, offset, size);
 
     if (*addr == NULL) {
         /* LibOS didn't provide address at which to map, can happen on sendfile() */
@@ -374,8 +374,8 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
     if (prot & PAL_PROT_READ) {
         /* we don't check this on writes since file size may be extended then */
         if (offset >= pf_size) {
-            SGX_DBG(DBG_E, "pf_file_map(PF fd %d): offset (%lu) >= file size (%lu)\n", fd, offset,
-                    pf_size);
+            log_error("pf_file_map(PF fd %d): offset (%lu) >= file size (%lu)\n", fd, offset,
+                      pf_size);
             ret = -PAL_ERROR_INVAL;
             goto out;
         }
@@ -389,7 +389,7 @@ static int pf_file_map(struct protected_file* pf, PAL_HANDLE handle, void** addr
             pf_ret = PF_STATUS_CORRUPTED;
         }
         if (PF_FAILURE(pf_ret)) {
-            SGX_DBG(DBG_E, "pf_file_map(PF fd %d): pf_read failed: %d\n", fd, pf_ret);
+            log_error("pf_file_map(PF fd %d): pf_read failed: %d\n", fd, pf_ret);
             ret = -PAL_ERROR_DENIED;
             goto out;
         }
@@ -433,11 +433,11 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
     }
 
     if (!(prot & PAL_PROT_WRITECOPY) && (prot & PAL_PROT_WRITE)) {
-        SGX_DBG(DBG_E,
-                "file_map does not currently support writable pass-through mappings on SGX.  You "
-                "may add the PAL_PROT_WRITECOPY (MAP_PRIVATE) flag to your file mapping to keep "
-                "the writes inside the enclave but they won't be reflected outside of the "
-                "enclave.\n");
+        log_error(
+            "file_map does not currently support writable pass-through mappings on SGX.  You "
+            "may add the PAL_PROT_WRITECOPY (MAP_PRIVATE) flag to your file mapping to keep "
+            "the writes inside the enclave but they won't be reflected outside of the "
+            "enclave.\n");
         return -PAL_ERROR_DENIED;
     }
 
@@ -466,7 +466,7 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
                                            mem, offset, end - offset,
                                            stubs, handle->file.total);
         if (ret < 0) {
-            SGX_DBG(DBG_E, "file_map - copy & verify on trusted file returned %d\n", ret);
+            log_error("file_map - copy & verify on trusted file returned %d\n", ret);
             goto out;
         }
     } else {
@@ -495,7 +495,7 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
             } else if (ERRNO(bytes) == EINTR || ERRNO(bytes) == EAGAIN) {
                 continue;
             } else {
-                SGX_DBG(DBG_E, "file_map - ocall_pread on allowed file returned %ld\n", bytes);
+                log_error("file_map - ocall_pread on allowed file returned %ld\n", bytes);
                 ret = unix_to_pal_error(ERRNO(bytes));
                 goto out;
             }
@@ -523,8 +523,8 @@ static int64_t pf_file_setlength(struct protected_file* pf, PAL_HANDLE handle, u
 
     pf_status_t pfs = pf_set_size(pf->context, length);
     if (PF_FAILURE(pfs)) {
-        SGX_DBG(DBG_E, "pf_file_setlength(PF fd %d, %lu): pf_set_size returned %d\n", fd, length,
-                pfs);
+        log_error("pf_file_setlength(PF fd %d, %lu): pf_set_size returned %d\n", fd, length,
+                  pfs);
         return -PAL_ERROR_DENIED;
     }
     return length;
@@ -551,12 +551,12 @@ static int file_flush(PAL_HANDLE handle) {
     if (pf) {
         int ret = flush_pf_maps(pf, /*buffer=*/NULL, /*remove=*/false);
         if (ret < 0) {
-            SGX_DBG(DBG_E, "file_flush(PF fd %d): flush_pf_maps returned %d\n", fd, ret);
+            log_error("file_flush(PF fd %d): flush_pf_maps returned %d\n", fd, ret);
             return ret;
         }
         pf_status_t pfs = pf_flush(pf->context);
         if (PF_FAILURE(pfs)) {
-            SGX_DBG(DBG_E, "file_flush(PF fd %d): pf_flush returned %d\n", fd, pfs);
+            log_error("file_flush(PF fd %d): pf_flush returned %d\n", fd, pfs);
             return -PAL_ERROR_DENIED;
         }
     } else {
@@ -597,8 +597,8 @@ static int pf_file_attrquery(struct protected_file* pf, int fd_from_attrquery, c
     pf = load_protected_file(path, &fd_from_attrquery, real_size, PF_FILE_MODE_READ,
                              /*create=*/false, pf);
     if (!pf) {
-        SGX_DBG(DBG_E, "pf_file_attrquery: load_protected_file(%s, %d) failed\n", path,
-                fd_from_attrquery);
+        log_error("pf_file_attrquery: load_protected_file(%s, %d) failed\n", path,
+                  fd_from_attrquery);
         /* The call above will fail for PFs that were tampered with or have a wrong path.
          * glibc kills the process if this fails during directory enumeration, but that
          * should be fine given the scenario.
@@ -651,7 +651,7 @@ static int file_attrquery(const char* type, const char* uri, PAL_STREAM_ATTR* at
     size_t len = URI_MAX;
     ret = get_norm_path(uri, path, &len);
     if (ret < 0) {
-        SGX_DBG(DBG_E, "Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
+        log_error("Could not normalize path (%s): %s\n", uri, pal_strerror(-ret));
         goto out;
     }
 

Pal/src/host/Linux-SGX/db_process.c
@@ -89,8 +89,7 @@ static int generate_sign_data(const PAL_SESSION_KEY* session_key, uint64_t encla
     if (ret < 0)
         return ret;
 
-    SGX_DBG(DBG_P | DBG_S, "Enclave identifier: %016lx -> %s\n", enclave_id,
-            ALLOCA_BYTES2HEXSTR(data.eid_mac));
+    log_debug("Enclave identifier: %016lx -> %s\n", enclave_id, ALLOCA_BYTES2HEXSTR(data.eid_mac));
 
     /* Copy proc_data into sgx_sign_data_t */
     assert(sizeof(data) <= sizeof(*sign_data));
@@ -280,7 +279,7 @@ int init_child_process(PAL_HANDLE* parent_handle) {
 
 noreturn void _DkProcessExit(int exitcode) {
     if (exitcode)
-        SGX_DBG(DBG_I, "DkProcessExit: Returning exit code %d\n", exitcode);
+        log_debug("DkProcessExit: Returning exit code %d\n", exitcode);
     ocall_exit(exitcode, /*is_exitgroup=*/true);
     /* Unreachable. */
 }