gramineproject/graphene

[LibOS] Change `mmap` signature so that the file offset is unsigned
On Linux `mmap` syscall takes the file offset as `unsigned long`, only
the POSIX wrapper has it as `off_t` (which is signed).

Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

LibOS/shim/src/bookkeep/shim_handle.c
@@ -474,21 +474,33 @@ void put_handle(struct shim_handle* hdl) {
     }
 }
 
-off_t get_file_size(struct shim_handle* hdl) {
+int get_file_size(struct shim_handle* hdl, size_t* size) {
     if (!hdl->fs || !hdl->fs->fs_ops)
         return -EINVAL;
 
-    if (hdl->fs->fs_ops->poll)
-        return hdl->fs->fs_ops->poll(hdl, FS_POLL_SZ);
+    if (hdl->fs->fs_ops->poll) {
+        off_t x = hdl->fs->fs_ops->poll(hdl, FS_POLL_SZ);
+        if (x < 0) {
+            return -EINVAL;
+        }
+        *size = (size_t)x;
+        return 0;
+    }
 
     if (hdl->fs->fs_ops->hstat) {
         struct stat stat;
         int ret = hdl->fs->fs_ops->hstat(hdl, &stat);
-        if (ret < 0)
+        if (ret < 0) {
             return ret;
-        return stat.st_size;
+        }
+        if (stat.st_size < 0) {
+            return -EINVAL;
+        }
+        *size = (size_t)stat.st_size;
+        return 0;
     }
 
+    *size = 0;
     return 0;
 }
 

LibOS/shim/src/fs/chroot/fs.c
@@ -691,7 +691,7 @@ out:
 }
 
 static int chroot_mmap(struct shim_handle* hdl, void** addr, size_t size, int prot, int flags,
-                       off_t offset) {
+                       size_t offset) {
     int ret;
     if (NEED_RECREATE(hdl) && (ret = chroot_recreate(hdl)) < 0)
         return ret;

gramineproject/gramine

LibOS/shim/src/bookkeep/shim_handle.c
@@ -474,21 +474,33 @@ void put_handle(struct shim_handle* hdl) {
     }
 }
 
-off_t get_file_size(struct shim_handle* hdl) {
+int get_file_size(struct shim_handle* hdl, uint64_t* size) {
     if (!hdl->fs || !hdl->fs->fs_ops)
         return -EINVAL;
 
-    if (hdl->fs->fs_ops->poll)
-        return hdl->fs->fs_ops->poll(hdl, FS_POLL_SZ);
+    if (hdl->fs->fs_ops->poll) {
+        off_t x = hdl->fs->fs_ops->poll(hdl, FS_POLL_SZ);
+        if (x < 0) {
+            return -EINVAL;
+        }
+        *size = (uint64_t)x;
+        return 0;
+    }
 
     if (hdl->fs->fs_ops->hstat) {
         struct stat stat;
         int ret = hdl->fs->fs_ops->hstat(hdl, &stat);
-        if (ret < 0)
+        if (ret < 0) {
             return ret;
-        return stat.st_size;
+        }
+        if (stat.st_size < 0) {
+            return -EINVAL;
+        }
+        *size = (uint64_t)stat.st_size;
+        return 0;
     }
 
+    *size = 0;
     return 0;
 }
 

LibOS/shim/src/fs/chroot/fs.c
@@ -691,7 +691,7 @@ out:
 }
 
 static int chroot_mmap(struct shim_handle* hdl, void** addr, size_t size, int prot, int flags,
-                       off_t offset) {
+                       uint64_t offset) {
     int ret;
     if (NEED_RECREATE(hdl) && (ret = chroot_recreate(hdl)) < 0)
         return ret;