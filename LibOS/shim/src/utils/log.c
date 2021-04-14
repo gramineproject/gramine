gramineproject/graphene

[Pal,LibOS] Make the logging system always flush data
This makes sure the behaviour of logging functions is consistent,
but that they still output whole lines with a single syscall
whenever possible.

The syscall trace (shim_parser.c), which depended on the buffering
mechanism in LibOS, is converted to use its own buffer. As a result,
we no longer need to keep a per-thread log buffer, just log prefix
for a given thread.

Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

LibOS/shim/src/utils/log.c
@@ -25,29 +25,9 @@ static const char* log_level_to_prefix[] = {
     [PAL_LOG_ALL]     = "", // same as for PAL_LOG_NONE
 };
 
-static int log_one_char(void* f, int ch, void* buf_) {
-    __UNUSED(f);
-    int ret = 0;
-    struct log_buf* buf = (struct log_buf*)buf_;
-
-    buf->buf[buf->end++] = ch;
-
-    if (ch == '\n' || buf->end == LOG_BUF_SIZE) {
-        ret = DkDebugLog((void*)buf->buf, buf->end);
-        if (ret < 0)
-            ret = pal_to_unix_errno(ret);
-        buf->end = buf->start;
-    }
-
-    return ret;
-}
-
 void log_setprefix(shim_tcb_t* tcb) {
     if (g_log_level <= PAL_LOG_NONE)
         return;
-
-    struct log_buf* buf = tcb->log_buf;
-    buf->start = buf->end = 0;
 
     const char* exec = PAL_CB(executable);
     for (const char* it = exec; *it; it++)
@@ -58,44 +38,53 @@ void log_setprefix(shim_tcb_t* tcb) {
     if (tcb->tp) {
         if (!is_internal_tid(tcb->tp->tid)) {
             /* normal app thread: show Process ID, Thread ID, and exec name */
-            fprintfmt(log_one_char, NULL, buf, "[P%u:T%u:%s] ", vmid, tcb->tp->tid, exec);
+            snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[P%u:T%u:%s] ", vmid,
+                     tcb->tp->tid, exec);
         } else {
             /* internal LibOS thread: show Process ID, Internal-thread ID, and exec name */
-            fprintfmt(log_one_char, NULL, buf, "[P%u:i%u:%s] ", vmid,
+            snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[P%u:i%u:%s] ", vmid,
                       tcb->tp->tid - INTERNAL_TID_BASE, exec);
         }
     } else if (g_process_ipc_info.vmid) {
         /* unknown thread (happens on process init): show Process ID and exec name */
-        fprintfmt(log_one_char, NULL, buf, "[P%u:%s] ", vmid, exec);
+        snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[P%u:%s] ", vmid, exec);
     } else {
         /* unknown process (must never happen): show exec name */
-        fprintfmt(log_one_char, NULL, buf, "[%s] ", exec);
+        snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[%s] ", exec);
     }
-
-    buf->start = buf->end;
 }
 
-static void log_vprintf(const char* fmt, va_list ap) {
-    vfprintfmt(log_one_char, NULL, shim_get_tcb()->log_buf, fmt, ap);
+static int write_all(const char* str, size_t size, void* arg) {
+    __UNUSED(arg);
+    DkDebugLog((PAL_PTR)str, size);
+    return 0;
 }
 
 void _log(int level, const char* fmt, ...) {
     if (level <= g_log_level) {
+        struct print_buf buf = INIT_PRINT_BUF(write_all);
+
+        buf_puts(&buf, shim_get_tcb()->log_prefix);
+        buf_puts(&buf, log_level_to_prefix[level]);
+
         va_list ap;
         va_start(ap, fmt);
-        // prepend prefix only on line starts
-        if (shim_get_tcb()->log_buf->end == shim_get_tcb()->log_buf->start) {
-            assert(0 <= level && (size_t)level < ARRAY_SIZE(log_level_to_prefix));
-            log_always("%s", log_level_to_prefix[level]);
-        }
-        log_vprintf(fmt, ap);
+        buf_vprintf(&buf, fmt, ap);
         va_end(ap);
+
+        buf_flush(&buf);
     }
 }
 
 void log_always(const char* fmt, ...) {
+    struct print_buf buf = INIT_PRINT_BUF(write_all);
+
+    buf_puts(&buf, shim_get_tcb()->log_prefix);
+
     va_list ap;
     va_start(ap, fmt);
-    log_vprintf(fmt, ap);
+    buf_vprintf(&buf, fmt, ap);
     va_end(ap);
+
+    buf_flush(&buf);
 }

Pal/src/printf.c
@@ -3,15 +3,8 @@
 
 #include "api.h"
 #include "assert.h"
+#include "pal_debug.h"
 #include "pal_internal.h"
-
-// Collect up to PRINTBUF_SIZE characters into a buffer
-// and perform ONE system call to print all of them,
-// in order to make the lines output to the console atomic
-// and prevent interrupts from causing context switches
-// in the middle of a console output line and such.
-
-#define PRINTBUF_SIZE 256
 
 static const char* log_level_to_prefix[] = {
     [PAL_LOG_NONE]    = "", // not a valid entry actually (no public wrapper uses this log level)
@@ -22,67 +15,41 @@ static const char* log_level_to_prefix[] = {
     [PAL_LOG_ALL]     = "", // same as for PAL_LOG_NONE
 };
 
-struct printbuf {
-    size_t idx;  // current buffer index
-    size_t cnt;  // total bytes printed so far
-    char buf[PRINTBUF_SIZE];
-};
-
-static int fputch(void* f, int ch, void* buf_) {
-    __UNUSED(f);
-    struct printbuf* buf = buf_;
-
-    buf->buf[buf->idx++] = ch;
-    if (buf->idx == PRINTBUF_SIZE - 1) {
-        _DkPrintConsole(buf->buf, buf->idx);
-        buf->idx = 0;
-    }
-    buf->cnt++;
+static int write_all(const char* str, size_t size, void* arg) {
+    __UNUSED(arg);
+    _DkDebugLog(str, size);
     return 0;
 }
 
-// TODO: Remove this and always use log_*.
-__attribute__((format(printf, 1, 0)))
-int vprintf(const char* fmt, va_list ap) {
-    struct printbuf buf;
+static void log_vprintf(const char* prefix, const char* fmt, va_list ap) {
+    struct print_buf buf = INIT_PRINT_BUF(write_all);
 
-    buf.idx = 0;
-    buf.cnt = 0;
-    vfprintfmt(fputch, NULL, &buf, fmt, ap);
-    _DkPrintConsole(buf.buf, buf.idx);
-
-    return buf.cnt;
+    if (prefix)
+        buf_puts(&buf, prefix);
+    buf_vprintf(&buf, fmt, ap);
+    buf_flush(&buf);
 }
 
-static void log_vprintf(const char* fmt, va_list ap) {
-    struct printbuf buf;
-
-    buf.idx = 0;
-    buf.cnt = 0;
-    vfprintfmt(fputch, NULL, &buf, fmt, ap);
-    _DkDebugLog(buf.buf, buf.idx);
-}
-
-// TODO: Make this static and always use log_* outside of this file.
-int printf(const char* fmt, ...) {
+// TODO: Replace this with log_* everywhere
+void pal_printf(const char* fmt, ...) {
     va_list ap;
-    int cnt;
 
     va_start(ap, fmt);
-    cnt = vprintf(fmt, ap);
+    log_vprintf(/*prefix=*/NULL, fmt, ap);
     va_end(ap);
-
-    return cnt;
 }
-EXTERN_ALIAS(printf);
+
+// TODO: Replace this with log_* everywhere
+void pal_vprintf(const char* fmt, va_list ap) {
+    return log_vprintf(/*prefix=*/NULL, fmt, ap);
+}
 
 void _log(int level, const char* fmt, ...) {
     if (level <= g_pal_control.log_level) {
         va_list ap;
         va_start(ap, fmt);
         assert(0 <= level && (size_t)level < ARRAY_SIZE(log_level_to_prefix));
-        log_always("%s", log_level_to_prefix[level]);
-        log_vprintf(fmt, ap);
+        log_vprintf(log_level_to_prefix[level], fmt, ap);
         va_end(ap);
     }
 }
@@ -90,6 +57,6 @@ void _log(int level, const char* fmt, ...) {
 void log_always(const char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    log_vprintf(fmt, ap);
+    log_vprintf(/*prefix=*/NULL, fmt, ap);
     va_end(ap);
 }

gramineproject/gramine

LibOS/shim/src/utils/log.c
@@ -25,29 +25,9 @@ static const char* log_level_to_prefix[] = {
     [PAL_LOG_ALL]     = "", // same as for PAL_LOG_NONE
 };
 
-static int log_one_char(void* f, int ch, void* buf_) {
-    __UNUSED(f);
-    int ret = 0;
-    struct log_buf* buf = (struct log_buf*)buf_;
-
-    buf->buf[buf->end++] = ch;
-
-    if (ch == '\n' || buf->end == LOG_BUF_SIZE) {
-        ret = DkDebugLog((void*)buf->buf, buf->end);
-        if (ret < 0)
-            ret = pal_to_unix_errno(ret);
-        buf->end = buf->start;
-    }
-
-    return ret;
-}
-
 void log_setprefix(shim_tcb_t* tcb) {
     if (g_log_level <= PAL_LOG_NONE)
         return;
-
-    struct log_buf* buf = tcb->log_buf;
-    buf->start = buf->end = 0;
 
     const char* exec = PAL_CB(executable);
     for (const char* it = exec; *it; it++)
@@ -58,44 +38,53 @@ void log_setprefix(shim_tcb_t* tcb) {
     if (tcb->tp) {
         if (!is_internal_tid(tcb->tp->tid)) {
             /* normal app thread: show Process ID, Thread ID, and exec name */
-            fprintfmt(log_one_char, NULL, buf, "[P%u:T%u:%s] ", vmid, tcb->tp->tid, exec);
+            snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[P%u:T%u:%s] ", vmid,
+                     tcb->tp->tid, exec);
         } else {
             /* internal LibOS thread: show Process ID, Internal-thread ID, and exec name */
-            fprintfmt(log_one_char, NULL, buf, "[P%u:i%u:%s] ", vmid,
-                      tcb->tp->tid - INTERNAL_TID_BASE, exec);
+            snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[P%u:i%u:%s] ", vmid,
+                     tcb->tp->tid - INTERNAL_TID_BASE, exec);
         }
     } else if (g_process_ipc_info.vmid) {
         /* unknown thread (happens on process init): show Process ID and exec name */
-        fprintfmt(log_one_char, NULL, buf, "[P%u:%s] ", vmid, exec);
+        snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[P%u:%s] ", vmid, exec);
     } else {
         /* unknown process (must never happen): show exec name */
-        fprintfmt(log_one_char, NULL, buf, "[%s] ", exec);
+        snprintf(tcb->log_prefix, ARRAY_SIZE(tcb->log_prefix), "[%s] ", exec);
     }
-
-    buf->start = buf->end;
 }
 
-static void log_vprintf(const char* fmt, va_list ap) {
-    vfprintfmt(log_one_char, NULL, shim_get_tcb()->log_buf, fmt, ap);
+static int buf_write_all(const char* str, size_t size, void* arg) {
+    __UNUSED(arg);
+    DkDebugLog((PAL_PTR)str, size);
+    return 0;
 }
 
 void _log(int level, const char* fmt, ...) {
     if (level <= g_log_level) {
+        struct print_buf buf = INIT_PRINT_BUF(buf_write_all);
+
+        buf_puts(&buf, shim_get_tcb()->log_prefix);
+        buf_puts(&buf, log_level_to_prefix[level]);
+
         va_list ap;
         va_start(ap, fmt);
-        // prepend prefix only on line starts
-        if (shim_get_tcb()->log_buf->end == shim_get_tcb()->log_buf->start) {
-            assert(0 <= level && (size_t)level < ARRAY_SIZE(log_level_to_prefix));
-            log_always("%s", log_level_to_prefix[level]);
-        }
-        log_vprintf(fmt, ap);
+        buf_vprintf(&buf, fmt, ap);
         va_end(ap);
+
+        buf_flush(&buf);
     }
 }
 
 void log_always(const char* fmt, ...) {
+    struct print_buf buf = INIT_PRINT_BUF(buf_write_all);
+
+    buf_puts(&buf, shim_get_tcb()->log_prefix);
+
     va_list ap;
     va_start(ap, fmt);
-    log_vprintf(fmt, ap);
+    buf_vprintf(&buf, fmt, ap);
     va_end(ap);
+
+    buf_flush(&buf);
 }

Pal/src/printf.c
@@ -3,15 +3,8 @@
 
 #include "api.h"
 #include "assert.h"
+#include "pal_debug.h"
 #include "pal_internal.h"
-
-// Collect up to PRINTBUF_SIZE characters into a buffer
-// and perform ONE system call to print all of them,
-// in order to make the lines output to the console atomic
-// and prevent interrupts from causing context switches
-// in the middle of a console output line and such.
-
-#define PRINTBUF_SIZE 256
 
 static const char* log_level_to_prefix[] = {
     [PAL_LOG_NONE]    = "", // not a valid entry actually (no public wrapper uses this log level)
@@ -22,67 +15,41 @@ static const char* log_level_to_prefix[] = {
     [PAL_LOG_ALL]     = "", // same as for PAL_LOG_NONE
 };
 
-struct printbuf {
-    size_t idx;  // current buffer index
-    size_t cnt;  // total bytes printed so far
-    char buf[PRINTBUF_SIZE];
-};
-
-static int fputch(void* f, int ch, void* buf_) {
-    __UNUSED(f);
-    struct printbuf* buf = buf_;
-
-    buf->buf[buf->idx++] = ch;
-    if (buf->idx == PRINTBUF_SIZE - 1) {
-        _DkPrintConsole(buf->buf, buf->idx);
-        buf->idx = 0;
-    }
-    buf->cnt++;
+static int buf_write_all(const char* str, size_t size, void* arg) {
+    __UNUSED(arg);
+    _DkDebugLog(str, size);
     return 0;
 }
 
-// TODO: Remove this and always use log_*.
-__attribute__((format(printf, 1, 0)))
-int vprintf(const char* fmt, va_list ap) {
-    struct printbuf buf;
+static void log_vprintf(const char* prefix, const char* fmt, va_list ap) {
+    struct print_buf buf = INIT_PRINT_BUF(buf_write_all);
 
-    buf.idx = 0;
-    buf.cnt = 0;
-    vfprintfmt(fputch, NULL, &buf, fmt, ap);
-    _DkPrintConsole(buf.buf, buf.idx);
-
-    return buf.cnt;
+    if (prefix)
+        buf_puts(&buf, prefix);
+    buf_vprintf(&buf, fmt, ap);
+    buf_flush(&buf);
 }
 
-static void log_vprintf(const char* fmt, va_list ap) {
-    struct printbuf buf;
-
-    buf.idx = 0;
-    buf.cnt = 0;
-    vfprintfmt(fputch, NULL, &buf, fmt, ap);
-    _DkDebugLog(buf.buf, buf.idx);
-}
-
-// TODO: Make this static and always use log_* outside of this file.
-int printf(const char* fmt, ...) {
+// TODO: Replace this with log_* everywhere
+void pal_printf(const char* fmt, ...) {
     va_list ap;
-    int cnt;
 
     va_start(ap, fmt);
-    cnt = vprintf(fmt, ap);
+    log_vprintf(/*prefix=*/NULL, fmt, ap);
     va_end(ap);
-
-    return cnt;
 }
-EXTERN_ALIAS(printf);
+
+// TODO: Replace this with log_* everywhere
+void pal_vprintf(const char* fmt, va_list ap) {
+    return log_vprintf(/*prefix=*/NULL, fmt, ap);
+}
 
 void _log(int level, const char* fmt, ...) {
     if (level <= g_pal_control.log_level) {
         va_list ap;
         va_start(ap, fmt);
         assert(0 <= level && (size_t)level < ARRAY_SIZE(log_level_to_prefix));
-        log_always("%s", log_level_to_prefix[level]);
-        log_vprintf(fmt, ap);
+        log_vprintf(log_level_to_prefix[level], fmt, ap);
         va_end(ap);
     }
 }
@@ -90,6 +57,6 @@ void _log(int level, const char* fmt, ...) {
 void log_always(const char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    log_vprintf(fmt, ap);
+    log_vprintf(/*prefix=*/NULL, fmt, ap);
     va_end(ap);
 }