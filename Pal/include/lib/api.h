gramineproject/graphene

[Pal] Rework DkEvent* functions
They were poorly written, did not provide the expected and documented
semantics and were bugged: e.g. on SGX PAL malicious host could spoof
signaling of an event.

Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

Pal/include/lib/api.h
@@ -367,6 +367,9 @@ int toml_sizestring_in(const toml_table_t* root, const char* key, uint64_t defau
 
 #define URI_PREFIX_FILE_LEN (static_strlen(URI_PREFIX_FILE))
 
+#define US_IN_S 1000000ul
+#define NS_IN_US 1000ul
+
 #ifdef __x86_64__
 static inline bool __range_not_ok(uintptr_t addr, size_t size) {
     addr += size;

Pal/regression/Event.c
@@ -1,79 +1,98 @@
-#include <stdatomic.h>
-#include <stdbool.h>
-
+#include "api.h"
+#include "cpu.h"
 #include "pal.h"
 #include "pal_debug.h"
 #include "pal_error.h"
 
-static PAL_HANDLE event1;
-atomic_int timeouts = 0;
+#define CHECK(x) ({                                                     \
+    __typeof__(x) _x = (x);                                             \
+    if (_x < 0) {                                                       \
+        pal_printf("Error at line %u, pal_errno: %d\n", __LINE__, _x);  \
+        DkProcessExit(1);                                               \
+    }                                                                   \
+    _x;                                                                 \
+})
 
-static int thread2_run(void* args) {
-    pal_printf("Second thread started.\n");
-    DkThreadDelayExecution(3000000);
+static void wait_for(int* ptr, int val) {
+    while (__atomic_load_n(ptr, __ATOMIC_ACQUIRE) != val) {
+        CPU_RELAX();
+    }
+}
 
-    pal_printf("Sending event...\n");
-    DkEventSet(event1);
-    pal_printf("End of second thread.\n");
-    DkThreadExit(/*clear_child_tid=*/NULL);
-    /* UNREACHABLE */
+static void set(int* ptr, int val) {
+    __atomic_store_n(ptr, val, __ATOMIC_RELEASE);
+}
+
+static int g_clear_thread_exit = 1;
+static int g_ready = 0;
+
+static void thread_func(void* arg) {
+    PAL_HANDLE event = (PAL_HANDLE)arg;
+    set(&g_ready, 1);
+    wait_for(&g_ready, 2);
+
+    if (DkThreadDelayExecution(US_IN_S) != US_IN_S) {
+        pal_printf("Error: unexpected short sleep\n");
+        DkProcessExit(1);
+    }
+
+    DkEventSet(event);
+
+    DkThreadExit(&g_clear_thread_exit);
 }
 
 int main(void) {
-    pal_printf("Started main thread.\n");
+    PAL_HANDLE event = NULL;
+    CHECK(DkEventCreate(&event, /*init_signaled=*/true, /*auto_clear=*/true));
 
-    int ret = DkNotificationEventCreate(0, &event1);
-    if (ret < 0 || event1 == NULL) {
-        pal_printf("DkNotificationEventCreate failed\n");
-        return 1;
-    }
+    /* Event is already set, should not sleep. */
+    CHECK(DkSynchronizationObjectWait(event, NO_TIMEOUT));
 
-    PAL_HANDLE thread2 = NULL;
-    ret = DkThreadCreate(thread2_run, NULL, &thread2);
-    if (ret < 0) {
-        pal_printf("DkThreadCreate failed\n");
-        return 1;
+    uint64_t start = 0;
+    CHECK(DkSystemTimeQuery(&start));
+    /* Sleep for one second. */
+    int ret = DkSynchronizationObjectWait(event, US_IN_S);
+    if (ret != -PAL_ERROR_TRYAGAIN) {
+        CHECK(-1);
     }
-    uint64_t t_start = 0;
-    if (DkSystemTimeQuery(&t_start) < 0) {
-        pal_printf("DkSystemTimeQuery failed\n");
-        return 1;
-    }
+    uint64_t end = 0;
+    CHECK(DkSystemTimeQuery(&end));
 
-    pal_printf("Testing wait with too short timeout...\n");
-    ret = DkSynchronizationObjectWait(event1, 1000000);
-    if (ret == -PAL_ERROR_TRYAGAIN) {
-        timeouts++;
+    if (end < start) {
+        CHECK(-1);
     }
-    uint64_t t_wait1 = 0;
-    if (DkSystemTimeQuery(&t_wait1) < 0) {
-        pal_printf("DkSystemTimeQuery failed\n");
-        return 1;
+    if (end - start < US_IN_S) {
+        CHECK(-1);
     }
-    uint64_t dt_wait1 = t_wait1 - t_start;
-    pal_printf("Wait returned after %lu us.\n", dt_wait1);
-    pal_printf("Timeout count: %d\n", timeouts);
-    if (dt_wait1 > 1000000 && dt_wait1 < 1100000 && timeouts == 1) {
-        pal_printf("Wait with too short timeout ok.\n");
+    if (end - start > US_IN_S * 3 / 2) {
+        CHECK(-1);
     }
 
-    pal_printf("Testing wait with long enough timeout...\n");
-    ret = DkSynchronizationObjectWait(event1, 5000000);
-    if (ret == -PAL_ERROR_TRYAGAIN) {
-        timeouts++;
+    PAL_HANDLE thread = NULL;
+    CHECK(DkThreadCreate(thread_func, event, &thread));
+
+    wait_for(&g_ready, 1);
+    set(&g_ready, 2);
+
+    CHECK(DkSystemTimeQuery(&start));
+    DkSynchronizationObjectWait(event, NO_TIMEOUT);
+    if (ret != -PAL_ERROR_TRYAGAIN) {
+        CHECK(-1);
     }
-    uint64_t t_wait2 = 0;
-    if (DkSystemTimeQuery(&t_wait2) < 0) {
-        pal_printf("DkSystemTimeQuery failed\n");
-        return 1;
+    CHECK(DkSystemTimeQuery(&end));
+
+    if (end < start) {
+        CHECK(-1);
     }
-    uint64_t dt_wait2 = t_wait2 - t_start;
-    pal_printf("Wait returned after %lu us since start.\n", dt_wait2);
-    pal_printf("Timeout count: %d\n", timeouts);
-    if (dt_wait2 > 3000000 && dt_wait2 < 3100000 && timeouts == 1) {
-        pal_printf("Wait with long enough timeout ok.\n");
+    if (end - start < US_IN_S) {
+        CHECK(-1);
+    }
+    if (end - start > US_IN_S * 3 / 2) {
+        CHECK(-1);
     }
 
-    pal_printf("End of main thread.\n");
+    wait_for(&g_clear_thread_exit, 0);
+
+    pal_printf("TEST OK\n");
     return 0;
 }

Pal/src/host/Linux/db_events.c
@@ -1,120 +1,75 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
-/* Copyright (C) 2014 Stony Brook University */
-
-/*
- * This file contains implementation of Drawbridge event synchronization APIs.
+/* Copyright (C) 2021 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
  */
 
 #include <asm/errno.h>
+#include <limits.h>
 #include <linux/futex.h>
 #include <linux/time.h>
+#include <stdbool.h>
 
 #include "api.h"
-#include "atomic.h"
+#include "assert.h"
 #include "pal.h"
-#include "pal_debug.h"
-#include "pal_defs.h"
-#include "pal_error.h"
 #include "pal_internal.h"
-#include "pal_linux.h"
-#include "pal_linux_defs.h"
+#include "pal_linux_error.h"
 
-int _DkEventCreate(PAL_HANDLE* event, bool initialState, bool isnotification) {
-    PAL_HANDLE ev = malloc(HANDLE_SIZE(event));
-    SET_HANDLE_TYPE(ev, event);
-    ev->event.isnotification = isnotification;
-    __atomic_store_n(&ev->event.nwaiters.counter, 0, __ATOMIC_SEQ_CST);
-    __atomic_store_n(&ev->event.signaled, initialState ? 1 : 0, __ATOMIC_SEQ_CST);
-    *event = ev;
+int _DkEventCreate(PAL_HANDLE* handle_ptr, bool init_signaled, bool auto_clear) {
+    PAL_HANDLE handle = malloc(HANDLE_SIZE(event));
+    if (!handle) {
+        return -PAL_ERROR_NOMEM;
+    }
+
+    SET_HANDLE_TYPE(handle, event);
+    handle->event.auto_clear = auto_clear;
+    __atomic_store_n(&handle->event.signaled, init_signaled ? 1 : 0, __ATOMIC_RELEASE);
+
+    *handle_ptr = handle;
     return 0;
 }
 
-int _DkEventSet(PAL_HANDLE event, int wakeup) {
-    int ret = 0;
-
-    if (event->event.isnotification) {
-        // Leave it signaled, wake all
-        uint32_t t = 0;
-        if (__atomic_compare_exchange_n(&event->event.signaled, &t, 1, /*weak=*/false,
-                                        __ATOMIC_ACQUIRE, __ATOMIC_RELAXED)) {
-            int nwaiters = __atomic_load_n(&event->event.nwaiters.counter, __ATOMIC_SEQ_CST);
-            if (nwaiters) {
-                if (wakeup != -1 && nwaiters > wakeup)
-                    nwaiters = wakeup;
-
-                ret = INLINE_SYSCALL(futex, 6, &event->event.signaled, FUTEX_WAKE, nwaiters, NULL,
-                                     NULL, 0);
-                if (ret < 0)
-                    __atomic_store_n(&event->event.signaled, 0, __ATOMIC_SEQ_CST);
-            }
-        }
-    } else {
-        // Only one thread wakes up, leave unsignaled
-        ret = INLINE_SYSCALL(futex, 6, &event->event.signaled, FUTEX_WAKE, 1, NULL, NULL, 0);
-    }
-
-    return ret < 0 ? -PAL_ERROR_TRYAGAIN : ret;
+void _DkEventSet(PAL_HANDLE handle) {
+    __atomic_store_n(&handle->event.signaled, 1, __ATOMIC_RELEASE);
+    int ret = INLINE_SYSCALL(futex, 6, &handle->event.signaled, FUTEX_WAKE,
+                             handle->event.auto_clear ? 1 : INT_MAX, NULL, NULL, 0);
+    __UNUSED(ret);
+    /* This `FUTEX_WAKE` cannot really fail. */
+    assert(ret >= 0);
 }
 
-int _DkEventWaitTimeout(PAL_HANDLE event, int64_t timeout_us) {
-    int ret = 0;
+void _DkEventClear(PAL_HANDLE handle) {
+    __atomic_store_n(&handle->event.signaled, 0, __ATOMIC_RELEASE);
+}
 
-    struct timespec waittime;
-    struct timespec* waittime_ptr = NULL;
+static int wait_timeout(PAL_HANDLE handle, int64_t timeout_us) {
+    struct timespec timeout = { 0 };
+    struct timespec* timeout_ptr = NULL;
     if (timeout_us >= 0) {
-        int64_t sec      = timeout_us / 1000000UL;
-        int64_t microsec = timeout_us - (sec * 1000000UL);
-        waittime.tv_sec  = sec;
-        waittime.tv_nsec = microsec * 1000;
-        waittime_ptr = &waittime;
+        timeout.tv_sec = timeout_us / US_IN_S;
+        timeout.tv_nsec = (timeout_us - timeout.tv_sec * US_IN_S) * NS_IN_US;
+        timeout_ptr = &timeout;
     }
 
-    if (!event->event.isnotification ||
-        !__atomic_load_n(&event->event.signaled, __ATOMIC_SEQ_CST)) {
+    while (1) {
+        bool needs_sleep = false;
+        if (handle->event.auto_clear) {
+            needs_sleep = __atomic_exchange_n(&handle->event.signaled, 0, __ATOMIC_ACQ_REL) == 0;
+        } else {
+            needs_sleep = __atomic_load_n(&handle->event.signaled, __ATOMIC_ACQUIRE) == 0;
+        }
 
-        __atomic_add_fetch(&event->event.nwaiters.counter, 1, __ATOMIC_SEQ_CST);
-
-        do {
-            ret = INLINE_SYSCALL(futex, 6, &event->event.signaled, FUTEX_WAIT, 0, waittime_ptr,
+        if (!needs_sleep) {
+            return 0;
+        }
+        int ret = INLINE_SYSCALL(futex, 6, &handle->event.signaled, FUTEX_WAIT, 0, timeout_ptr,
                                  NULL, 0);
-
-            if (ret < 0) {
-                if (ret == -EWOULDBLOCK) {
-                    ret = 0;
-                } else if (ret == -EINTR
-                           && (!event->event.isnotification
-                               || __atomic_load_n(&event->event.signaled, __ATOMIC_SEQ_CST))) {
-                    ret = 0;
-                    break;
-                } else {
-                    ret = unix_to_pal_error(ret);
-                    break;
-                }
-            }
-        } while (event->event.isnotification &&
-                 !__atomic_load_n(&event->event.signaled, __ATOMIC_SEQ_CST));
-
-        __atomic_sub_fetch(&event->event.nwaiters.counter, 1, __ATOMIC_SEQ_CST);
+        if (ret < 0 && ret != -EAGAIN) {
+            return unix_to_pal_error(ret);
+        }
     }
-
-    return ret;
-}
-
-int _DkEventClear(PAL_HANDLE event) {
-    __atomic_store_n(&event->event.signaled, 0, __ATOMIC_SEQ_CST);
-    return 0;
-}
-
-static int event_close(PAL_HANDLE handle) {
-    _DkEventSet(handle, -1);
-    return 0;
-}
-
-static int event_wait(PAL_HANDLE handle, int64_t timeout_us) {
-    return _DkEventWaitTimeout(handle, timeout_us);
 }
 
 struct handle_ops g_event_ops = {
-    .close = &event_close,
-    .wait  = &event_wait,
+    .wait = wait_timeout,
 };

gramineproject/gramine

Pal/include/lib/api.h
@@ -367,6 +367,9 @@ int toml_sizestring_in(const toml_table_t* root, const char* key, uint64_t defau
 
 #define URI_PREFIX_FILE_LEN (static_strlen(URI_PREFIX_FILE))
 
+#define TIME_US_IN_S 1000000ul
+#define TIME_NS_IN_US 1000ul
+
 #ifdef __x86_64__
 static inline bool __range_not_ok(uintptr_t addr, size_t size) {
     addr += size;

Pal/regression/Event.c
@@ -1,79 +1,95 @@
-#include <stdatomic.h>
-#include <stdbool.h>
-
+#include "api.h"
+#include "cpu.h"
 #include "pal.h"
 #include "pal_debug.h"
 #include "pal_error.h"
 
-static PAL_HANDLE event1;
-atomic_int timeouts = 0;
+#define CHECK(x) ({                                                     \
+    __typeof__(x) _x = (x);                                             \
+    if (_x < 0) {                                                       \
+        pal_printf("Error at line %u, pal_errno: %d\n", __LINE__, _x);  \
+        DkProcessExit(1);                                               \
+    }                                                                   \
+    _x;                                                                 \
+})
 
-static int thread2_run(void* args) {
-    pal_printf("Second thread started.\n");
-    DkThreadDelayExecution(3000000);
+static void wait_for(int* ptr, int val) {
+    while (__atomic_load_n(ptr, __ATOMIC_ACQUIRE) != val) {
+        CPU_RELAX();
+    }
+}
 
-    pal_printf("Sending event...\n");
-    DkEventSet(event1);
-    pal_printf("End of second thread.\n");
-    DkThreadExit(/*clear_child_tid=*/NULL);
-    /* UNREACHABLE */
+static void set(int* ptr, int val) {
+    __atomic_store_n(ptr, val, __ATOMIC_RELEASE);
+}
+
+static int g_clear_thread_exit = 1;
+static int g_ready = 0;
+
+static void thread_func(void* arg) {
+    PAL_HANDLE event = (PAL_HANDLE)arg;
+    set(&g_ready, 1);
+    wait_for(&g_ready, 2);
+
+    if (DkThreadDelayExecution(TIME_US_IN_S) != TIME_US_IN_S) {
+        pal_printf("Error: unexpected short sleep\n");
+        DkProcessExit(1);
+    }
+
+    DkEventSet(event);
+
+    DkThreadExit(&g_clear_thread_exit);
 }
 
 int main(void) {
-    pal_printf("Started main thread.\n");
+    PAL_HANDLE event = NULL;
+    CHECK(DkEventCreate(&event, /*init_signaled=*/true, /*auto_clear=*/true));
 
-    int ret = DkNotificationEventCreate(0, &event1);
-    if (ret < 0 || event1 == NULL) {
-        pal_printf("DkNotificationEventCreate failed\n");
-        return 1;
-    }
+    /* Event is already set, should not sleep. */
+    CHECK(DkSynchronizationObjectWait(event, NO_TIMEOUT));
 
-    PAL_HANDLE thread2 = NULL;
-    ret = DkThreadCreate(thread2_run, NULL, &thread2);
-    if (ret < 0) {
-        pal_printf("DkThreadCreate failed\n");
-        return 1;
+    uint64_t start = 0;
+    CHECK(DkSystemTimeQuery(&start));
+    /* Sleep for one second. */
+    int ret = DkSynchronizationObjectWait(event, TIME_US_IN_S);
+    if (ret != -PAL_ERROR_TRYAGAIN) {
+        CHECK(-1);
     }
-    uint64_t t_start = 0;
-    if (DkSystemTimeQuery(&t_start) < 0) {
-        pal_printf("DkSystemTimeQuery failed\n");
-        return 1;
-    }
+    uint64_t end = 0;
+    CHECK(DkSystemTimeQuery(&end));
 
-    pal_printf("Testing wait with too short timeout...\n");
-    ret = DkSynchronizationObjectWait(event1, 1000000);
-    if (ret == -PAL_ERROR_TRYAGAIN) {
-        timeouts++;
+    if (end < start) {
+        CHECK(-1);
     }
-    uint64_t t_wait1 = 0;
-    if (DkSystemTimeQuery(&t_wait1) < 0) {
-        pal_printf("DkSystemTimeQuery failed\n");
-        return 1;
+    if (end - start < TIME_US_IN_S) {
+        CHECK(-1);
     }
-    uint64_t dt_wait1 = t_wait1 - t_start;
-    pal_printf("Wait returned after %lu us.\n", dt_wait1);
-    pal_printf("Timeout count: %d\n", timeouts);
-    if (dt_wait1 > 1000000 && dt_wait1 < 1100000 && timeouts == 1) {
-        pal_printf("Wait with too short timeout ok.\n");
+    if (end - start > TIME_US_IN_S * 3 / 2) {
+        CHECK(-1);
     }
 
-    pal_printf("Testing wait with long enough timeout...\n");
-    ret = DkSynchronizationObjectWait(event1, 5000000);
-    if (ret == -PAL_ERROR_TRYAGAIN) {
-        timeouts++;
+    PAL_HANDLE thread = NULL;
+    CHECK(DkThreadCreate(thread_func, event, &thread));
+
+    wait_for(&g_ready, 1);
+    set(&g_ready, 2);
+
+    CHECK(DkSystemTimeQuery(&start));
+    CHECK(DkSynchronizationObjectWait(event, NO_TIMEOUT));
+    CHECK(DkSystemTimeQuery(&end));
+
+    if (end < start) {
+        CHECK(-1);
     }
-    uint64_t t_wait2 = 0;
-    if (DkSystemTimeQuery(&t_wait2) < 0) {
-        pal_printf("DkSystemTimeQuery failed\n");
-        return 1;
+    if (end - start < TIME_US_IN_S) {
+        CHECK(-1);
     }
-    uint64_t dt_wait2 = t_wait2 - t_start;
-    pal_printf("Wait returned after %lu us since start.\n", dt_wait2);
-    pal_printf("Timeout count: %d\n", timeouts);
-    if (dt_wait2 > 3000000 && dt_wait2 < 3100000 && timeouts == 1) {
-        pal_printf("Wait with long enough timeout ok.\n");
+    if (end - start > TIME_US_IN_S * 3 / 2) {
+        CHECK(-1);
     }
 
-    pal_printf("End of main thread.\n");
+    wait_for(&g_clear_thread_exit, 0);
+
+    pal_printf("TEST OK\n");
     return 0;
 }

Pal/src/host/Linux/db_events.c
@@ -1,120 +1,76 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
-/* Copyright (C) 2014 Stony Brook University */
-
-/*
- * This file contains implementation of Drawbridge event synchronization APIs.
+/* Copyright (C) 2021 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
  */
 
 #include <asm/errno.h>
+#include <limits.h>
 #include <linux/futex.h>
 #include <linux/time.h>
+#include <stdbool.h>
 
 #include "api.h"
-#include "atomic.h"
+#include "assert.h"
 #include "pal.h"
-#include "pal_debug.h"
-#include "pal_defs.h"
-#include "pal_error.h"
 #include "pal_internal.h"
-#include "pal_linux.h"
-#include "pal_linux_defs.h"
+#include "pal_linux_error.h"
 
-int _DkEventCreate(PAL_HANDLE* event, bool initialState, bool isnotification) {
-    PAL_HANDLE ev = malloc(HANDLE_SIZE(event));
-    SET_HANDLE_TYPE(ev, event);
-    ev->event.isnotification = isnotification;
-    __atomic_store_n(&ev->event.nwaiters.counter, 0, __ATOMIC_SEQ_CST);
-    __atomic_store_n(&ev->event.signaled, initialState ? 1 : 0, __ATOMIC_SEQ_CST);
-    *event = ev;
+int _DkEventCreate(PAL_HANDLE* handle_ptr, bool init_signaled, bool auto_clear) {
+    PAL_HANDLE handle = malloc(HANDLE_SIZE(event));
+    if (!handle) {
+        return -PAL_ERROR_NOMEM;
+    }
+
+    SET_HANDLE_TYPE(handle, event);
+    handle->event.auto_clear = auto_clear;
+    __atomic_store_n(&handle->event.signaled, init_signaled ? 1 : 0, __ATOMIC_RELEASE);
+
+    *handle_ptr = handle;
     return 0;
 }
 
-int _DkEventSet(PAL_HANDLE event, int wakeup) {
-    int ret = 0;
-
-    if (event->event.isnotification) {
-        // Leave it signaled, wake all
-        uint32_t t = 0;
-        if (__atomic_compare_exchange_n(&event->event.signaled, &t, 1, /*weak=*/false,
-                                        __ATOMIC_ACQUIRE, __ATOMIC_RELAXED)) {
-            int nwaiters = __atomic_load_n(&event->event.nwaiters.counter, __ATOMIC_SEQ_CST);
-            if (nwaiters) {
-                if (wakeup != -1 && nwaiters > wakeup)
-                    nwaiters = wakeup;
-
-                ret = INLINE_SYSCALL(futex, 6, &event->event.signaled, FUTEX_WAKE, nwaiters, NULL,
-                                     NULL, 0);
-                if (ret < 0)
-                    __atomic_store_n(&event->event.signaled, 0, __ATOMIC_SEQ_CST);
-            }
-        }
-    } else {
-        // Only one thread wakes up, leave unsignaled
-        ret = INLINE_SYSCALL(futex, 6, &event->event.signaled, FUTEX_WAKE, 1, NULL, NULL, 0);
-    }
-
-    return ret < 0 ? -PAL_ERROR_TRYAGAIN : ret;
+void _DkEventSet(PAL_HANDLE handle) {
+    __atomic_store_n(&handle->event.signaled, 1, __ATOMIC_RELEASE);
+    int ret = INLINE_SYSCALL(futex, 6, &handle->event.signaled, FUTEX_WAKE,
+                             handle->event.auto_clear ? 1 : INT_MAX, NULL, NULL, 0);
+    __UNUSED(ret);
+    /* This `FUTEX_WAKE` cannot really fail. */
+    assert(ret >= 0);
 }
 
-int _DkEventWaitTimeout(PAL_HANDLE event, int64_t timeout_us) {
-    int ret = 0;
+void _DkEventClear(PAL_HANDLE handle) {
+    __atomic_store_n(&handle->event.signaled, 0, __ATOMIC_RELEASE);
+}
 
-    struct timespec waittime;
-    struct timespec* waittime_ptr = NULL;
+static int wait_timeout(PAL_HANDLE handle, int64_t timeout_us) {
+    struct timespec timeout = { 0 };
+    struct timespec* timeout_ptr = NULL;
     if (timeout_us >= 0) {
-        int64_t sec      = timeout_us / 1000000UL;
-        int64_t microsec = timeout_us - (sec * 1000000UL);
-        waittime.tv_sec  = sec;
-        waittime.tv_nsec = microsec * 1000;
-        waittime_ptr = &waittime;
+        timeout.tv_sec = timeout_us / TIME_US_IN_S;
+        timeout.tv_nsec = (timeout_us - timeout.tv_sec * TIME_US_IN_S) * TIME_NS_IN_US;
+        timeout_ptr = &timeout;
     }
 
-    if (!event->event.isnotification ||
-        !__atomic_load_n(&event->event.signaled, __ATOMIC_SEQ_CST)) {
+    while (1) {
+        bool needs_sleep = false;
+        if (handle->event.auto_clear) {
+            needs_sleep = __atomic_exchange_n(&handle->event.signaled, 0, __ATOMIC_ACQ_REL) == 0;
+        } else {
+            needs_sleep = __atomic_load_n(&handle->event.signaled, __ATOMIC_ACQUIRE) == 0;
+        }
 
-        __atomic_add_fetch(&event->event.nwaiters.counter, 1, __ATOMIC_SEQ_CST);
-
-        do {
-            ret = INLINE_SYSCALL(futex, 6, &event->event.signaled, FUTEX_WAIT, 0, waittime_ptr,
+        if (!needs_sleep) {
+            return 0;
+        }
+        /* TODO: we do not decrease timeout, so it might be off if we get woken up spuriously. */
+        int ret = INLINE_SYSCALL(futex, 6, &handle->event.signaled, FUTEX_WAIT, 0, timeout_ptr,
                                  NULL, 0);
-
-            if (ret < 0) {
-                if (ret == -EWOULDBLOCK) {
-                    ret = 0;
-                } else if (ret == -EINTR
-                           && (!event->event.isnotification
-                               || __atomic_load_n(&event->event.signaled, __ATOMIC_SEQ_CST))) {
-                    ret = 0;
-                    break;
-                } else {
-                    ret = unix_to_pal_error(ret);
-                    break;
-                }
-            }
-        } while (event->event.isnotification &&
-                 !__atomic_load_n(&event->event.signaled, __ATOMIC_SEQ_CST));
-
-        __atomic_sub_fetch(&event->event.nwaiters.counter, 1, __ATOMIC_SEQ_CST);
+        if (ret < 0 && ret != -EAGAIN) {
+            return unix_to_pal_error(ret);
+        }
     }
-
-    return ret;
-}
-
-int _DkEventClear(PAL_HANDLE event) {
-    __atomic_store_n(&event->event.signaled, 0, __ATOMIC_SEQ_CST);
-    return 0;
-}
-
-static int event_close(PAL_HANDLE handle) {
-    _DkEventSet(handle, -1);
-    return 0;
-}
-
-static int event_wait(PAL_HANDLE handle, int64_t timeout_us) {
-    return _DkEventWaitTimeout(handle, timeout_us);
 }
 
 struct handle_ops g_event_ops = {
-    .close = &event_close,
-    .wait  = &event_wait,
+    .wait = wait_timeout,
 };