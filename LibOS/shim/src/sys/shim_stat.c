gramineproject/graphene

[LibOS] Rewrite path lookup functions
This commit rewrites `path_lookupat` and `open_namei` functions, as well
as related helper functions.

- Rewrite the algorithm to be iterative, with the exception of symlink
  lookup which remains recursive.

- Distinguish the case when a file does not exist, but can be created
  (if LOOKUP_CREATE is set): previously, the code handled -ENOENT
  specially and checked if a dentry got created anyway, which was
  confusing and incorrect with respect to reference counting.

  Instead, the code now treats such cases as a success and create a
  negative dentry. On failures, no dentry is returned (the code sets the
  pointer to NULL), and the temporary dentry that has been created is
  ready to be garbage-collected.

- Follow Linux semantics more closely: make LOOKUP_FOLLOW control only
  what we do with the last path segment (the intermediate segments
  always should follow symlinks), and treat paths ending with slash
  specially.

  This also adds a LOOKUP_NO_FOLLOW value to make the the call sites
  more clear, and changes the call sites to always use one or the other.

  (Most Graphene filesystems do not handle symlinks yet, but this
  prepares the ground for them).

- Rename DENTRY_ANCESTOR (synthetic nodes created for mount purposes) to
  DENTRY_SYNTHETIC.

- Add a helper function (`dump_dentry`) to dump the dentry tree for
  debugging.

Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

LibOS/shim/src/sys/shim_stat.c
@@ -26,7 +26,7 @@ long shim_do_stat(const char* file, struct stat* stat) {
     int ret;
     struct shim_dentry* dent = NULL;
 
-    if ((ret = path_lookupat(NULL, file, LOOKUP_ACCESS | LOOKUP_FOLLOW, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(/*start=*/NULL, file, LOOKUP_FOLLOW, &dent)) < 0)
         goto out;
 
     struct shim_mount* fs = dent->fs;
@@ -53,7 +53,7 @@ long shim_do_lstat(const char* file, struct stat* stat) {
     int ret;
     struct shim_dentry* dent = NULL;
 
-    if ((ret = path_lookupat(NULL, file, LOOKUP_ACCESS, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(/*start=*/NULL, file, LOOKUP_NO_FOLLOW, &dent)) < 0)
         goto out;
 
     struct shim_mount* fs = dent->fs;
@@ -109,7 +109,7 @@ long shim_do_readlinkat(int dirfd, const char* file, char* buf, int bufsize) {
 
     struct shim_qstr qstr = QSTR_INIT;
 
-    if ((ret = path_lookupat(dir, file, LOOKUP_ACCESS, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(dir, file, LOOKUP_NO_FOLLOW, &dent)) < 0)
         goto out;
 
     ret = -EINVAL;
@@ -168,7 +168,7 @@ long shim_do_statfs(const char* path, struct statfs* buf) {
     int ret;
     struct shim_dentry* dent = NULL;
 
-    if ((ret = path_lookupat(NULL, path, LOOKUP_ACCESS | LOOKUP_FOLLOW, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(/*start=*/NULL, path, LOOKUP_NO_FOLLOW, &dent)) < 0)
         return ret;
 
     struct shim_mount* fs = dent->fs;
@@ -194,7 +194,7 @@ long shim_do_newfstatat(int dirfd, const char* pathname, struct stat* statbuf, i
     if (test_user_memory(statbuf, sizeof(*statbuf), true))
         return -EFAULT;
 
-    int lookup_flags = LOOKUP_ACCESS | LOOKUP_FOLLOW;
+    int lookup_flags = LOOKUP_FOLLOW;
     if (flags & AT_SYMLINK_NOFOLLOW)
         lookup_flags &= ~LOOKUP_FOLLOW;
     if (flags & AT_NO_AUTOMOUNT) {
@@ -234,7 +234,7 @@ long shim_do_newfstatat(int dirfd, const char* pathname, struct stat* statbuf, i
     }
 
     struct shim_dentry* dent = NULL;
-    ret = path_lookupat(dir, pathname, lookup_flags, &dent, NULL);
+    ret = path_lookupat(dir, pathname, lookup_flags, &dent);
     if (ret >= 0) {
         struct shim_d_ops* d_ops = dent->fs->d_ops;
         if (d_ops && d_ops->stat)

gramineproject/gramine

  Instead, the code now treats such cases as a success, and creates a
  This also adds a LOOKUP_NO_FOLLOW value to make the call sites more
  clear, and changes the call sites to always use one or the other.

LibOS/shim/src/sys/shim_stat.c
@@ -26,7 +26,7 @@ long shim_do_stat(const char* file, struct stat* stat) {
     int ret;
     struct shim_dentry* dent = NULL;
 
-    if ((ret = path_lookupat(NULL, file, LOOKUP_ACCESS | LOOKUP_FOLLOW, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(/*start=*/NULL, file, LOOKUP_FOLLOW, &dent)) < 0)
         goto out;
 
     struct shim_mount* fs = dent->fs;
@@ -53,7 +53,7 @@ long shim_do_lstat(const char* file, struct stat* stat) {
     int ret;
     struct shim_dentry* dent = NULL;
 
-    if ((ret = path_lookupat(NULL, file, LOOKUP_ACCESS, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(/*start=*/NULL, file, LOOKUP_NO_FOLLOW, &dent)) < 0)
         goto out;
 
     struct shim_mount* fs = dent->fs;
@@ -109,7 +109,7 @@ long shim_do_readlinkat(int dirfd, const char* file, char* buf, int bufsize) {
 
     struct shim_qstr qstr = QSTR_INIT;
 
-    if ((ret = path_lookupat(dir, file, LOOKUP_ACCESS, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(dir, file, LOOKUP_NO_FOLLOW, &dent)) < 0)
         goto out;
 
     ret = -EINVAL;
@@ -168,7 +168,7 @@ long shim_do_statfs(const char* path, struct statfs* buf) {
     int ret;
     struct shim_dentry* dent = NULL;
 
-    if ((ret = path_lookupat(NULL, path, LOOKUP_ACCESS | LOOKUP_FOLLOW, &dent, NULL)) < 0)
+    if ((ret = path_lookupat(/*start=*/NULL, path, LOOKUP_FOLLOW, &dent)) < 0)
         return ret;
 
     struct shim_mount* fs = dent->fs;
@@ -194,7 +194,7 @@ long shim_do_newfstatat(int dirfd, const char* pathname, struct stat* statbuf, i
     if (test_user_memory(statbuf, sizeof(*statbuf), true))
         return -EFAULT;
 
-    int lookup_flags = LOOKUP_ACCESS | LOOKUP_FOLLOW;
+    int lookup_flags = LOOKUP_FOLLOW;
     if (flags & AT_SYMLINK_NOFOLLOW)
         lookup_flags &= ~LOOKUP_FOLLOW;
     if (flags & AT_NO_AUTOMOUNT) {
@@ -234,7 +234,7 @@ long shim_do_newfstatat(int dirfd, const char* pathname, struct stat* statbuf, i
     }
 
     struct shim_dentry* dent = NULL;
-    ret = path_lookupat(dir, pathname, lookup_flags, &dent, NULL);
+    ret = path_lookupat(dir, pathname, lookup_flags, &dent);
     if (ret >= 0) {
         struct shim_d_ops* d_ops = dent->fs->d_ops;
         if (d_ops && d_ops->stat)