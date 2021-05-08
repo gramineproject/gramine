gramineproject/graphene

[Pal] Report all preloaded ranges to LibOS
Previously PALs could only report a predefined set of preloaded memory
ranges ("vdso" and "manifest"). Now they could report an arbitrary array
of such ranges.
This commit also adds reporting of "vvar" range on Linux PAL.

Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

LibOS/shim/src/bookkeep/shim_vma.c
@@ -524,36 +524,27 @@ static int _bkeep_initial_vma(struct shim_vma* new_vma) {
 static void* g_aslr_addr_top = NULL;
 
 int init_vma(void) {
-    struct shim_vma init_vmas[] = {
-        {.begin = 0}, // vma for creation of memory manager
-        {
-            .begin   = (uintptr_t)&__load_address,
-            .end     = (uintptr_t)ALLOC_ALIGN_UP_PTR(&__load_address_end),
-            .prot    = PROT_NONE,
-            .flags   = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL,
-            .file    = NULL,
-            .offset  = 0,
-            .comment = "LibOS",
-        },
-        {
-            .begin   = (uintptr_t)ALLOC_ALIGN_DOWN_PTR(g_pal_control->manifest_preload.start),
-            .end     = (uintptr_t)ALLOC_ALIGN_UP_PTR(g_pal_control->manifest_preload.end),
-            .prot    = PROT_NONE,
-            .flags   = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL,
-            .file    = NULL,
-            .offset  = 0,
-            .comment = "manifest",
-        },
-        {
-            .begin   = (uintptr_t)ALLOC_ALIGN_DOWN_PTR(g_pal_control->vdso_preload.start),
-            .end     = (uintptr_t)ALLOC_ALIGN_UP_PTR(g_pal_control->vdso_preload.end),
-            .prot    = PROT_NONE,
-            .flags   = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL,
-            .file    = NULL,
-            .offset  = 0,
-            .comment = "vDSO",
-        },
-    };
+    struct shim_vma init_vmas[2 + g_pal_control->preloaded_ranges_len];
+
+    init_vmas[0].begin = 0; // vma for creation of memory manager
+
+    init_vmas[1].begin  = (uintptr_t)&__load_address;
+    init_vmas[1].end    = (uintptr_t)ALLOC_ALIGN_UP_PTR(&__load_address_end);
+    init_vmas[1].prot   = PROT_NONE;
+    init_vmas[1].flags  = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL;
+    init_vmas[1].file   = NULL;
+    init_vmas[1].offset = 0;
+    copy_comment(&init_vmas[1], "LibOS");
+
+    for (size_t i = 0; i < g_pal_control->preloaded_ranges_len; i++) {
+        init_vmas[2 + i].begin  = ALLOC_ALIGN_DOWN(g_pal_control->preloaded_ranges[i].start);
+        init_vmas[2 + i].end    = ALLOC_ALIGN_UP(g_pal_control->preloaded_ranges[i].end);
+        init_vmas[2 + i].prot   = PROT_NONE;
+        init_vmas[2 + i].flags  = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL;
+        init_vmas[2 + i].file   = NULL;
+        init_vmas[2 + i].offset = 0;
+        copy_comment(&init_vmas[2 + i], g_pal_control->preloaded_ranges[i].comment);
+    }
 
     spinlock_lock(&vma_tree_lock);
     int ret = 0;

Pal/include/pal/pal.h
@@ -155,8 +155,12 @@ typedef struct PAL_CONTROL_ {
     PAL_BOL disable_aslr;       /*!< disable ASLR (may be necessary for restricted environments) */
     PAL_PTR_RANGE user_address; /*!< The range of user addresses */
 
-    PAL_PTR_RANGE manifest_preload; /*!< manifest was preloaded here */
-    PAL_PTR_RANGE vdso_preload;     /*!< vDSO was preloaded here */
+    struct {
+        uintptr_t start;
+        uintptr_t end;
+        const char* comment;
+    }* preloaded_ranges; /*!< array of memory ranges which are preoccupied */
+    size_t preloaded_ranges_len;
 
     /*
      * Host information

Pal/src/db_memory.c
@@ -58,3 +58,26 @@ int DkVirtualMemoryProtect(PAL_PTR addr, PAL_NUM size, PAL_FLG prot) {
 
     return _DkVirtualMemoryProtect((void*)addr, size, prot);
 }
+
+int add_preloaded_range(uintptr_t start, uintptr_t end, const char* comment) {
+    size_t new_len = g_pal_control.preloaded_ranges_len + 1;
+    void* new_ranges = malloc(new_len * sizeof(*g_pal_control.preloaded_ranges));
+    if (!new_ranges) {
+        return -PAL_ERROR_NOMEM;
+    }
+
+    if (g_pal_control.preloaded_ranges_len) {
+        memcpy(new_ranges, g_pal_control.preloaded_ranges,
+               g_pal_control.preloaded_ranges_len * sizeof(*g_pal_control.preloaded_ranges));
+    }
+
+    free(g_pal_control.preloaded_ranges);
+    g_pal_control.preloaded_ranges = new_ranges;
+
+    g_pal_control.preloaded_ranges[g_pal_control.preloaded_ranges_len].start = start;
+    g_pal_control.preloaded_ranges[g_pal_control.preloaded_ranges_len].end = end;
+    g_pal_control.preloaded_ranges[g_pal_control.preloaded_ranges_len].comment = comment;
+    g_pal_control.preloaded_ranges_len++;
+
+    return 0;
+}

gramineproject/gramine

LibOS/shim/src/bookkeep/shim_vma.c
@@ -524,36 +524,27 @@ static int _bkeep_initial_vma(struct shim_vma* new_vma) {
 static void* g_aslr_addr_top = NULL;
 
 int init_vma(void) {
-    struct shim_vma init_vmas[] = {
-        {.begin = 0}, // vma for creation of memory manager
-        {
-            .begin   = (uintptr_t)&__load_address,
-            .end     = (uintptr_t)ALLOC_ALIGN_UP_PTR(&__load_address_end),
-            .prot    = PROT_NONE,
-            .flags   = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL,
-            .file    = NULL,
-            .offset  = 0,
-            .comment = "LibOS",
-        },
-        {
-            .begin   = (uintptr_t)ALLOC_ALIGN_DOWN_PTR(g_pal_control->manifest_preload.start),
-            .end     = (uintptr_t)ALLOC_ALIGN_UP_PTR(g_pal_control->manifest_preload.end),
-            .prot    = PROT_NONE,
-            .flags   = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL,
-            .file    = NULL,
-            .offset  = 0,
-            .comment = "manifest",
-        },
-        {
-            .begin   = (uintptr_t)ALLOC_ALIGN_DOWN_PTR(g_pal_control->vdso_preload.start),
-            .end     = (uintptr_t)ALLOC_ALIGN_UP_PTR(g_pal_control->vdso_preload.end),
-            .prot    = PROT_NONE,
-            .flags   = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL,
-            .file    = NULL,
-            .offset  = 0,
-            .comment = "vDSO",
-        },
-    };
+    struct shim_vma init_vmas[2 + g_pal_control->preloaded_ranges_cnt];
+
+    init_vmas[0].begin = 0; // vma for creation of memory manager
+
+    init_vmas[1].begin  = (uintptr_t)&__load_address;
+    init_vmas[1].end    = (uintptr_t)ALLOC_ALIGN_UP_PTR(&__load_address_end);
+    init_vmas[1].prot   = PROT_NONE;
+    init_vmas[1].flags  = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL;
+    init_vmas[1].file   = NULL;
+    init_vmas[1].offset = 0;
+    copy_comment(&init_vmas[1], "LibOS");
+
+    for (size_t i = 0; i < g_pal_control->preloaded_ranges_cnt; i++) {
+        init_vmas[2 + i].begin  = ALLOC_ALIGN_DOWN(g_pal_control->preloaded_ranges[i].start);
+        init_vmas[2 + i].end    = ALLOC_ALIGN_UP(g_pal_control->preloaded_ranges[i].end);
+        init_vmas[2 + i].prot   = PROT_NONE;
+        init_vmas[2 + i].flags  = MAP_PRIVATE | MAP_ANONYMOUS | VMA_INTERNAL;
+        init_vmas[2 + i].file   = NULL;
+        init_vmas[2 + i].offset = 0;
+        copy_comment(&init_vmas[2 + i], g_pal_control->preloaded_ranges[i].comment);
+    }
 
     spinlock_lock(&vma_tree_lock);
     int ret = 0;

Pal/include/pal/pal.h
@@ -155,8 +155,12 @@ typedef struct PAL_CONTROL_ {
     PAL_BOL disable_aslr;       /*!< disable ASLR (may be necessary for restricted environments) */
     PAL_PTR_RANGE user_address; /*!< The range of user addresses */
 
-    PAL_PTR_RANGE manifest_preload; /*!< manifest was preloaded here */
-    PAL_PTR_RANGE vdso_preload;     /*!< vDSO was preloaded here */
+    struct {
+        uintptr_t start;
+        uintptr_t end;
+        const char* comment;
+    }* preloaded_ranges; /*!< array of memory ranges which are preoccupied */
+    size_t preloaded_ranges_cnt;
 
     /*
      * Host information

Pal/src/db_memory.c
@@ -58,3 +58,26 @@ int DkVirtualMemoryProtect(PAL_PTR addr, PAL_NUM size, PAL_FLG prot) {
 
     return _DkVirtualMemoryProtect((void*)addr, size, prot);
 }
+
+int add_preloaded_range(uintptr_t start, uintptr_t end, const char* comment) {
+    size_t new_cnt = g_pal_control.preloaded_ranges_cnt + 1;
+    void* new_ranges = malloc(new_cnt * sizeof(*g_pal_control.preloaded_ranges));
+    if (!new_ranges) {
+        return -PAL_ERROR_NOMEM;
+    }
+
+    if (g_pal_control.preloaded_ranges_cnt) {
+        memcpy(new_ranges, g_pal_control.preloaded_ranges,
+               g_pal_control.preloaded_ranges_cnt * sizeof(*g_pal_control.preloaded_ranges));
+    }
+
+    free(g_pal_control.preloaded_ranges);
+    g_pal_control.preloaded_ranges = new_ranges;
+
+    g_pal_control.preloaded_ranges[g_pal_control.preloaded_ranges_cnt].start = start;
+    g_pal_control.preloaded_ranges[g_pal_control.preloaded_ranges_cnt].end = end;
+    g_pal_control.preloaded_ranges[g_pal_control.preloaded_ranges_cnt].comment = comment;
+    g_pal_control.preloaded_ranges_cnt++;
+
+    return 0;
+}