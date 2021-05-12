gramineproject/graphene

[Pal] Return remaining time in DkEventWait
Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

Pal/src/db_events.c
@@ -22,7 +22,7 @@ void DkEventClear(PAL_HANDLE handle) {
     _DkEventClear(handle);
 }
 
-int DkEventWait(PAL_HANDLE handle, int64_t timeout_us) {
+int DkEventWait(PAL_HANDLE handle, uint64_t* timeout) {
     assert(handle && IS_HANDLE_TYPE(handle, event));
-    return _DkEventWait(handle, timeout_us);
+    return _DkEventWait(handle, timeout);
 }

Pal/src/host/Linux-common/timespec_utils.c
@@ -1,0 +1,34 @@
+#include <linux/time.h>
+
+#include "api.h"
+#include "cpu.h"
+#include "linux_utils.h"
+#include "sysdep-arch.h"
+
+void time_get_now_plus_ns(struct timespec* ts, uint64_t val) {
+    /* This can only fail if arguments are invalid. */
+    int ret = INLINE_SYSCALL(clock_gettime, 2, CLOCK_MONOTONIC, ts);
+    if (ret < 0) {
+        die_or_inf_loop();
+    }
+
+    ts->tv_sec += val / TIME_NS_IN_S;
+    ts->tv_nsec += val % TIME_NS_IN_S;
+    if ((uint64_t)ts->tv_nsec >= TIME_NS_IN_S) {
+        ts->tv_nsec -= TIME_NS_IN_S;
+        ts->tv_sec += 1;
+    }
+}
+
+int64_t time_ns_diff_from_now(struct timespec* ts) {
+    struct timespec time_now;
+    /* This can only fail if arguments are invalid. */
+    int ret = INLINE_SYSCALL(clock_gettime, 2, CLOCK_MONOTONIC, &time_now);
+    if (ret < 0) {
+        die_or_inf_loop();
+    }
+
+    int64_t diff = (ts->tv_sec - time_now.tv_sec) * TIME_NS_IN_S;
+    diff += ts->tv_nsec - time_now.tv_nsec;
+    return diff;
+}

Pal/src/pal_internal.h
@@ -202,7 +202,7 @@ int _DkThreadGetCpuAffinity(PAL_HANDLE thread, PAL_NUM cpumask_size, PAL_PTR cpu
 int _DkEventCreate(PAL_HANDLE* handle_ptr, bool init_signaled, bool auto_clear);
 void _DkEventSet(PAL_HANDLE handle);
 void _DkEventClear(PAL_HANDLE handle);
-int _DkEventWait(PAL_HANDLE handle, int64_t timeout_us);
+int _DkEventWait(PAL_HANDLE handle, uint64_t* timeout);
 
 /* DkVirtualMemory calls */
 int _DkVirtualMemoryAlloc(void** paddr, uint64_t size, int alloc_type, int prot);

gramineproject/gramine

Pal/src/db_events.c
@@ -22,7 +22,7 @@ void DkEventClear(PAL_HANDLE handle) {
     _DkEventClear(handle);
 }
 
-int DkEventWait(PAL_HANDLE handle, int64_t timeout_us) {
+int DkEventWait(PAL_HANDLE handle, uint64_t* timeout_us) {
     assert(handle && IS_HANDLE_TYPE(handle, event));
     return _DkEventWait(handle, timeout_us);
 }

Pal/src/host/Linux-common/timespec_utils.c
@@ -1,0 +1,34 @@
+#include <linux/time.h>
+
+#include "api.h"
+#include "cpu.h"
+#include "linux_utils.h"
+#include "sysdep-arch.h"
+
+void time_get_now_plus_ns(struct timespec* ts, uint64_t addend_ns) {
+    /* This can only fail if arguments are invalid. */
+    int ret = INLINE_SYSCALL(clock_gettime, 2, CLOCK_MONOTONIC, ts);
+    if (ret < 0) {
+        die_or_inf_loop();
+    }
+
+    ts->tv_sec += addend_ns / TIME_NS_IN_S;
+    ts->tv_nsec += addend_ns % TIME_NS_IN_S;
+    if ((uint64_t)ts->tv_nsec >= TIME_NS_IN_S) {
+        ts->tv_nsec -= TIME_NS_IN_S;
+        ts->tv_sec += 1;
+    }
+}
+
+int64_t time_ns_diff_from_now(struct timespec* ts) {
+    struct timespec time_now;
+    /* This can only fail if arguments are invalid. */
+    int ret = INLINE_SYSCALL(clock_gettime, 2, CLOCK_MONOTONIC, &time_now);
+    if (ret < 0) {
+        die_or_inf_loop();
+    }
+
+    int64_t diff = (ts->tv_sec - time_now.tv_sec) * TIME_NS_IN_S;
+    diff += ts->tv_nsec - time_now.tv_nsec;
+    return diff;
+}

Pal/src/pal_internal.h
@@ -202,7 +202,7 @@ int _DkThreadGetCpuAffinity(PAL_HANDLE thread, PAL_NUM cpumask_size, PAL_PTR cpu
 int _DkEventCreate(PAL_HANDLE* handle_ptr, bool init_signaled, bool auto_clear);
 void _DkEventSet(PAL_HANDLE handle);
 void _DkEventClear(PAL_HANDLE handle);
-int _DkEventWait(PAL_HANDLE handle, int64_t timeout_us);
+int _DkEventWait(PAL_HANDLE handle, uint64_t* timeout_us);
 
 /* DkVirtualMemory calls */
 int _DkVirtualMemoryAlloc(void** paddr, uint64_t size, int alloc_type, int prot);