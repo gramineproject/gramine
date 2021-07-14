jefrww/graphene

[LibOS] Make Graphene internal IDs managed by LibOS
Previously Graphene internal IDs were just equal to host PIDs, which was
a security issue on SGX PAL.

Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

LibOS/shim/include/shim_ipc.h
@@ -19,6 +19,7 @@
 
 enum {
     IPC_MSG_RESP = 0,
+    IPC_MSG_GET_VMID,           /*!< Request new VMID. */
     IPC_MSG_CHILDEXIT,          /*!< Child exit/death information. */
     IPC_MSG_ALLOC_ID_RANGE,     /*!< Request new IDs range. */
     IPC_MSG_RELEASE_ID_RANGE,   /*!< Release IDs range. */
@@ -42,12 +43,14 @@ enum kill_type { KILL_THREAD, KILL_PROCESS, KILL_PGROUP, KILL_ALL };
 
 enum pid_meta_code { PID_META_CRED, PID_META_EXEC, PID_META_CWD, PID_META_ROOT };
 
+#define STARTING_VMID 1
+
 struct shim_ipc_ids {
+    IDTYPE self_vmid;
     IDTYPE parent_vmid;
     IDTYPE leader_vmid;
 };
 
-extern IDTYPE g_self_vmid;
 extern struct shim_ipc_ids g_process_ipc_ids;
 
 int init_ipc(void);
@@ -141,6 +144,14 @@ int ipc_broadcast(struct shim_ipc_msg* msg, IDTYPE exclude_vmid);
  * free it!
  */
 int ipc_response_callback(IDTYPE src, void* data, uint64_t seq);
+
+/*!
+ * \brief Get a new VMID
+ *
+ * \param[out] vmid contains the new VMID
+ */
+int ipc_get_new_vmid(IDTYPE* vmid);
+int ipc_get_new_vmid_callback(IDTYPE src, void* data, uint64_t seq);
 
 struct shim_ipc_cld_exit {
     IDTYPE ppid, pid;

LibOS/shim/src/ipc/shim_ipc_vmid.c
@@ -1,0 +1,61 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2021 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
+ */
+
+#include "api.h"
+#include "log.h"
+#include "shim_ipc.h"
+#include "shim_types.h"
+#include "shim_utils.h"
+
+static IDTYPE g_last_vmid = STARTING_VMID;
+
+static IDTYPE get_next_vmid(void) {
+    return __atomic_add_fetch(&g_last_vmid, 1, __ATOMIC_ACQ_REL);
+}
+
+int ipc_get_new_vmid(IDTYPE* vmid) {
+    if (!g_process_ipc_ids.leader_vmid) {
+        *vmid = get_next_vmid();
+        return 0;
+    }
+
+    size_t msg_size = get_ipc_msg_size(0);
+    struct shim_ipc_msg* msg = malloc(msg_size);
+    if (!msg) {
+        return -ENOMEM;
+    }
+    init_ipc_msg(msg, IPC_MSG_GET_VMID, msg_size);
+
+    log_debug("%s: sending a request", __func__);
+
+    void* resp = NULL;
+    int ret = ipc_send_msg_and_get_response(g_process_ipc_ids.leader_vmid, msg, &resp);
+    if (ret < 0) {
+        goto out;
+    }
+
+    *vmid = *(IDTYPE*)resp;
+    ret = 0;
+    log_debug("%s: got a response: %u", __func__, *vmid);
+
+out:
+    free(resp);
+    free(msg);
+    return ret;
+}
+
+int ipc_get_new_vmid_callback(IDTYPE src, void* data, uint64_t seq) {
+    __UNUSED(data);
+    IDTYPE vmid = get_next_vmid();
+
+    log_debug("%s: %u", __func__, vmid);
+
+    size_t msg_size = get_ipc_msg_size(sizeof(vmid));
+    struct shim_ipc_msg* msg = __alloca(msg_size);
+    init_ipc_response(msg, seq, msg_size);
+    memcpy(&msg->data, &vmid, sizeof(vmid));
+
+    return ipc_send_message(src, msg);
+}

LibOS/shim/src/ipc/shim_ipc_worker.c
@@ -46,6 +46,7 @@ static PAL_HANDLE g_self_ipc_handle = NULL;
 typedef int (*ipc_callback)(IDTYPE src, void* data, uint64_t seq);
 static ipc_callback ipc_callbacks[] = {
     [IPC_MSG_RESP]              = ipc_response_callback,
+    [IPC_MSG_GET_VMID]          = ipc_get_new_vmid_callback,
     [IPC_MSG_CHILDEXIT]         = ipc_cld_exit_callback,
     [IPC_MSG_ALLOC_ID_RANGE]    = ipc_alloc_id_range_callback,
     [IPC_MSG_RELEASE_ID_RANGE]  = ipc_release_id_range_callback,

gramineproject/gramine

LibOS/shim/include/shim_ipc.h
@@ -19,6 +19,7 @@
 
 enum {
     IPC_MSG_RESP = 0,
+    IPC_MSG_GET_NEW_VMID,       /*!< Request new VMID. */
     IPC_MSG_CHILDEXIT,          /*!< Child exit/death information. */
     IPC_MSG_ALLOC_ID_RANGE,     /*!< Request new IDs range. */
     IPC_MSG_RELEASE_ID_RANGE,   /*!< Release IDs range. */
@@ -42,12 +43,14 @@ enum kill_type { KILL_THREAD, KILL_PROCESS, KILL_PGROUP, KILL_ALL };
 
 enum pid_meta_code { PID_META_CRED, PID_META_EXEC, PID_META_CWD, PID_META_ROOT };
 
+#define STARTING_VMID 1
+
 struct shim_ipc_ids {
+    IDTYPE self_vmid;
     IDTYPE parent_vmid;
     IDTYPE leader_vmid;
 };
 
-extern IDTYPE g_self_vmid;
 extern struct shim_ipc_ids g_process_ipc_ids;
 
 int init_ipc(void);
@@ -141,6 +144,14 @@ int ipc_broadcast(struct shim_ipc_msg* msg, IDTYPE exclude_vmid);
  * free it!
  */
 int ipc_response_callback(IDTYPE src, void* data, uint64_t seq);
+
+/*!
+ * \brief Get a new VMID
+ *
+ * \param[out] vmid contains the new VMID
+ */
+int ipc_get_new_vmid(IDTYPE* vmid);
+int ipc_get_new_vmid_callback(IDTYPE src, void* data, uint64_t seq);
 
 struct shim_ipc_cld_exit {
     IDTYPE ppid, pid;

LibOS/shim/src/ipc/shim_ipc_vmid.c
@@ -1,0 +1,61 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2021 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
+ */
+
+#include "api.h"
+#include "log.h"
+#include "shim_ipc.h"
+#include "shim_types.h"
+#include "shim_utils.h"
+
+static IDTYPE g_last_vmid = STARTING_VMID;
+
+static IDTYPE get_next_vmid(void) {
+    return __atomic_add_fetch(&g_last_vmid, 1, __ATOMIC_RELAXED);
+}
+
+int ipc_get_new_vmid(IDTYPE* vmid) {
+    if (!g_process_ipc_ids.leader_vmid) {
+        *vmid = get_next_vmid();
+        return 0;
+    }
+
+    size_t msg_size = get_ipc_msg_size(0);
+    struct shim_ipc_msg* msg = malloc(msg_size);
+    if (!msg) {
+        return -ENOMEM;
+    }
+    init_ipc_msg(msg, IPC_MSG_GET_NEW_VMID, msg_size);
+
+    log_debug("%s: sending a request", __func__);
+
+    void* resp = NULL;
+    int ret = ipc_send_msg_and_get_response(g_process_ipc_ids.leader_vmid, msg, &resp);
+    if (ret < 0) {
+        goto out;
+    }
+
+    *vmid = *(IDTYPE*)resp;
+    ret = 0;
+    log_debug("%s: got a response: %u", __func__, *vmid);
+
+out:
+    free(resp);
+    free(msg);
+    return ret;
+}
+
+int ipc_get_new_vmid_callback(IDTYPE src, void* data, uint64_t seq) {
+    __UNUSED(data);
+    IDTYPE vmid = get_next_vmid();
+
+    log_debug("%s: %u", __func__, vmid);
+
+    size_t msg_size = get_ipc_msg_size(sizeof(vmid));
+    struct shim_ipc_msg* msg = __alloca(msg_size);
+    init_ipc_response(msg, seq, msg_size);
+    memcpy(&msg->data, &vmid, sizeof(vmid));
+
+    return ipc_send_message(src, msg);
+}

LibOS/shim/src/ipc/shim_ipc_worker.c
@@ -46,6 +46,7 @@ static PAL_HANDLE g_self_ipc_handle = NULL;
 typedef int (*ipc_callback)(IDTYPE src, void* data, uint64_t seq);
 static ipc_callback ipc_callbacks[] = {
     [IPC_MSG_RESP]              = ipc_response_callback,
+    [IPC_MSG_GET_NEW_VMID]      = ipc_get_new_vmid_callback,
     [IPC_MSG_CHILDEXIT]         = ipc_cld_exit_callback,
     [IPC_MSG_ALLOC_ID_RANGE]    = ipc_alloc_id_range_callback,
     [IPC_MSG_RELEASE_ID_RANGE]  = ipc_release_id_range_callback,