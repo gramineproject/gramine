48d58031f2daac92bc745c08171cdb52fc1eb57b
Borys Popławski <borysp@invisiblethingslab.com>
2021-09-30 22:05:27 +0200

    fixup! [python] Rework graminelibos python library
    Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

--- python/graminelibos/manifest.py
+++ python/graminelibos/manifest.py
@@ -10,99 +10,14 @@ Gramine manifest renderer
 '''
 
 import hashlib
-import os
 import pathlib
-import subprocess
-import sys
-import sysconfig
 
-import jinja2
 import toml
 
-from . import (
-    _CONFIG_PKGLIBDIR,
-)
+from . import _env
 
 DEFAULT_ENCLAVE_SIZE = '256M'
 DEFAULT_THREAD_NUM = 4
-
-def ldd(*args):
-    '''
-    Args:
-        binaries for which to generate manifest trusted files list.
-    '''
-    # Be careful: We have to skip vdso, which doesn't have a corresponding file on the disk (we
-    # assume that such files have paths starting with '/', seems ldd always prints absolute paths).
-    # Also, old ldd (from Ubuntu 16.04) prints vdso differently than newer ones:
-    # old:
-    #     linux-vdso.so.1 =>  (0x00007ffd31fee000)
-    # new:
-    #     linux-vdso.so.1 (0x00007ffd31fee000)
-    ret = set()
-    for line in subprocess.check_output(['ldd', *(os.fspath(i) for i in args)]).decode('ascii'):
-        line = line.strip().split()
-        if line[1] == '=>' and line[2].startswith('/'):
-            ret.add(line[2])
-        elif line[0].startswith('/') and line[1].startswith('/'):
-            ret.add(line[0])
-    return sorted(ret)
-
-def add_globals_from_python(env):
-    paths = sysconfig.get_paths()
-    env.globals['python'] = {
-        'stdlib': pathlib.Path(paths['stdlib']),
-        'platstdlib': pathlib.Path(paths['platstdlib']),
-        'purelib': pathlib.Path(paths['purelib']),
-
-        # TODO rpm-based distros
-        'distlib': pathlib.Path(sysconfig.get_path('stdlib',
-                vars={'py_version_short': sys.version_info[0]})
-            ) / 'dist-packages',
-
-        'get_config_var': sysconfig.get_config_var,
-        'ext_suffix': sysconfig.get_config_var('EXT_SUFFIX'),
-
-        'get_path': sysconfig.get_path,
-        'get_paths': sysconfig.get_paths,
-
-        'implementation': sys.implementation,
-    }
-
-class Runtimedir:
-    @staticmethod
-    def __call__(libc='glibc'):
-        return (pathlib.Path(_CONFIG_PKGLIBDIR) / 'runtime' / libc).resolve()
-    def __str__(self):
-        return str(self())
-    def __truediv__(self, other):
-        return self() / other
-
-def add_globals_from_gramine(env):
-    env.globals['gramine'] = {
-        'libos': pathlib.Path(_CONFIG_PKGLIBDIR) / 'libsysdb.so',
-        'pkglibdir': pathlib.Path(_CONFIG_PKGLIBDIR),
-        'runtimedir': Runtimedir(),
-    }
-
-    try:
-        from . import _offsets as offsets # pylint: disable=import-outside-toplevel
-    except ImportError: # no SGX gramine installed, skipping
-        pass
-    else:
-        env.globals['gramine'].update(
-            (k, v) for k, v in offsets.__dict__.items()
-            if not k.startswith('_'))
-
-def add_globals_misc(env):
-    env.globals['env'] = os.environ
-    env.globals['ldd'] = ldd
-
-def make_env():
-    env = jinja2.Environment(undefined=jinja2.StrictUndefined, keep_trailing_newline=True)
-    add_globals_from_gramine(env)
-    add_globals_from_python(env)
-    add_globals_misc(env)
-    return env
 
 class ManifestError(Exception):
     pass
@@ -145,8 +60,6 @@ def append_trusted_dir_or_file(trusted_files, val):
         append_tf(trusted_files, uri, hash_file_contents(path))
 
 class Manifest:
-    _env = make_env()
-
     def __init__(self, manifest_str):
         manifest = toml.loads(manifest_str)
 
@@ -193,7 +106,7 @@ class Manifest:
     def from_template(cls, template, variables=None):
         '''Render template into a Manifest from the given string. Optional variables may be given
         as mapping.'''
-        return cls(cls._env.from_string(template).render(**(variables or {})))
+        return cls(_env.from_string(template).render(**(variables or {})))
 
     @classmethod
     def loads(cls, s):
