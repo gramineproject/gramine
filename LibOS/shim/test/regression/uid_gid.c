3736f4d688e9dff6703dcc34dd09244de70ef91c
Denis Zygann <denis@zygann.de>
2021-10-04 20:23:14 +0200

    fixup! [LibOS] Add `loader.uid` and `loader.gid` manifest options
    This implementation allows Gramine to run an executable with another user than root.
    By default, Gramine uses the root user (uid = gid = 0), which may cause some apps to refuse to run.
    We also set effective UID/GID to the same value as UID/GID respectively.
    This can be defined as manifest option `loader.uid`/`loader.gid` in the manifest file.
    
    Signed-off-by: Denis Zygann <denis@zygann.de>

--- LibOS/shim/test/regression/uid_gid.c
+++ LibOS/shim/test/regression/uid_gid.c
@@ -9,14 +9,14 @@ int main(int argc, char** argv) {
     uid_t euid = geteuid();
 
     if (uid != 1338 || euid != 1338) {
-        errx(EXIT_FAILURE, "UID/effective UID are not equal to the value in the the manifest");
+        errx(EXIT_FAILURE, "UID/effective UID are not equal to the value in the manifest");
     }
 
     uid_t gid = getgid();
     uid_t egid = getegid();
 
     if (gid != 1337 || egid != 1337) {
-        errx(EXIT_FAILURE, "GID/effective GID are not equal to the value in the the manifest");
+        errx(EXIT_FAILURE, "GID/effective GID are not equal to the value in the manifest");
     }
 
     puts("TEST OK");
