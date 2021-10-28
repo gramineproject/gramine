0b7123e6a3db4063730c0d3c924809b9b4359a56
Borys Popławski <borysp@invisiblethingslab.com>
2021-10-28 17:59:17 +0200

    [Pal/Linux] Add vDSO as allowed range in seccomp policy
    In some cases vDSO stubs may fallback to raw syscalls, which we now
    allow in our seccomp filter.
    
    Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

--- Pal/src/host/Linux/db_exception.c
+++ Pal/src/host/Linux/db_exception.c
@@ -127,6 +127,31 @@ static void handle_async_signal(int signum, siginfo_t* info, struct ucontext* uc
     perform_signal_handling(event, ADDR_IN_PAL_OR_VDSO(rip), /*addr=*/0, uc);
 }
 
+#define BPF_GENERATE_ALLOWED_RANGE(range_begin_low, range_begin_high,                           \
+                                   range_end_low, range_end_high)                               \
+    /* 0: A = ip >> 32 */                                                                       \
+    BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer) + 4), \
+    /* 1: A >= range_begin_high ? 0 : NEXT */                                                   \
+    BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, (range_begin_high), 0, /*NEXT*/9),                      \
+    /* 2: A == range_begin_high ? 0 : CMP_END */                                                \
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, (range_begin_high), 0, /*CMP_END*/2),                   \
+    /* 3: A = ip & (2**32 - 1) */                                                               \
+    BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer)),     \
+    /* 4: A >= range_begin_low ? CMP_END : NEXT */                                              \
+    BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, (range_begin_low), /*CMP_END*/0, /*NEXT*/6),            \
+    /* 5: CMP_END: A = ip >> 32 */                                                              \
+    BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer) + 4), \
+    /* 6: A > range_end_high ? NEXT : 0 */                                                      \
+    BPF_JUMP(BPF_JMP | BPF_JGT | BPF_K, (range_end_high), /*NEXT*/4, 0),                        \
+    /* 7: A == range_end_high ? 0 : ALLOW */                                                    \
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, (range_end_high), 0, /*ALLOW*/2),                       \
+    /* 8: A = ip & (2**32 - 1) */                                                               \
+    BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer)),     \
+    /* 9: A >= range_end_low ? NEXT : ALLOW */                                                  \
+    BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, (range_end_low), /*NEXT*/1, 0),                         \
+                                                                                                \
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW)
+
 static int setup_seccomp(void) {
     int ret = DO_SYSCALL(prctl, PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
     if (ret < 0) {
@@ -134,33 +159,19 @@ static int setup_seccomp(void) {
         return -1;
     }
 
-    uint32_t syscalls_code_begin_low = (uintptr_t)gramine_raw_syscalls_code_begin & 0xffffffffu;
+    uint32_t syscalls_code_begin_low = (uintptr_t)gramine_raw_syscalls_code_begin;
     uint32_t syscalls_code_begin_high = (uintptr_t)gramine_raw_syscalls_code_begin >> 32;
-    uint32_t syscalls_code_end_low = (uintptr_t)gramine_raw_syscalls_code_end & 0xffffffffu;
+    uint32_t syscalls_code_end_low = (uintptr_t)gramine_raw_syscalls_code_end;
     uint32_t syscalls_code_end_high = (uintptr_t)gramine_raw_syscalls_code_end >> 32;
-    struct sock_filter filter[] = {
-        /* 0: A = ip >> 32 */
-        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer) + 4),
-        /* 1: A >= syscalls_code_begin_high ? 0 : TRAP */
-        BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, syscalls_code_begin_high, 0, /*TRAP*/9),
-        /* 2: A == syscalls_code_begin_high ? 0 : CMP_END */
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, syscalls_code_begin_high, 0, /*CMP_END*/2),
-        /* 3: A = ip & (2**32 - 1) */
-        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer)),
-        /* 4: A >= syscalls_code_begin_low ? CMP_END : TRAP */
-        BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, syscalls_code_begin_low, /*CMP_END*/0, /*TRAP*/6),
-        /* 5: CMP_END: A = ip >> 32 */
-        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer) + 4),
-        /* 6: A > syscalls_code_end_high ? TRAP : 0 */
-        BPF_JUMP(BPF_JMP | BPF_JGT | BPF_K, syscalls_code_end_high, /*TRAP*/4, 0),
-        /* 7: A == syscalls_code_end_high ? 0 : ALLOW */
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, syscalls_code_end_high, 0, /*ALLOW*/2),
-        /* 8: A = ip & (2**32 - 1) */
-        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, instruction_pointer)),
-        /* 9: A >= syscalls_code_end_low ? TRAP : ALLOW */
-        BPF_JUMP(BPF_JMP | BPF_JGE | BPF_K, syscalls_code_end_low, /*TRAP*/1, 0),
+    uint32_t vdso_begin_low = g_vdso_start;
+    uint32_t vdso_begin_high = g_vdso_start >> 32;
+    uint32_t vdso_end_low = g_vdso_end;
+    uint32_t vdso_end_high = g_vdso_end >> 32;
 
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+    struct sock_filter filter[] = {
+        BPF_GENERATE_ALLOWED_RANGE(syscalls_code_begin_low, syscalls_code_begin_high,
+                                   syscalls_code_end_low, syscalls_code_end_high),
+        BPF_GENERATE_ALLOWED_RANGE(vdso_begin_low, vdso_begin_high, vdso_end_low, vdso_end_high),
         BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
     };
 
