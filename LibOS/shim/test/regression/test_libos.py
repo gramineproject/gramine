b4c9a89998ac029e92548f87ec21cff6a5fe86de
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-11-10 15:07:12 +0100

    [tests] Fix `has_debug()` invocation
    This bug caused the ASan test to fail when running in release mode.
    
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- LibOS/shim/test/regression/test_libos.py
+++ LibOS/shim/test/regression/test_libos.py
@@ -30,7 +30,7 @@ class TC_00_Unittests(RegressionTestCase):
     @unittest.skipUnless(os.environ.get('ASAN') == '1', 'test only enabled with ASAN=1')
     def test_021_asan(self):
         expected_list = ['asan: heap-buffer-overflow']
-        if self.has_debug:
+        if self.has_debug():
             expected_list.append('asan: location: run_test_asan_buffer_overflow at shim_call.c')
         self._test_abort('asan_buffer_overflow', expected_list)
 
