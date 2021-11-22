8107343d4d8f54ab01d8276075a89281e40a2f9e
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-11-22 12:30:48 +0100

    [LibOS] Bump up the stack size when ASan is enabled
    ASan stack sanitization causes functions to take up more space, and 7
    pages of stack is sometimes not enough.
    
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- LibOS/shim/include/shim_defs.h
+++ LibOS/shim/include/shim_defs.h
@@ -8,8 +8,13 @@
 #define ERESTARTNOINTR  513 /* Always restart. */
 #define ERESTARTNOHAND  514 /* Restart if no signal handler. */
 
-/* Internal LibOS stack size: 7 pages + one guard page. */
+/* Internal LibOS stack size: 7 pages + one guard page normally, 15 pages + one guard page when ASan
+ * is enabled (stack sanitization causes functions to take up more space). */
+#ifdef ASAN
+#define SHIM_THREAD_LIBOS_STACK_SIZE (15 * PAGE_SIZE + PAGE_SIZE)
+#else
 #define SHIM_THREAD_LIBOS_STACK_SIZE (7 * PAGE_SIZE + PAGE_SIZE)
+#endif
 
 #define DEFAULT_BRK_MAX_SIZE   (256 * 1024)        /* 256KB */
 #define DEFAULT_SYS_STACK_SIZE (256 * 1024)        /* 256KB */
