f0ead4d446dc73d723622040d539f7abdac76270
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-11-25 16:52:35 +0100

    fixup! [Pal/Linux] Optimize `DkEventSet` by avoiding `futex` calls
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- Pal/src/host/Linux/db_events.c
+++ Pal/src/host/Linux/db_events.c
@@ -27,7 +27,7 @@ int _DkEventCreate(PAL_HANDLE* handle_ptr, bool init_signaled, bool auto_clear) 
     spinlock_init(&handle->event.lock);
     handle->event.auto_clear = auto_clear;
     handle->event.waiters_cnt = 0;
-    handle->event.signaled = init_signaled ? 1 : 0;
+    __atomic_store_n(&handle->event.signaled, init_signaled ? 1 : 0, __ATOMIC_RELEASE);
 
     *handle_ptr = handle;
     return 0;
@@ -35,10 +35,12 @@ int _DkEventCreate(PAL_HANDLE* handle_ptr, bool init_signaled, bool auto_clear) 
 
 void _DkEventSet(PAL_HANDLE handle) {
     spinlock_lock(&handle->event.lock);
-    handle->event.signaled = 1;
+    __atomic_store_n(&handle->event.signaled, 1, __ATOMIC_RELEASE);
     bool need_wake = handle->event.waiters_cnt > 0;
     spinlock_unlock(&handle->event.lock);
     if (need_wake) {
+        /* We could just use `FUTEX_WAKE`, but using `FUTEX_WAKE_BITSET` is more consistent with
+         * `FUTEX_WAIT_BITSET` in `_DkEventWait`. */
         int ret = DO_SYSCALL(futex, &handle->event.signaled, FUTEX_WAKE_BITSET,
                              handle->event.auto_clear ? 1 : INT_MAX, NULL, NULL,
                              FUTEX_BITSET_MATCH_ANY);
@@ -50,7 +52,7 @@ void _DkEventSet(PAL_HANDLE handle) {
 
 void _DkEventClear(PAL_HANDLE handle) {
     spinlock_lock(&handle->event.lock);
-    handle->event.signaled = 0;
+    __atomic_store_n(&handle->event.signaled, 0, __ATOMIC_RELEASE);
     spinlock_unlock(&handle->event.lock);
 }
 
@@ -65,9 +67,14 @@ int _DkEventWait(PAL_HANDLE handle, uint64_t* timeout_us) {
     handle->event.waiters_cnt++;
 
     while (1) {
-        if (handle->event.signaled) {
-            if (handle->event.auto_clear)
-                handle->event.signaled = 0;
+        bool needs_sleep = false;
+        if (handle->event.auto_clear) {
+            needs_sleep = __atomic_exchange_n(&handle->event.signaled, 0, __ATOMIC_ACQ_REL) == 0;
+        } else {
+            needs_sleep = __atomic_load_n(&handle->event.signaled, __ATOMIC_ACQUIRE) == 0;
+        }
+
+        if (!needs_sleep) {
             ret = 0;
             break;
         }
