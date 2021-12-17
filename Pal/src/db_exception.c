e1d738db4c8a43069438f4b321aabb04895ab4a1
Vijay Dhanraj <vijay.dhanraj@intel.com>
2021-12-17 15:20:15 -0800

    fixup! [Pal,LibOS] Move to C11 atomics from GCC built-ins

--- Pal/src/db_exception.c
+++ Pal/src/db_exception.c
@@ -14,17 +14,28 @@
 #include "pal.h"
 #include "pal_error.h"
 #include "pal_internal.h"
+#include "spinlock.h"
+
+static spinlock_t g_handler_lock = INIT_SPINLOCK_UNLOCKED;
 
 static pal_event_handler_t g_handlers[PAL_EVENT_NUM_BOUND] = {0};
 
 pal_event_handler_t _DkGetExceptionHandler(enum pal_event event) {
-    return atomic_load_explicit(&g_handlers[event], memory_order_acquire);
+    pal_event_handler_t handler;
+
+    spinlock_lock(&g_handler_lock);
+    handler = g_handlers[event];
+    spinlock_unlock(&g_handler_lock);
+
+    return handler;
 }
 
 void DkSetExceptionHandler(pal_event_handler_t handler, enum pal_event event) {
     assert(handler && event != PAL_EVENT_NO_EVENT && event < ARRAY_SIZE(g_handlers));
 
-    atomic_store_explicit(&g_handlers[event], handler, memory_order_release);
+    spinlock_lock(&g_handler_lock);
+    g_handlers[event] = handler;
+    spinlock_unlock(&g_handler_lock);
 }
 
 /* The below function is used by stack protector's __stack_chk_fail(), _FORTIFY_SOURCE's *_chk()
