ab8187e46def7ca45b4de08a5fd8be741786f428
Michał Kowalczyk <mkow@invisiblethingslab.com>
2022-01-09 23:51:56 +0100

    fixup! [Pal] Remove `pal_sec` and refactor topology structures
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- Pal/src/host/Linux-SGX/db_misc.c
+++ Pal/src/host/Linux-SGX/db_misc.c
@@ -183,7 +183,7 @@ static inline uint32_t extension_enabled(uint32_t xfrm, uint32_t bit_idx) {
  * \param[in] leaf       CPUID leaf
  * \param[in] subleaf    CPUID subleaf
  * \param[in,out] values untrusted result to sanitize
- * 
+ *
  * The basic idea is that there are only a handful of extensions and we know the size needed to
  * store each extension's state. Use this to sanitize host's untrusted cpuid output. We also know
  * through xfrm what extensions are enabled inside the enclave.
