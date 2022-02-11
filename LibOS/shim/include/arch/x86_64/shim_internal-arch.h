caa2454b24838543ea0a0025e27b1a72958f0aba
Paweł Marczewski <pawel@invisiblethingslab.com>
2022-02-11 15:05:31 +0100

    fixup! [LibOS,Pal/Linux-SGX] Don't use RBX as operand in inline asm
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- LibOS/shim/include/arch/x86_64/shim_internal-arch.h
+++ LibOS/shim/include/arch/x86_64/shim_internal-arch.h
@@ -3,13 +3,20 @@
 #ifndef _SHIM_INTERNAL_ARCH_H_
 #define _SHIM_INTERNAL_ARCH_H_
 
+/*
+ * As explained in glibc (`x86_64/start.S`), the System V ABI expects us to set the following before
+ * jumping to the entry point:
+ *
+ * - RDX: function pointer to be registered with `atexit` (we pass 0)
+ * - RSP: the initial stack, which contains program arguments and environment
+ */
 #define CALL_ELF_ENTRY(ENTRY, ARGP)         \
     do {                                    \
         __asm__ volatile(                   \
             "movq %1, %%rsp\r\n"            \
             "jmp *%0\r\n"                   \
             :                               \
-            : "r"(ENTRY), "r"(ARGP)         \
+            : "r"(ENTRY), "r"(ARGP), "d"(0) \
             : "memory");                    \
         __builtin_unreachable();            \
     } while(0)
