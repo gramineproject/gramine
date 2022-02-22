f53eec95f31fa27d6a5a42ff6664727a2d92ba67
Dong, Xiaocheng <xiaocheng.dong@intel.com>
2022-02-22 04:50:07 -0500

    [Pal,LibOS] Add AMX-relevant `{GET,REQ}_XCOMP` commands to arch_prctl()
    Intel Advanced Matrix Extensions (AMX) are enabled in Linux kernel
    v5.16. This enablement is opt-in for processes: a process that intends
    to use AMX must verify AMX availability with
    `syscall(arch_prctl,ARCH_GET_XCOMP_PERM)` and set the AMX permission
    bit with `syscall(arch_prctl, ARCH_REQ_XCOMP_PERM)`. Without this
    opt-in, any  AMX-related instruction triggers an #NM exception, and
    the process gets SIGILL.
    
    With this commit, Linux PAL unconditionally sets the AMX permission on
    Gramine startup; it may result in slightly worse performance but we do
    it for simplicity. Linux-SGX PAL sets the AMX permission only if
    `AMX_TILEDATA` bit is set in the SECS.ATTRIBUTES.XFRM (i.e., the
    enclave allows AMX support). LibOS emulates `{GET,REQ}_XCOMP` commands
    in `arch_prctl()` syscall effectively as no-ops: LibOS queries CPUID
    whether AMX is supported and returns success if it is (and a
    corresponding error code if AMX is not supported by the host/enclave).
    
    Note that on Linux kernel prior to v5.16, Gramine may enable the AMX
    hardware feature and use it. This should be fine because Gramine
    allocates large-enough signal frames for AMX state and correctly
    saves/restores the whole XSAVE area, including AMX.
    
    Signed-off-by: Dong, Xiaocheng <xiaocheng.dong@intel.com>

--- LibOS/shim/include/arch/x86_64/shim_syscalls.h
+++ LibOS/shim/include/arch/x86_64/shim_syscalls.h
@@ -52,4 +52,19 @@
 #ifndef __NR_syscalls
 #define __NR_syscalls 428
 #endif
+
+/* Linux v5.16 supports Intel AMX. To enable this feature, Linux added several XSTATE-related
+ * arch_prctl() commands. To support Gramine on older Linux kernels, we explicitly define these
+ * commands. See
+ * https://elixir.bootlin.com/linux/v5.16/source/arch/x86/include/uapi/asm/prctl.h */
+#if !defined(ARCH_GET_XCOMP_SUPP)
+#define ARCH_GET_XCOMP_SUPP 0x1021
+#endif
+#if !defined(ARCH_GET_XCOMP_PERM)
+#define ARCH_GET_XCOMP_PERM 0x1022
+#endif
+#if !defined(ARCH_REQ_XCOMP_PERM)
+#define ARCH_REQ_XCOMP_PERM 0x1023
+#endif
+
 #endif /* SHIM_SYSCALLS_H_ */
