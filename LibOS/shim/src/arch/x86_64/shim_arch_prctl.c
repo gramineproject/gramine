1fcd1b45c5be524d1005cad99c910ebfd9a9c44a
Dong, Xiaocheng <xiaocheng.dong@intel.com>
2022-02-22 04:50:07 -0500

    [Pal,LibOS] Add AMX-relevant `{GET,REQ}_XCOMP` commands to arch_prctl()
    Intel Advanced Matrix Extensions (AMX) are enabled in Linux kernel
    v5.16. This enablement is opt-in for processes: a process that intends
    to use AMX must verify AMX availability with
    `syscall(arch_prctl,ARCH_GET_XCOMP_PERM)` and set the AMX permission
    bit with `syscall(arch_prctl, ARCH_REQ_XCOMP_PERM)`. Without this
    opt-in, any  AMX-related instruction triggers an #NM exception, and
    the process gets SIGILL.
    
    With this commit, Linux PAL unconditionally sets the AMX permission on
    Gramine startup; it may result in slightly worse performance but we do
    it for simplicity. Linux-SGX PAL sets the AMX permission only if
    `AMX_TILEDATA` bit is set in the SECS.ATTRIBUTES.XFRM (i.e., the
    enclave allows AMX support). LibOS emulates `{GET,REQ}_XCOMP` commands
    in `arch_prctl()` syscall effectively as no-ops: LibOS queries CPUID
    whether AMX is supported and returns success if it is (and a
    corresponding error code if AMX is not supported by the host/enclave).
    
    Note that on Linux kernel prior to v5.16, Gramine may enable the AMX
    hardware feature and use it. This should be fine because Gramine
    allocates large-enough signal frames for AMX state and correctly
    saves/restores the whole XSAVE area, including AMX.
    
    Signed-off-by: Dong, Xiaocheng <xiaocheng.dong@intel.com>

--- LibOS/shim/src/arch/x86_64/shim_arch_prctl.c
+++ LibOS/shim/src/arch/x86_64/shim_arch_prctl.c
@@ -13,6 +13,10 @@
 #include "shim_tcb.h"
 
 long shim_do_arch_prctl(int code, unsigned long addr) {
+    unsigned int values[CPUID_WORD_NUM];
+    unsigned int amx_mask;
+    int ret;
+
     switch (code) {
         case ARCH_SET_FS:
             set_tls(addr);
@@ -20,6 +24,41 @@ long shim_do_arch_prctl(int code, unsigned long addr) {
 
         case ARCH_GET_FS:
             return pal_to_unix_errno(DkSegmentBaseGet(PAL_SEGMENT_FS, (unsigned long*)addr));
+
+        /* Emulate ARCH_GET_XCOMP_SUPP, ARCH_GET_XCOMP_PERM, ARCH_REQ_XCOMP_PERM by
+         * querying CPUID, it's safe because the PAL already requested AMX permission
+         * at startup */
+        case ARCH_GET_XCOMP_SUPP:
+        case ARCH_GET_XCOMP_PERM:
+            ret = DkCpuIdRetrieve(EXTENDED_STATE_LEAF, 0, values);
+            if (ret < 0) {
+                return pal_to_unix_errno(ret);
+            }
+            *(uint64_t*)addr = values[CPUID_WORD_EAX] | ((uint64_t)values[CPUID_WORD_EDX] << 32);
+            return 0;
+
+        case ARCH_REQ_XCOMP_PERM:
+            /* The request must be the highest state component number related to that facility,
+             * current Linux kernel supports only AMX_TILEDATA (bit 18) */
+            if (addr != AMX_TILEDATA) {
+                log_warning("ARCH_REQ_XCOMP_PERM on unsupported feature %lu requested", addr);
+                return -EOPNOTSUPP;
+            }
+
+            ret = DkCpuIdRetrieve(EXTENDED_STATE_LEAF, 0, values);
+            if (ret < 0) {
+                return pal_to_unix_errno(ret);
+            }
+
+            amx_mask = (1 << AMX_TILECFG) | (1 << AMX_TILEDATA);
+            if ((values[CPUID_WORD_EAX] & amx_mask) != amx_mask) {
+                log_warning("AMX is not supported on this CPU (XSAVE bits are 0x%x)",
+                            values[CPUID_WORD_EAX]);
+                return -EINVAL;
+            }
+
+            /* PAL already requested AMX permission at startup, here just a no-op */
+            return 0;
 
         default:
             log_warning("Not supported flag (0x%x) passed to arch_prctl", code);
