9927b4eb5459f26c8b33031737a02632a8d0f9df
Satyanaraya Illa <satyanaraya.illa@intel.com>
2022-02-26 07:33:53 +0530

    fixup! fixup! fixup! fixup! fixup! [Pal,LibOS] Fix error codes for sendfile and bind syscall flows
    Signed-off-by: Satyanaraya Illa <satyanaraya.illa@intel.com>

--- LibOS/shim/src/sys/shim_file.c
+++ LibOS/shim/src/sys/shim_file.c
@@ -456,7 +456,7 @@ long shim_do_sendfile(int out_fd, int in_fd, off_t* offset, size_t count) {
     }
 
     int mode = out_hdl->flags & O_ACCMODE;
-    if (!((mode == O_WRONLY) || (mode == O_RDWR))) {
+    if (!(mode == O_WRONLY || mode == O_RDWR)) {
         /* Linux errors out if output fd isn't writable */
         ret = -EBADF;
         goto out;
