572d5c6e888c1c5e2fff40f2460e2fce639f0a2d
Kailun Qin <kailun.qin@intel.com>
2022-02-28 09:23:53 -0500

    [Pal] Consolidate db_*.c for Pal/Linux and Pal/Linux-SGX
    `db_*.c` of Pal/Linux and Pal/Linux_SGX are mostly following the same
    logic to implement narrow Drawbridge-like ABI interfaces where the only
    difference is host syscall or ocall. Maintaining them separately may
    lead to inconsistent behaviors when bugs are fixed in one place but not
    in the other and may consequently raise the maintenance cost.
    
    This patch proposes to:
    * share code of `db_*.c` in Pal/Linux and Pal/Linux_SGX
    * have the common code maintained in Pal/Linux-common
    * determine the hostcall type thru conditional compilation
    * leverage the unified hostcall for syscall or ocall
    
    Partially addresses https://github.com/gramineproject/gramine/issues/14
    
    Related to https://github.com/gramineproject/graphene/issues/443
    
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- Pal/src/host/Linux-common/db_devices.c
+++ Pal/src/host/Linux-common/db_devices.c
@@ -11,11 +11,13 @@
  */
 
 #include "api.h"
+#include "hostcall.h"
 #include "pal.h"
 #include "pal_error.h"
 #include "pal_flags_conv.h"
 #include "pal_internal.h"
 #include "pal_linux.h"
+#include "pal_linux_error.h"
 #include "perm.h"
 
 static int dev_open(PAL_HANDLE* handle, const char* type, const char* uri, enum pal_access access,
@@ -54,10 +56,10 @@ static int dev_open(PAL_HANDLE* handle, const char* type, const char* uri, enum 
         /* other devices must be opened through the host */
         hdl->dev.nonblocking = !!(options & PAL_OPTION_NONBLOCK);
 
-        ret = DO_SYSCALL(open, uri, PAL_ACCESS_TO_LINUX_OPEN(access)  |
-                                    PAL_CREATE_TO_LINUX_OPEN(create)  |
-                                    PAL_OPTION_TO_LINUX_OPEN(options),
-                         share);
+        ret = HOSTCALL(open, uri, PAL_ACCESS_TO_LINUX_OPEN(access)  |
+                PAL_CREATE_TO_LINUX_OPEN(create)  |
+                PAL_OPTION_TO_LINUX_OPEN(options),
+                share);
         if (ret < 0) {
             ret = unix_to_pal_error(ret);
             goto fail;
@@ -82,6 +84,12 @@ fail:
 }
 
 static int64_t dev_read(PAL_HANDLE handle, uint64_t offset, uint64_t size, void* buffer) {
+#if defined(DB_LINUX)
+    int64_t bytes;
+#elif defined(DB_LINUX_SGX)
+    ssize_t bytes;
+#endif
+
     if (offset || HANDLE_HDR(handle)->type != PAL_TYPE_DEV)
         return -PAL_ERROR_INVAL;
 
@@ -91,11 +99,17 @@ static int64_t dev_read(PAL_HANDLE handle, uint64_t offset, uint64_t size, void*
     if (handle->dev.fd == PAL_IDX_POISON)
         return -PAL_ERROR_DENIED;
 
-    int64_t bytes = DO_SYSCALL(read, handle->dev.fd, buffer, size);
+    bytes = HOSTCALL(read, handle->dev.fd, buffer, size);
     return bytes < 0 ? unix_to_pal_error(bytes) : bytes;
 }
 
 static int64_t dev_write(PAL_HANDLE handle, uint64_t offset, uint64_t size, const void* buffer) {
+#if defined(DB_LINUX)
+    int64_t bytes;
+#elif defined(DB_LINUX_SGX)
+    ssize_t bytes;
+#endif
+
     if (offset || HANDLE_HDR(handle)->type != PAL_TYPE_DEV)
         return -PAL_ERROR_INVAL;
 
@@ -105,7 +119,7 @@ static int64_t dev_write(PAL_HANDLE handle, uint64_t offset, uint64_t size, cons
     if (handle->dev.fd == PAL_IDX_POISON)
         return -PAL_ERROR_DENIED;
 
-    int64_t bytes = DO_SYSCALL(write, handle->dev.fd, buffer, size);
+    bytes = HOSTCALL(write, handle->dev.fd, buffer, size);
     return bytes < 0 ? unix_to_pal_error(bytes) : bytes;
 }
 
@@ -116,7 +130,7 @@ static int dev_close(PAL_HANDLE handle) {
     /* currently we just assign `0`/`1` FDs without duplicating, so close is a no-op for them */
     int ret = 0;
     if (handle->dev.fd != PAL_IDX_POISON && handle->dev.fd != 0 && handle->dev.fd != 1) {
-        ret = DO_SYSCALL(close, handle->dev.fd);
+        ret = HOSTCALL(close, handle->dev.fd);
     }
     handle->dev.fd = PAL_IDX_POISON;
     return ret < 0 ? unix_to_pal_error(ret) : 0;
@@ -127,7 +141,7 @@ static int dev_flush(PAL_HANDLE handle) {
         return -PAL_ERROR_INVAL;
 
     if (handle->dev.fd != PAL_IDX_POISON) {
-        int ret = DO_SYSCALL(fsync, handle->dev.fd);
+        int ret = HOSTCALL(fsync, handle->dev.fd);
         if (ret < 0)
             return unix_to_pal_error(ret);
     }
@@ -150,9 +164,23 @@ static int dev_attrquery(const char* type, const char* uri, PAL_STREAM_ATTR* att
     } else {
         /* other devices must query the host */
         struct stat stat_buf;
-        int ret = DO_SYSCALL(stat, uri, &stat_buf);
+#if defined(DB_LINUX)
+        int ret = HOSTCALL(stat, uri, &stat_buf);
         if (ret < 0)
             return unix_to_pal_error(ret);
+#elif defined(DB_LINUX_SGX)
+        int fd = ocall_open(uri, 0, 0);
+        if (fd < 0)
+            return unix_to_pal_error(fd);
+
+        int ret = HOSTCALL(fstat, fd, &stat_buf);
+        if (ret < 0) {
+            ocall_close(fd);
+            return unix_to_pal_error(ret);
+        }
+
+        ocall_close(fd);
+#endif
 
         attr->readable     = stataccess(&stat_buf, ACCESS_R);
         attr->writable     = stataccess(&stat_buf, ACCESS_W);
@@ -180,7 +208,7 @@ static int dev_attrquerybyhdl(PAL_HANDLE handle, PAL_STREAM_ATTR* attr) {
     } else {
         /* other devices must query the host */
         struct stat stat_buf;
-        int ret = DO_SYSCALL(fstat, handle->dev.fd, &stat_buf);
+        int ret = HOSTCALL(fstat, handle->dev.fd, &stat_buf);
         if (ret < 0)
             return unix_to_pal_error(ret);
 
