4f415140ee12389e8980b2bea10330c7f51ca14d
Kailun Qin <kailun.qin@intel.com>
2022-03-14 15:17:34 -0400

    [Pal,meson] Link Gramine-patched mbedTLS statically
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- subprojects/packagefiles/mbedtls/compile-gramine.sh
+++ subprojects/packagefiles/mbedtls/compile-gramine.sh
@@ -2,5 +2,9 @@
 
 CFLAGS='-O2 -DMBEDTLS_CONFIG_FILE=\"mbedtls/config-gramine.h\"'
 
+# Generate position-independent code even for a static library, so that it can be used in
+# Gramine-provided shared libraries
+CFLAGS="$CFLAGS -fPIC"
+
 export CFLAGS
 exec "$(dirname "$0")"/compile.sh "$@"
