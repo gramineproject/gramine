86b43030cd9aea5ebe9ace341fe85ac1dd45bd9c
Jakub Kądziołka <niedzejkob@invisiblethingslab.com>
2022-04-06 13:40:11 +0200

    fixup! [CI-Examples] Add an async Rust example
    Signed-off-by: Jakub Kądziołka <niedzejkob@invisiblethingslab.com>

--- CI-Examples/rust/src/main.rs
+++ CI-Examples/rust/src/main.rs
@@ -7,9 +7,12 @@ async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> 
     Ok(Response::new("Hello, World".into()))
 }
 
-// If you wish to use a multithreaded runtime, then depending on how many CPU cores your
-// host has, you might need to adjust sgx.thread_num in the manifest.
-#[tokio::main(flavor = "current_thread")]
+// By default, tokio spawns as many threads as there are CPU cores. This is undesirable,
+// because you need to specify in the Gramine manifest the maximal number of threads per
+// process, and ideally this wouldn't depend on your hardware.
+//
+// See sgx.thread_num in the manifest.
+#[tokio::main(worker_threads = 4)]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
