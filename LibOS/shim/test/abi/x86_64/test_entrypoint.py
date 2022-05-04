ff487600ebd0896e40d3ea5e3351e491f9a82b05
Mariusz Zaborski <oshogbo@invisiblethingslab.com>
2022-05-04 07:20:58 -0400

    [tests] Verify auxiliary
    According to System V Application Binary Interface for
    AMD64 Architecture Processor Supplement the auxiliary variables
    should start just after environment variables, so at
    'rsp + 8 + 8 * (argc + 1) + 8 * (envp_count + 1) + 8'.
    
    The Supplement doesn't specify which auxiliary should be set.
    In this test, we verify the ones tha Gramine currently sets.
    If developers add new ones, the test won't fail. In case
    of removing one, the test will fail. The test also verifies
    duplicates.
    
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>

--- LibOS/shim/test/abi/x86_64/test_entrypoint.py
+++ LibOS/shim/test/abi/x86_64/test_entrypoint.py
@@ -23,3 +23,6 @@ class TC_00_Entrypoint(RegressionTestCase):
 
     def test_070_env(self):
         self.run_binary(['stack_env'])
+
+    def test_080_auxv(self):
+        self.run_binary(['stack_auxiliary'])
