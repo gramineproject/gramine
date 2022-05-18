643e228855bf3545eb894c983479ceffcfff76b4
aneessahib <anees.a.sahib@intel.com>
2022-05-18 16:20:21 +0530

    fixup! fixup! fixup! fixup! fixup! fixup! [LibOS] Add support to run scripts in  execve()

--- LibOS/shim/src/shim_rtld.c
+++ LibOS/shim/src/shim_rtld.c
@@ -745,12 +745,12 @@ err:
 static int load_elf_header(struct shim_handle* file, elf_ehdr_t* ehdr) {
     int ret = read_file_fragment(file, ehdr, sizeof(*ehdr), /*offset=*/0);
     if (ret < 0) {
-        return ret;
+        return -ENOEXEC;
     }
 
     ret = check_elf_header(ehdr);
     if (ret < 0) {
-        return ret;
+        return -ENOEXEC;
     }
 
     return 0;
