3ff0563769a51d3e0bf6b155329d6803048a4382
Stefan Berger <stefanb@linux.ibm.com>
2022-05-25 14:26:55 +0000

    [LibOS] Move procfs cpuinfo display code into functions
    Move the code for generating the output of /proc/cpuinfo so that it
    is easier to adapt it to other architectures.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- LibOS/shim/src/fs/proc/info.c
+++ LibOS/shim/src/fs/proc/info.c
@@ -121,17 +121,94 @@ retry:
 
 #define ADD_INFO(fmt, ...)                                            \
     do {                                                              \
-        int ret = print_to_str(&str, size, &max, fmt, ##__VA_ARGS__); \
+        int ret = print_to_str(str, *size, max, fmt, ##__VA_ARGS__);  \
         if (ret < 0) {                                                \
             free(str);                                                \
             return ret;                                               \
         }                                                             \
-        size += ret;                                                  \
+        *size += ret;                                                  \
     } while (0)
+
+#if defined(__x86_64__)
+static int proc_cpuinfo_display_cpu(char** str, size_t* size, size_t* max,
+                                    const struct pal_topo_info* topo,
+                                    const struct pal_cpu_info* cpu, size_t i,
+                                    struct pal_cpu_thread_info* thread) {
+    struct pal_cpu_core_info* core = &topo->cores[thread->core_id];
+    /* Below strings must match exactly the strings retrieved from /proc/cpuinfo
+     * (see Linux's arch/x86/kernel/cpu/proc.c) */
+    ADD_INFO("processor\t: %lu\n",   i);
+    ADD_INFO("vendor_id\t: %s\n",    cpu->cpu_vendor);
+    ADD_INFO("cpu family\t: %lu\n",  cpu->cpu_family);
+    ADD_INFO("model\t\t: %lu\n",     cpu->cpu_model);
+    ADD_INFO("model name\t: %s\n",   cpu->cpu_brand);
+    ADD_INFO("stepping\t: %lu\n",    cpu->cpu_stepping);
+    ADD_INFO("physical id\t: %zu\n", core->socket_id);
+
+    /* Linux keeps this numbering socket-local, but we can use a different one, and it's
+     * documented as "hardware platform's identifier (rather than the kernel's)" anyways. */
+    ADD_INFO("core id\t\t: %lu\n",   thread->core_id);
+
+    size_t cores_in_socket = 0;
+    for (size_t j = 0; j < topo->cores_cnt; j++) { // slow, but shouldn't matter
+        if (topo->cores[j].socket_id == core->socket_id)
+            cores_in_socket++;
+    }
+    ADD_INFO("cpu cores\t: %zu\n", cores_in_socket);
+    double bogomips = cpu->cpu_bogomips;
+    // Apparently Gramine snprintf cannot into floats.
+    ADD_INFO("bogomips\t: %lu.%02lu\n", (unsigned long)bogomips,
+             (unsigned long)(bogomips * 100.0 + 0.5) % 100);
+    ADD_INFO("\n");
+
+    return 0;
+}
+
+static int proc_cpuinfo_display_tail(char** str, size_t* size, size_t* max,
+                                     const struct pal_cpu_info* cpu) {
+    __UNUSED(str);
+    __UNUSED(size);
+    __UNUSED(max);
+    __UNUSED(cpu);
+
+    return 0;
+}
+#endif
+
+#if defined(__powerpc64__)
+static int proc_cpuinfo_display_cpu(char** str, size_t* size, size_t* max,
+                                    const struct pal_topo_info* topo,
+                                    const struct pal_cpu_info* cpu, size_t i,
+                                    struct pal_cpu_thread_info* thread) {
+    __UNUSED(topo);
+    __UNUSED(thread);
+
+    ADD_INFO("processor\t: %lu\n", i);
+    ADD_INFO("cpu\t\t: %s\n", cpu->cpu);
+    ADD_INFO("clock\t\t: %s\n", cpu->clock);
+    ADD_INFO("revision\t: %s\n", cpu->revision);
+    ADD_INFO("\n");
+
+    return 0;
+}
+
+static int proc_cpuinfo_display_tail(char** str, size_t* size, size_t* max,
+                                     const struct pal_cpu_info* cpu) {
+    ADD_INFO("timebase\t: %s\n", cpu->timebase);
+    ADD_INFO("platform\t: %s\n", cpu->platform);
+    ADD_INFO("model\t\t: %s\n", cpu->model);
+    ADD_INFO("machine\t\t: %s\n", cpu->machine);
+    ADD_INFO("firmware\t: %s\n", cpu->firmware);
+    ADD_INFO("MMU\t\t: %s\n", cpu->mmu);
+
+    return 0;
+}
+#endif
 
 int proc_cpuinfo_load(struct shim_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
 
+    int ret;
     size_t size = 0;
     size_t max = 128;
     char* str = malloc(max);
@@ -146,38 +223,31 @@ int proc_cpuinfo_load(struct shim_dentry* dent, char** out_data, size_t* out_siz
             /* Offline cores are skipped in cpuinfo, with gaps in numbering. */
             continue;
         }
-        struct pal_cpu_core_info* core = &topo->cores[thread->core_id];
-        /* Below strings must match exactly the strings retrieved from /proc/cpuinfo
-         * (see Linux's arch/x86/kernel/cpu/proc.c) */
-        ADD_INFO("processor\t: %lu\n",   i);
-        ADD_INFO("vendor_id\t: %s\n",    cpu->cpu_vendor);
-        ADD_INFO("cpu family\t: %lu\n",  cpu->cpu_family);
-        ADD_INFO("model\t\t: %lu\n",     cpu->cpu_model);
-        ADD_INFO("model name\t: %s\n",   cpu->cpu_brand);
-        ADD_INFO("stepping\t: %lu\n",    cpu->cpu_stepping);
-        ADD_INFO("physical id\t: %zu\n", core->socket_id);
-
-        /* Linux keeps this numbering socket-local, but we can use a different one, and it's
-         * documented as "hardware platform's identifier (rather than the kernel's)" anyways. */
-        ADD_INFO("core id\t\t: %lu\n",   thread->core_id);
-
-        size_t cores_in_socket = 0;
-        for (size_t j = 0; j < topo->cores_cnt; j++) { // slow, but shouldn't matter
-            if (topo->cores[j].socket_id == core->socket_id)
-                cores_in_socket++;
-        }
-        ADD_INFO("cpu cores\t: %zu\n", cores_in_socket);
-        double bogomips = cpu->cpu_bogomips;
-        // Apparently Gramine snprintf cannot into floats.
-        ADD_INFO("bogomips\t: %lu.%02lu\n", (unsigned long)bogomips,
-                 (unsigned long)(bogomips * 100.0 + 0.5) % 100);
-        ADD_INFO("\n");
+        ret = proc_cpuinfo_display_cpu(&str, &size, &max, topo, cpu, i, thread);
+        if (ret)
+            return ret;
     }
+
+    ret = proc_cpuinfo_display_tail(&str, &size, &max, cpu);
+    if (ret)
+        return ret;
 
     *out_data = str;
     *out_size = size;
     return 0;
 }
+
+#undef ADD_INFO
+
+#define ADD_INFO(fmt, ...)                                            \
+    do {                                                              \
+        int ret = print_to_str(&str, size, &max, fmt, ##__VA_ARGS__);  \
+        if (ret < 0) {                                                \
+            free(str);                                                \
+            return ret;                                               \
+        }                                                             \
+        size += ret;                                                  \
+    } while (0)
 
 int proc_stat_load(struct shim_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
