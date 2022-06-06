87504d4bbc40da17159e26368205d362b70ba3a2
Borys Popławski <borysp@invisiblethingslab.com>
2022-06-06 15:52:42 +0200

    fixup! [LibOS,Pal] Completely rewrite networking code
    Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

--- LibOS/shim/src/sys/shim_open.c
+++ LibOS/shim/src/sys/shim_open.c
@@ -48,12 +48,6 @@ long shim_do_read(int fd, void* buf, size_t count) {
     if (!hdl)
         return -EBADF;
 
-    /* sockets may read from LibOS buffer due to MSG_PEEK, so need to call socket-specific recv */
-    if (hdl->type == TYPE_SOCK) {
-        put_handle(hdl);
-        return shim_do_recvfrom(fd, buf, count, 0, NULL, NULL);
-    }
-
     ssize_t ret = do_handle_read(hdl, buf, count);
     put_handle(hdl);
     if (ret == -EINTR) {
