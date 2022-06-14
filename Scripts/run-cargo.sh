d18411d9f2a209063b22a32d4dd0de931b2087fa
Jakub Kądziołka <niedzejkob@invisiblethingslab.com>
2022-06-14 20:37:38 +0200

    fixup! [Pal/Linux-SGX] Implement gramine-sgx-get-token in Rust
    Signed-off-by: Jakub Kądziołka <niedzejkob@invisiblethingslab.com>

--- Scripts/run-cargo.sh
+++ Scripts/run-cargo.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
 
 # The main issue this script needs to work around is that meson's custom_target doesn't
 # like slashes in the output argument, so we need to move the output from cargo's `target`
@@ -7,19 +9,37 @@
 # The alternative would be to use cargo's --out-dir option, however said option is currently
 # unstable (see https://github.com/rust-lang/cargo/issues/6790).
 
+# Moreover, we need to set up the settings normally controlled by the Cargo profile (debug vs release)
+# in a way that makes sense with the more fine-grained control that Meson exposes
+# in `meson configure`.
+
 INPUT="$1"
 OUTPUT="$2"
 export CARGO_TARGET_DIR="$3"
-BUILD_TYPE="$4"
-shift 4
+DEBUG="$4"
+OPTLEVEL="$5"
+shift 5
 
-# For historical reasons, the profile that outputs to target/debug is called dev.
-if [ "$BUILD_TYPE" = "debug" ]; then
-    PROFILE="dev"
-else
-    PROFILE="$BUILD_TYPE"
+# Rust doesn't understand opt-level=g.
+if [ "$OPTLEVEL" = "g" ]; then
+    OPTLEVEL=0
 fi
 
-cargo build --manifest-path "$INPUT" --profile "$PROFILE" "$@"
-cp "$CARGO_TARGET_DIR/$BUILD_TYPE/$(basename "$OUTPUT")" "$OUTPUT"
-cp "$CARGO_TARGET_DIR/$BUILD_TYPE/$(basename "$OUTPUT").d" "$OUTPUT.d"
+export CARGO_PROFILE_MESON_OPT_LEVEL="$OPTLEVEL"
+export CARGO_PROFILE_MESON_DEBUG="$DEBUG"
+
+# We don't want bounds-checking for -Dbuildtype=debugoptimized.
+if [ "$DEBUG" = "true" ] && [ "$OPTLEVEL" = 0 ]; then
+    export CARGO_PROFILE_MESON_DEBUG_ASSERTIONS=true
+    export CARGO_PROFILE_MESON_OVERFLOW_CHECKS=true
+fi
+
+# Unoptimized builds are more likely to be used for quick iteration, where incremental
+# compilation will be useful.
+if [ "$OPTLEVEL" = 0 ]; then
+    export CARGO_PROFILE_MESON_INCREMENTAL=true
+fi
+
+cargo build --manifest-path "$INPUT" --profile "meson" "$@"
+cp "$CARGO_TARGET_DIR/meson/$(basename "$OUTPUT")" "$OUTPUT"
+cp "$CARGO_TARGET_DIR/meson/$(basename "$OUTPUT").d" "$OUTPUT.d"
