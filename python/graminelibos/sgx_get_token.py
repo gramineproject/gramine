6b90c1c5e50f6bb6563e305b6c5a93a4f8649f0c
Jakub Kądziołka <niedzejkob@invisiblethingslab.com>
2022-06-29 17:39:24 +0200

    [python] Compile aesm_pb2.py during build
    Keeping the compiled protobuf output in the repository is a dirty hack,
    which has finally started to break because of some breaking changes made
    by protobuf's upstream. Do this properly instead.
    
    Signed-off-by: Jakub Kądziołka <niedzejkob@invisiblethingslab.com>

--- python/graminelibos/sgx_get_token.py
+++ python/graminelibos/sgx_get_token.py
@@ -6,7 +6,7 @@ import hashlib
 import socket
 import struct
 
-from . import _aesm_pb2 as aesm_pb2
+from . import aesm_pb2 # pylint: disable=import-error,no-name-in-module
 from . import _offsets as offs # pylint: disable=import-error,no-name-in-module
 
 def get_optional_sgx_features(sig):
