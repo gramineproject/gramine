e296d4e475f3a8aab4f005ffcce4a84bb6a844c4
Kailun Qin <kailun.qin@intel.com>
2022-06-29 11:11:03 -0400

    [Pal] Move shared functions to Linux-common header
    `file_stat_type`, `file_attrcopy` and `is_dot_or_dotdot` are shared between
    pal/linux and pal/linux-sgx, hence can be moved to Linux-common header.
    
    Fixes https://github.com/gramineproject/gramine/issues/696
    
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- pal/include/host/linux-common/linux_utils.h
+++ pal/include/host/linux-common/linux_utils.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <asm/stat.h>
 #include <linux/time.h>
 #include <linux/un.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdnoreturn.h>
+
+#include "pal.h"
+#include "stat.h"
 
 char* get_main_exec_path(void);
 
@@ -44,3 +48,30 @@ int64_t time_ns_diff_from_now(struct timespec* ts);
 
 int get_gramine_unix_socket_addr(uint64_t instance_id, const char* name,
                                  struct sockaddr_un* out_addr);
+
+static inline int file_stat_type(struct stat* stat) {
+    if (S_ISREG(stat->st_mode))
+        return PAL_TYPE_FILE;
+    if (S_ISDIR(stat->st_mode))
+        return PAL_TYPE_DIR;
+    if (S_ISCHR(stat->st_mode))
+        return PAL_TYPE_DEV;
+    if (S_ISFIFO(stat->st_mode))
+        return PAL_TYPE_PIPE;
+    if (S_ISSOCK(stat->st_mode))
+        return PAL_TYPE_DEV;
+
+    return 0;
+}
+
+/* copy attr content from POSIX stat struct to PAL_STREAM_ATTR */
+static inline void file_attrcopy(PAL_STREAM_ATTR* attr, struct stat* stat) {
+    attr->handle_type  = file_stat_type(stat);
+    attr->nonblocking  = false;
+    attr->share_flags  = stat->st_mode & PAL_SHARE_MASK;
+    attr->pending_size = stat->st_size;
+}
+
+static inline bool is_dot_or_dotdot(const char* name) {
+    return (name[0] == '.' && !name[1]) || (name[0] == '.' && name[1] == '.' && !name[2]);
+}
