1ae61c06ed853dddc67341f2c88a23102d725da0
aneessahib <anees.a.sahib@intel.com>
2022-07-12 15:01:51 +0530

    fixup! Modify the secret provisioning example and the libsecret_prov_attest.so library to terminate upon unsuccessful attestation

--- CI-Examples/ra-tls-secret-prov/src/secret_prov_server.c
+++ CI-Examples/ra-tls-secret-prov/src/secret_prov_server.c
@@ -107,13 +107,13 @@ int main(int argc, char** argv) {
     if (argc < 2) {
         puts("--- Proceeding without a master key ---");
         static_assert(sizeof(g_secret_string) >= sizeof(ATTESTATION_SUCCESS_STRING),
-        "size of g_secret_string is too small");
+                      "size of g_secret_string is too small");
         strcpy(g_secret_string, ATTESTATION_SUCCESS_STRING);
     } else {
-        printf("--- Reading the master key for encrypted files from '%s' ---", argv[1]);
+        printf("--- Reading the master key for encrypted files from '%s' ---\n", argv[1]);
         int fd = open(argv[1], O_RDONLY);
         if (fd < 0) {
-            fprintf(stderr, "[error] cannot read %s\n", argv[1]);
+            fprintf(stderr, "[error] cannot open %s\n", argv[1]);
             close(fd);
             return 1;
         }
