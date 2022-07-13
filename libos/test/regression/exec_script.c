b16ead857e9cf2f58fb06ee014b85c74629ce219
aneessahib <anees.a.sahib@intel.com>
2022-07-13 22:53:02 +0530

    [LibOS] Override `argv[0]` with file pathname for interpreter scripts
    According to POSIX, if the pathname argument of `execve()` specifies an
    interpreter script, then interpreter will be invoked with the following
    arguments:
    
        interpreter [optional-arg] pathname arg...
    
    where `pathname` is the pathname of the file specified as the first
    argument of `execve()`, and `arg...` is the series of words pointed to
    by the `argv` argument of `execve()`, starting at `argv[1]`.
    
    Signed-off-by: aneessahib <anees.a.sahib@intel.com>

--- libos/test/regression/exec_script.c
+++ libos/test/regression/exec_script.c
@@ -3,7 +3,11 @@
 #include <unistd.h>
 
 int main(void) {
-    char* const argv[] = {(char*)"scripts/foo.sh", (char*)"STRING FROM EXECVE", NULL};
-    execv(argv[0], argv);
+    char* const argv[] = {(char*)"foo.sh", (char*)"STRING FROM EXECVE", NULL};
+
+    /* The pathname arg to `execv()` differs from `argv[0]` on purpose: we want to test the corner
+     * case where `argv[0]` is overridden by the file pathname for interpreter scripts in execve
+     * family of syscalls. */
+    execv("scripts/foo.sh", argv);
     err(EXIT_FAILURE, "execve failed");
 }
