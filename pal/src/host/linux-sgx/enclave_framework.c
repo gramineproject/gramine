71150ad28f5bb1a0b26371f75c50140188542e36
Borys Popławski <borysp@invisiblethingslab.com>
2022-09-06 12:22:15 +0200

    [PAL/Linux-SGX] Mitigate CVE-2022-21233 aka INTEL-SA-00657
    If malicious host maps APIC registers in some untrusted memory (MMIO)
    and this memory is subsequently read by the enclave (e.g. when reading
    some results of an ocall) such read can potentially return some stale,
    enclave-originated data. Naturally aligned 8-byte reads are not affected
    by this vulnerability, so as software mitigation, this commit makes all
    untrusted memory accesses 8-byte wide and naturally aligned.
    
    As a side effect the exitless ocalls feature becomes potentially
    insecure. It uses untrusted memory in some complex manner, which we did
    not bother changing for now, as the feature is not widely used.
    
    Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

--- pal/src/host/linux-sgx/enclave_framework.c
+++ pal/src/host/linux-sgx/enclave_framework.c
@@ -129,13 +129,74 @@ void sgx_reset_ustack(const void* old_ustack) {
     UPDATE_USTACK(old_ustack);
 }
 
+static void copy_u64s(void* dst, const void* untrusted_src, size_t count) {
+    assert((uintptr_t)untrusted_src % 8 == 0);
+    __asm__ volatile (
+        "rep movsq\n"
+        :
+        : "D"(dst), "S"(untrusted_src), "c"(count)
+        : "memory", "cc"
+    );
+}
+
+void sgx_copy_to_enclave_verified(void* ptr, const void* uptr, size_t size) {
+    assert(sgx_is_valid_untrusted_ptr(uptr, size, /*alignment=*/1));
+    assert(sgx_is_completely_within_enclave(ptr, size));
+
+    if (size == 0) {
+        return;
+    }
+
+    /*
+     * This should be simple `memcpy(ptr, uptr, size)`, but CVE-2022-21233 (INTEL-SA-00657).
+     * To mitigate this issue, all reads of untrusted memory from within the enclave must be done
+     * in 8-byte chunks aligned to 8-bytes boundary. Since x64 allocates memory in pages of
+     * (at least) 0x1000 in size, we can safely 8-align the pointer down and the size up.
+     */
+    size_t copy_len;
+    size_t prefix_misalignment = (uintptr_t)uptr & 7;
+    if (prefix_misalignment) {
+        /* Beginning of the copied range is misaligned. */
+        uint64_t prefix_val = 0;
+        copy_u64s(&prefix_val, (char*)uptr - prefix_misalignment, /*count=*/1);
+
+        copy_len = MIN(sizeof(prefix_val) - prefix_misalignment, size);
+        memcpy(ptr, (char*)&prefix_val + prefix_misalignment, copy_len);
+        ptr = (char*)ptr + copy_len;
+        uptr = (const char*)uptr + copy_len;
+        size -= copy_len;
+
+        if (size == 0) {
+            return;
+        }
+    }
+    assert((uintptr_t)uptr % 8 == 0);
+
+    size_t suffix_misalignment = size & 7;
+    copy_len = size - suffix_misalignment;
+    assert(copy_len % 8 == 0);
+    copy_u64s(ptr, uptr, copy_len / 8);
+    ptr = (char*)ptr + copy_len;
+    uptr = (const char*)uptr + copy_len;
+    size -= copy_len;
+
+    assert(size == suffix_misalignment);
+    if (suffix_misalignment) {
+        /* End of the copied range is misaligned. */
+        uint64_t suffix_val = 0;
+        copy_u64s(&suffix_val, uptr, /*count=*/1);
+        memcpy(ptr, &suffix_val, suffix_misalignment);
+    }
+}
+
 bool sgx_copy_to_enclave(void* ptr, size_t maxsize, const void* uptr, size_t usize) {
     if (usize > maxsize ||
         !sgx_is_valid_untrusted_ptr(uptr, usize, /*alignment=*/1) ||
         !sgx_is_completely_within_enclave(ptr, maxsize)) {
         return false;
     }
-    memcpy(ptr, uptr, usize);
+
+    sgx_copy_to_enclave_verified(ptr, uptr, usize);
     return true;
 }
 
