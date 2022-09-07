4ee8bdcae81a00823ec14e0a77ffb376b213140b
Kailun Qin <kailun.qin@intel.com>
2022-09-06 21:02:13 -0400

    fixup! [tools/sgx,curl,Docs] Minimize and build libcurl statically to link with `tools/sgx`
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- subprojects/packagefiles/mbedtls/compile-curl.sh
+++ subprojects/packagefiles/mbedtls/compile-curl.sh
@@ -10,8 +10,6 @@ PRIVATE_DIR="$4"
 SUBPROJ_ROOT="$5"
 shift 5
 
-MBEDTLS_CURL_DIR="$SUBPROJ_ROOT"/mbedtls-curl
-
 rm -rf "$PRIVATE_DIR"
 
 cp -ar "$VENDOR_SOURCE_DIR" "$PRIVATE_DIR"
@@ -19,5 +17,5 @@ cp "$CURRENT_SOURCE_DIR"/include/mbedtls/*.h "$PRIVATE_DIR"/include/mbedtls/
 patch -p1 --directory "$PRIVATE_DIR" <"$CURRENT_SOURCE_DIR"/gramine.patch
 patch -p1 --directory "$PRIVATE_DIR" <"$CURRENT_SOURCE_DIR"/fcntl.patch
 
-make -C "$PRIVATE_DIR" lib SUFFIX="''" install DESTDIR="$MBEDTLS_CURL_DIR"
-touch "$MBEDTLS_CURL_DIR"/mbedtls-curl-dummy.h
+make -C "$PRIVATE_DIR" lib SUFFIX="''" install DESTDIR="$SUBPROJ_ROOT"/mbedtls-curl
+touch "$CURRENT_BUILD_DIR"/mbedtls-curl-dummy.h
