65bba7ab9afabe84ae4ad813ee4e482eb95df6f9
Kailun Qin <kailun.qin@intel.com>
2022-09-06 21:25:58 -0400

    fixup! [tools/sgx,curl,Docs] Minimize and build libcurl statically to link with `tools/sgx`
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- subprojects/packagefiles/mbedtls/compile-curl.sh
+++ subprojects/packagefiles/mbedtls/compile-curl.sh
@@ -10,6 +10,17 @@ PRIVATE_DIR="$4"
 SUBPROJ_ROOT="$5"
 shift 5
 
+OUTPUTS=""
+while test "$#" -gt 0 && ! test "$1" = --
+do
+    OUTPUTS="$OUTPUTS $1"
+    shift
+done
+if test "$1" = --
+then
+    shift
+fi
+
 rm -rf "$PRIVATE_DIR"
 
 cp -ar "$VENDOR_SOURCE_DIR" "$PRIVATE_DIR"
@@ -17,5 +28,10 @@ cp "$CURRENT_SOURCE_DIR"/include/mbedtls/*.h "$PRIVATE_DIR"/include/mbedtls/
 patch -p1 --directory "$PRIVATE_DIR" <"$CURRENT_SOURCE_DIR"/gramine.patch
 patch -p1 --directory "$PRIVATE_DIR" <"$CURRENT_SOURCE_DIR"/fcntl.patch
 
-make -C "$PRIVATE_DIR" lib SUFFIX="''" install DESTDIR="$SUBPROJ_ROOT"/mbedtls-curl
-touch "$CURRENT_BUILD_DIR"/mbedtls-curl-dummy.h
+make -C "$PRIVATE_DIR" lib "$@" install DESTDIR="$SUBPROJ_ROOT"/mbedtls-curl
+touch "$PRIVATE_DIR"/library/mbedtls-curl-dummy.h
+
+for output in $OUTPUTS
+do
+    cp -a "$PRIVATE_DIR"/library/"$(basename "$output")" "$output"
+done
