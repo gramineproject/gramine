d5f04615a54a0238ff5d84e68a9843b566a4c042
Mariusz Zaborski <oshogbo@invisiblethingslab.com>
2022-09-21 17:55:05 -0400

    [LibOS,PAL] Extend DNS runtime configuration
    This commit adds support for edns0, use-vc, and ndots in /etc/resolv.conf.
    In case of ndots, untrusted PAL tries to behave in similar manner to GLIBC
    and corrects invalid values.
    
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>

--- pal/src/host/linux-common/etc_host_info.c
+++ pal/src/host/linux-common/etc_host_info.c
@@ -280,10 +280,21 @@ static void resolv_options_setter(struct pal_dns_host_conf* conf, const char* pt
     memcpy(option, ptr, length);
     option[length] = 0x00;
 
-    if (strcmp(option, "inet6") == 0) {
+    if (strcmp(option, "edns0") == 0) {
+        conf->edns0 = true;
+    } else if (strcmp(option, "inet6") == 0) {
         conf->inet6 = true;
+    } else if (strncmp(option, "ndots:", sizeof("ndots:") - 1) == 0) {
+        long ndots = strtoll(option + sizeof("ndots:") - 1, NULL, 10);
+        if (ndots > PAL_MAX_NDOTS)
+            ndots = PAL_MAX_NDOTS;
+        if (ndots < 0)
+            ndots = 0;
+        conf->ndots = ndots;
     } else if (strcmp(option, "rotate") == 0) {
         conf->rotate = true;
+    } else if (strcmp(option, "use-vc") == 0) {
+        conf->use_vc = true;
     }
 }
 
@@ -300,8 +311,14 @@ static struct {
     { "options",    resolv_options },
 };
 
+static void conf_init_defaults_options(struct pal_dns_host_conf* conf) {
+    conf->ndots = 1;
+}
+
 static void parse_resolv_buf_conf(struct pal_dns_host_conf* conf, const char* buf) {
     const char* ptr = buf;
+
+    conf_init_defaults_options(conf);
 
     /*
      * From resolv.conf(5):
