2a0d0e32b1abf29421e5c6e664b63777011bbaf9
Stefan Berger <stefanb@linux.ibm.com>
2022-10-06 18:02:22 -0400

    fixup! fixup! [common] Fix value of strtol/strtoll endptr if nothing was converted

--- pal/regression/strtoll_test.c
+++ pal/regression/strtoll_test.c
@@ -13,24 +13,24 @@ int main(int argc, const char** argv) {
 
     long long a = strtoll(ptr, (char**)&next, 10);
     if (a != 123 || next != ptr + 3) {
-        FAIL("Wrong return values in %d, got (a, ptr, next) = (%lld, %c, %p), expected (%d, %c, %p)",
-             __LINE__, a, *ptr, next, 123, '1', ptr + 3);
+        FAIL("Wrong return values in %d, got (a, next) = (%lld, %p), expected (%d, %p)",
+             __LINE__, a, next, 123, ptr + 3);
     }
     ptr = next;
 
     /* nothing to convert */
     a = strtoll(ptr, (char**)&next, 10);
     if (a != 0 || next != ptr) {
-        FAIL("Wrong return values in %d, got (a, ptr, next) = (%lld, %c, %p), expected (%d, %c, %p)",
-             __LINE__, a, *ptr, next, 0, ' ', ptr);
+        FAIL("Wrong return values in %d, got (a, next) = (%lld, %p), expected (%d, %p)",
+             __LINE__, a, next, 0, ptr);
     }
 
     ptr = " ";
     next = ptr;
     a = strtoll(ptr, (char**)&next, 10);
     if (a != 0 || next != ptr) {
-        FAIL("Wrong return values in %d, got (a, ptr, next) = (%lld, %c, %p), expected (%d, %c, %p)",
-             __LINE__, a, *ptr, next, 0, ' ', ptr);
+        FAIL("Wrong return values in %d, got (a, next) = (%lld, %p), expected (%d, %p)",
+             __LINE__, a, next, 0, ptr);
     }
 
     pal_printf("TEST OK\n");
