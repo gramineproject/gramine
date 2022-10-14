9ae42b1a1f837f992a56508c3cac7bcfb9ccc68b
Stefan Berger <stefanb@linux.ibm.com>
2022-10-14 11:07:49 -0400

    fixup! [LibOS] Add support for execve with argv = NULL

--- libos/test/regression/exec_null.c
+++ libos/test/regression/exec_null.c
@@ -2,7 +2,7 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int main(int argc, const char** argv, const char** envp) {
+int main(void) {
     syscall(__NR_execve, "./exec_victim", NULL, NULL);
     return 1;
 }
