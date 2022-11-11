d3210636e356d7ba1a4f3baf3ad1d0225488d23e
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2022-11-11 06:13:49 -0800

    DONTMERGE [tools/sgx] Interoperable RA-TLS
    Interoperable RA-TLS is a spec that allows different RA-TLS
    implementations (from different SGX frameworks, e.g. Gramine and Occlum)
    to interoperate and recognize each others' SGX evidence (SGX quotes and
    attached SGX claims). For example, Gramine app enclave can establish a
    TLS connection with an Occlum app enclave and verify its SGX evidence.
    
    The spec standardizes the OID extension for X.509 certs that is used for
    the SGX evidence. It also standardizes the format of the OID contents: a
    CBOR-formatted tag with an array that contains the SGX quote and a dict
    of related claims (with the most important dict item being the public
    key hash encoded as a CBOR array `hash-entry`).
    
    Current RA-TLS implementation creates X.509 certs that have both the old
    (legacy) OID with plain SGX quote as well as the new (standardized) OID
    with the CBOR-formatted SGX evidence. Thus, backward compatibility is
    preserved at a small cost of larger-sized certs.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- tools/sgx/ra-tls/ra_tls_verify_epid.c
+++ tools/sgx/ra-tls/ra_tls_verify_epid.c
@@ -121,6 +121,8 @@ int ra_tls_verify_callback(void* data, mbedtls_x509_crt* crt, int depth, uint32_
     (void)data;
 
     int ret;
+    sgx_quote_t* quote = NULL;
+
     struct ias_context_t* ias = NULL;
     char* ias_pub_key_pem     = NULL;
 
@@ -160,9 +162,8 @@ int ra_tls_verify_callback(void* data, mbedtls_x509_crt* crt, int depth, uint32_
         goto out;
 
     /* extract SGX quote from "quote" OID extension from crt */
-    sgx_quote_t* quote;
     size_t quote_size;
-    ret = extract_quote_and_verify_pubkey(crt, &quote, &quote_size);
+    ret = extract_quote_and_verify_claims(crt, &quote, &quote_size);
     if (ret < 0)
         goto out;
 
@@ -256,6 +257,7 @@ out:
     if (ias)
         ias_cleanup(ias);
 
+    free(quote);
     free(ias_pub_key_pem);
     free(quote_from_ias);
     free(report_data);
