cc2734511f85f1c1bd96c99d265c6e670e5eadf2
Mariusz Zaborski <oshogbo@invisiblethingslab.com>
2023-01-04 10:36:41 -0500

    fixup! [common] Make logs more verbose
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>

--- common/include/log.h
+++ common/include/log.h
@@ -14,10 +14,31 @@ enum {
     LOG_LEVEL_ALL     = 5,
 };
 
-/* All of them implicitly append a newline at the end of the message. */
-#define log_always(fmt...)   _log(LOG_LEVEL_NONE, __FILE__, __func__, __LINE__, fmt)
-#define log_error(fmt...)    _log(LOG_LEVEL_ERROR, __FILE__, __func__, __LINE__, fmt)
-#define log_warning(fmt...)  _log(LOG_LEVEL_WARNING, __FILE__, __func__, __LINE__, fmt)
-#define log_debug(fmt...)    _log(LOG_LEVEL_DEBUG, __FILE__, __func__, __LINE__, fmt)
+/*
+ * __FILE_NAME__ was introduced in GCC11 and clang9.
+ * If it's not defined we have to do our own magic.
+ */
+#ifndef __FILE_NAME__
+static inline const char* truncate_file_name(const char *filename) {
+    const char *ret = filename;
 
-#define log_trace(fmt...)    _log(LOG_LEVEL_TRACE, __FILE__, __func__, __LINE__, fmt)
+    while (*filename != '\0') {
+        if (*filename == '/') {
+            ret = filename + 1;
+        }
+        filename++;
+    }
+
+    return ret;
+}
+
+#define __FILE_NAME__ truncate_file_name(__FILE__)
+#endif
+
+/* All of them implicitly append a newline at the end of the message. */
+#define log_always(fmt...)   _log(LOG_LEVEL_NONE, __FILE_NAME__, __func__, __LINE__, fmt)
+#define log_error(fmt...)    _log(LOG_LEVEL_ERROR, __FILE_NAME__, __func__, __LINE__, fmt)
+#define log_warning(fmt...)  _log(LOG_LEVEL_WARNING, __FILE_NAME__, __func__, __LINE__, fmt)
+#define log_debug(fmt...)    _log(LOG_LEVEL_DEBUG, __FILE_NAME__, __func__, __LINE__, fmt)
+
+#define log_trace(fmt...)    _log(LOG_LEVEL_TRACE, __FILE_NAME__, __func__, __LINE__, fmt)
