3c5273f5cbf58dc47e30ac9b8239709882c63529
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-01-25 05:26:24 -0800

    fixup! [PAL/Linux-SGX] Print message on EACCES during SGX driver open
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- pal/src/host/linux-sgx/host_framework.c
+++ pal/src/host/linux-sgx/host_framework.c
@@ -32,8 +32,8 @@ int open_sgx_driver(void) {
     for (size_t i = 0; i < ARRAY_SIZE(paths_to_try); i++) {
         ret = DO_SYSCALL(open, paths_to_try[i], O_RDWR | O_CLOEXEC, 0);
         if (ret == -EACCES) {
-            log_error("Cannot open %s (permission denied). This may happen because this device "
-                      "has insufficient permissions for the current user.", paths_to_try[i]);
+            log_error("Cannot open %s (permission denied). This may happen because the current "
+                      "user has insufficient permissions to this device.", paths_to_try[i]);
             return ret;
         }
         if (ret >= 0) {
