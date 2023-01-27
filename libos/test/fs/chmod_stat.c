0e3eabe813af9a8a20e15dd53d44aa1c7665ee56
Jaewon Hur <hurjaewon@snu.ac.kr>
2023-01-27 01:47:42 +0000

    fixup! fixup! [LibOS] Add chmod test
    Signed-off-by: Jaewon Hur <hurjaewon@snu.ac.kr>

--- libos/test/fs/chmod_stat.c
+++ libos/test/fs/chmod_stat.c
@@ -2,7 +2,7 @@
 
 static void chmod_stat(const char* file_path) {
     struct stat st;
-    uint32_t perm, expected_perm;
+    uint32_t perm, expected_perm = (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
     int fd = open_output_fd(file_path, /*rdwr*/true);
 
@@ -10,18 +10,28 @@ static void chmod_stat(const char* file_path) {
         fatal_error("Failed to stat file %s: %s\n", file_path, strerror(errno));
 
     perm = st.st_mode & ACCESSPERMS;
-    expected_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
     if (perm != expected_perm)
         fatal_error("File permission is different (expected: %o, actual: %o)\n", expected_perm, perm);
 
-    if (chmod(file_path, (st.st_mode & ~S_IWUSR)))
+    expected_perm &= ~S_IWUSR;
+    if (chmod(file_path, expected_perm))
         fatal_error("chmod failed\n");
 
     if (stat(file_path, &st) != 0)
         fatal_error("Failed to stat file %s: %s\n", file_path, strerror(errno));
 
     perm = st.st_mode & ACCESSPERMS;
-    expected_perm = S_IRUSR | S_IRGRP | S_IROTH;
+    if (perm != expected_perm)
+        fatal_error("File permission is different (expected: %o, actual: %o)\n", expected_perm, perm);
+
+    expected_perm &= ~S_IRGRP;
+    if (fchmod(fd, expected_perm))
+        fatal_error("fchmod failed\n");
+
+    if (stat(file_path, &st) != 0)
+        fatal_error("Failed to stat file %s: %s\n", file_path, strerror(errno));
+
+    perm = st.st_mode & ACCESSPERMS;
     if (perm != expected_perm)
         fatal_error("File permission is different (expected: %o, actual: %o)\n", expected_perm, perm);
 
