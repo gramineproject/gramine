558bed4ba7c0b755691d95d669bf052606158780
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-01-27 01:35:49 -0800

    fixup! [tests] Add "execute" subtest to `memory` test
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- pal/regression/memory.c
+++ pal/regression/memory.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
-/* Copyright (C) 2022 Intel Corporation
+/* Copyright (C) 2023 Intel Corporation
  *                    Borys Popławski <borysp@invisiblethingslab.com>
+ *                    Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
  */
 
 #include <stdbool.h>
@@ -10,12 +11,12 @@
 #include "pal.h"
 #include "pal_regression.h"
 
-static void* g_exec_addr;
+static void (*g_mem_exec_func)(void);
+
 static bool g_exec_failed;
 static bool g_write_failed;
 static bool g_read_failed;
 
-void mem_exec(void* addr) __attribute__((visibility("internal")));
 void mem_write(void* addr, uint8_t val) __attribute__((visibility("internal")));
 uint8_t mem_read(void* addr) __attribute__((visibility("internal")));
 static bool is_pc_at_func(uintptr_t pc, void (*func)(void));
@@ -29,7 +30,6 @@ __asm__ (
 ".pushsection .text\n"
 ".type mem_write, @function\n"
 ".type mem_read, @function\n"
-".type mem_exec, @function\n"
 ".type ret, @function\n"
 ".type end_of_ret, @function\n"
 "mem_write:\n"
@@ -37,9 +37,6 @@ __asm__ (
     "ret\n"
 "mem_read:\n"
     "movb (%rdi), %al\n"
-    "ret\n"
-"mem_exec:\n"
-    "call *(%rdi)\n"
     "ret\n"
 "ret:\n"
     "ret\n"
@@ -70,7 +67,7 @@ static void fixup_context_after_read(PAL_CONTEXT* context) {
 
 static void memfault_handler(bool is_in_pal, uintptr_t addr, PAL_CONTEXT* context) {
     uintptr_t pc = pal_context_get_ip(context);
-    if (pc == (uintptr_t)g_exec_addr) {
+    if (is_pc_at_func(pc, g_mem_exec_func)) {
         fixup_context_after_exec(context);
         g_exec_failed = true;
         return;
@@ -105,11 +102,11 @@ int main(void) {
     CHECK(memory_alloc(PAGE_SIZE * 3, PAL_PROT_READ | PAL_PROT_WRITE | PAL_PROT_EXEC, &addr1));
 
     memcpy(addr1, ret, (uintptr_t)end_of_ret - (uintptr_t)ret);
-    g_exec_addr = addr1;
+    g_mem_exec_func = (void (*)(void))addr1;
 
     g_exec_failed = false;
     COMPILER_BARRIER();
-    mem_exec(&addr1);
+    g_mem_exec_func();
     COMPILER_BARRIER();
     if (g_exec_failed) {
         log_error("exec on RWX mem at %p failed", addr1);
@@ -120,7 +117,7 @@ int main(void) {
 
     g_exec_failed = false;
     COMPILER_BARRIER();
-    mem_exec(&addr1);
+    g_mem_exec_func();
     COMPILER_BARRIER();
     if (!g_exec_failed) {
         log_error("exec on RW mem at %p unexpectedly succeeded", addr1);
@@ -128,7 +125,7 @@ int main(void) {
     }
 
     memset(addr1, 0, (uintptr_t)end_of_ret - (uintptr_t)ret);
-    g_exec_addr = NULL;
+    g_mem_exec_func = NULL;
 
     g_write_failed = false;
     COMPILER_BARRIER();
