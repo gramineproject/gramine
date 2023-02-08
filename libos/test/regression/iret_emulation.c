c6d480073040ca4de27bc4cd85816cced54e4847
Mariusz Zaborski <oshogbo@invisiblethingslab.com>
2023-02-08 18:49:56 -0500

    [Pal] Emulate `iret` assembly instruction
    Intel SGX prohibits the `iret` instruction, however some
    applications (like ones based on ASP.NET) use it.
    When `iret` is used exception will be generated. This commit adds
    emulation of this instruction in the exception handler.
    
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>

---
+++ libos/test/regression/iret_emulation.c
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2023 Intel Corporation
+ *                    Mariusz Zaborski <oshogbo@invisiblethingslab.com>
+ */
+
+#define _GNU_SOURCE 1
+
+#include <err.h>
+#include <signal.h>
+#include <stdio.h>
+#include <ucontext.h>
+
+static void sigfpe_handler(int sig, siginfo_t* info, void* arg) {
+    ucontext_t* context = (ucontext_t*)arg;
+
+    __asm__ volatile (
+        "mov %%ss, %%rax\n"
+        "pushq %%rax\n"
+        "pushq %0\n"
+        "pushq $0x00\n"
+        "pushq %1\n"
+        "pushq %2\n"
+        "movq %3, %%rbp\n"
+        "iretq\n"
+        :
+        : "r" (context->uc_mcontext.gregs[REG_RSP]),
+          "r" (context->uc_mcontext.gregs[REG_CSGSFS] & 0xFF),
+          "r" (context->uc_mcontext.gregs[REG_RIP] + 3),
+          "r" (context->uc_mcontext.gregs[REG_RBP])
+        : "rax"
+    );
+}
+
+int main(void) {
+    struct sigaction action = { 0 };
+
+    action.sa_sigaction = &sigfpe_handler;
+    action.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGFPE, &action, NULL) < 0) {
+        err(1, "unable to set singal handler");
+    }
+
+    __asm__ inline volatile (
+        "mov $0, %rax\n"
+        "mov $0, %rdx\n"
+        "idiv %rdx"
+    );
+
+    printf("TEST OK\n");
+    return 0;
+}
