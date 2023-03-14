5add3021efd295918dca0dc6306d0bc8f438616e
Nirjhar Roy <nirjhar.roy@fortanix.com>
2023-03-14 09:06:17 +0000

    fixup
    Signed-off-by: Nirjhar Roy <nirjhar.roy@fortanix.com>

--- common/src/arch/x86_64/cpu.c
+++ common/src/arch/x86_64/cpu.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
-/* Copyright (C) 2022 Fortanix Inc
+/* Copyright (C) 2023 Fortanix Inc
  *                    Nirjhar Roy <nirjhar.roy@fortanix.com>
  */
 
@@ -12,11 +12,11 @@
 
 bool is_x86_instr_legacy_prefix(uint8_t op) {
     uint8_t prefix_list[] = {
-                             /* Group 0 */
+                             /* Group 1 */
                              0xf0, /* LOCK prefix */
                              0xf2, /* REPNE/REPNZ prefix */
                              0xf3, /* REP or REPE/REPZ prefix */
-                             /* Group 1 */
+                             /* Group 2 */
                              0x2e, /* CS segment override */
                              0x36, /* SS segment override */
                              0x3e, /* DS segment override */
@@ -25,9 +25,9 @@ bool is_x86_instr_legacy_prefix(uint8_t op) {
                              0x65, /* GS segment override */
                              0x2e, /* Branch not taken */
                              0x3e, /* Branch taken */
-                             /* Group 2 */
-                             0x66, /*  Operand-size override prefix */
                              /* Group 3 */
+                             0x66, /*  Operand-size override prefix */
+                             /* Group 4 */
                              0x67, /* Address-size override prefix  */
                             };
     for (size_t i = 0; i < ARRAY_SIZE(prefix_list); i++) {
