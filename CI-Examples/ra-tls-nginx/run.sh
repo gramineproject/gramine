5664cea54d7a0dfb5e1e650006cd47b0d88b487a
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-06-27 04:18:47 -0700

    [LibOS] Add SIGTERM signal to list of pending signals
    Previously, `get_all_pending_signals()` helper function did not check
    for the SIGTERM signal (which has a special handling via
    `g_host_injected_signal`). Syscalls like `rt_sigsuspend()` and
    `rt_sigtimedwait()` use this helper function. Because of that erroneous
    behavior, apps like Nginx would wait for signals on one of these
    syscalls but would *not* react to SIGTERM (i.e., won't react to `kill
    <app>`). This commit fixes this bug, which in turns fixes nginx and
    ra-tls-nginx examples.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- CI-Examples/ra-tls-nginx/run.sh
+++ CI-Examples/ra-tls-nginx/run.sh
@@ -10,7 +10,16 @@ sleep 10
 test "$(curl --insecure -s https://localhost:8000/)" = "$(cat html/index.html)"
 ret=$?
 
-kill -9 $pid
+# verify that SIGTERM terminates nginx with all its sub-processes
+kill $pid
+sleep 1
+
+if test -f /proc/$pid/exe
+then
+    echo "Gramine nginx server (PID=$pid) is still running"
+    echo FAIL
+    exit 1
+fi
 
 if test $ret -eq 0
 then
