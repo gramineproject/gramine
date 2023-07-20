8b8da94742e1e4779f25152eddad65b0c7881362
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-07-20 03:37:52 -0700

    fixup! [common] Do not expand `__FILE_NAME__` to our func call
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- common/include/log.h
+++ common/include/log.h
@@ -29,42 +29,27 @@ static inline const char* truncate_file_name(const char* filename) {
 }
 #define __FILE_NAME__ (truncate_file_name(__FILE__))
 #else  // ifndef __OPTIMIZE__
-/* Below macro will be optimized at compile-time, incurring no perf overhead at runtime. */
+/* Below macros will be optimized at compile-time, incurring no perf overhead at runtime. */
+#define TRY_TRUNCATE_FILE_NAME_AT_LOC(s, loc) \
+    (sizeof(s) > (loc) && (s)[sizeof(s)-(loc)] == '/') ? ((s) + sizeof(s) - (loc) + 1)
 #define TRUNCATE_FILE_NAME(s) \
-    (sizeof(s) > 2 && (s)[sizeof(s)-2] == '/' ? (s) + sizeof(s) - 1 : \
-     sizeof(s) > 3 && (s)[sizeof(s)-3] == '/' ? (s) + sizeof(s) - 2 : \
-     sizeof(s) > 4 && (s)[sizeof(s)-4] == '/' ? (s) + sizeof(s) - 3 : \
-     sizeof(s) > 5 && (s)[sizeof(s)-5] == '/' ? (s) + sizeof(s) - 4 : \
-     sizeof(s) > 6 && (s)[sizeof(s)-6] == '/' ? (s) + sizeof(s) - 5 : \
-     sizeof(s) > 7 && (s)[sizeof(s)-7] == '/' ? (s) + sizeof(s) - 6 : \
-     sizeof(s) > 8 && (s)[sizeof(s)-8] == '/' ? (s) + sizeof(s) - 7 : \
-     sizeof(s) > 9 && (s)[sizeof(s)-9] == '/' ? (s) + sizeof(s) - 8 : \
-     sizeof(s) > 10 && (s)[sizeof(s)-10] == '/' ? (s) + sizeof(s) - 9 : \
-     sizeof(s) > 11 && (s)[sizeof(s)-11] == '/' ? (s) + sizeof(s) - 10 : \
-     sizeof(s) > 12 && (s)[sizeof(s)-12] == '/' ? (s) + sizeof(s) - 11 : \
-     sizeof(s) > 13 && (s)[sizeof(s)-13] == '/' ? (s) + sizeof(s) - 12 : \
-     sizeof(s) > 14 && (s)[sizeof(s)-14] == '/' ? (s) + sizeof(s) - 13 : \
-     sizeof(s) > 15 && (s)[sizeof(s)-15] == '/' ? (s) + sizeof(s) - 14 : \
-     sizeof(s) > 16 && (s)[sizeof(s)-16] == '/' ? (s) + sizeof(s) - 15 : \
-     sizeof(s) > 17 && (s)[sizeof(s)-17] == '/' ? (s) + sizeof(s) - 16 : \
-     sizeof(s) > 18 && (s)[sizeof(s)-18] == '/' ? (s) + sizeof(s) - 17 : \
-     sizeof(s) > 19 && (s)[sizeof(s)-19] == '/' ? (s) + sizeof(s) - 18 : \
-     sizeof(s) > 20 && (s)[sizeof(s)-20] == '/' ? (s) + sizeof(s) - 19 : \
-     sizeof(s) > 21 && (s)[sizeof(s)-21] == '/' ? (s) + sizeof(s) - 20 : \
-     sizeof(s) > 22 && (s)[sizeof(s)-22] == '/' ? (s) + sizeof(s) - 21 : \
-     sizeof(s) > 23 && (s)[sizeof(s)-23] == '/' ? (s) + sizeof(s) - 22 : \
-     sizeof(s) > 24 && (s)[sizeof(s)-24] == '/' ? (s) + sizeof(s) - 23 : \
-     sizeof(s) > 25 && (s)[sizeof(s)-25] == '/' ? (s) + sizeof(s) - 24 : \
-     sizeof(s) > 26 && (s)[sizeof(s)-26] == '/' ? (s) + sizeof(s) - 25 : \
-     sizeof(s) > 27 && (s)[sizeof(s)-27] == '/' ? (s) + sizeof(s) - 26 : \
-     sizeof(s) > 28 && (s)[sizeof(s)-28] == '/' ? (s) + sizeof(s) - 27 : \
-     sizeof(s) > 29 && (s)[sizeof(s)-29] == '/' ? (s) + sizeof(s) - 28 : \
-     sizeof(s) > 30 && (s)[sizeof(s)-30] == '/' ? (s) + sizeof(s) - 29 : \
-     sizeof(s) > 31 && (s)[sizeof(s)-31] == '/' ? (s) + sizeof(s) - 30 : \
-     sizeof(s) > 32 && (s)[sizeof(s)-32] == '/' ? (s) + sizeof(s) - 31 : \
-     sizeof(s) > 33 && (s)[sizeof(s)-33] == '/' ? (s) + sizeof(s) - 32 : \
-     sizeof(s) > 34 && (s)[sizeof(s)-34] == '/' ? (s) + sizeof(s) - 33 : \
-     sizeof(s) > 35 && (s)[sizeof(s)-35] == '/' ? (s) + sizeof(s) - 34 : (s))
+    (TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 1)  : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 2)  : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 3)  : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 4)  : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 5)  : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 6)  : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 7)  : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 8)  : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 9)  : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 10) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 11) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 12) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 13) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 14) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 15) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 16) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 17) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 18) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 19) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 20) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 21) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 22) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 23) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 24) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 25) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 26) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 27) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 28) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 29) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 30) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 31) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 32) : \
+     TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 33) : TRY_TRUNCATE_FILE_NAME_AT_LOC(s, 34) : (s))
 #define __FILE_NAME__ (TRUNCATE_FILE_NAME(__FILE__))
 #endif // ifndef __OPTIMIZE__
 #endif // #ifndef __FILE_NAME__
