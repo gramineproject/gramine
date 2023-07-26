f76f398d5995ad318222814c7cdae6b4f865bdc9
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-07-26 00:13:40 -0700

    fixup! [LibOS] Add SIGTERM signal to list of pending signals
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- CI-Examples/ra-tls-nginx/run.sh
+++ CI-Examples/ra-tls-nginx/run.sh
@@ -10,11 +10,8 @@ sleep 10
 test "$(curl --insecure -s https://localhost:8000/)" = "$(cat html/index.html)"
 ret=$?
 
-# wait till SIGTERM terminates nginx with all its sub-processes
 kill $pid
-while $(kill -0 $pid 2> /dev/null); do
-    sleep 1
-done
+wait $pid
 
 if test $ret -eq 0
 then
