d5338a677aad639aef6dd0599277f5cdb6abeef5
Kailun Qin <kailun.qin@intel.com>
2023-08-24 08:42:32 +0800

    fixup! [PAL/Linux-SGX] Disallow memfault handling if `sgx.require_exinfo = false`
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- pal/regression/test_pal.py
+++ pal/regression/test_pal.py
@@ -203,25 +203,32 @@ class TC_10_Exception(RegressionTestCase):
 
     @unittest.skipUnless(ON_X86, "x86-specific")
     def test_000_exception(self):
-        _, stderr = self.run_binary(['Exception'])
+        try:
+            _, stderr = self.run_binary(['Exception'])
+            if HAS_SGX and not HAS_EDMM:
+                self.fail('expected to return nonzero')
+        except subprocess.CalledProcessError as e:
+            if HAS_SGX and not HAS_EDMM:
+                self.assertEqual(e.returncode, 1)
+                stderr = e.stderr.decode()
+            else:
+                self.fail('expected to return zero')
+        finally:
+            self.assertTrue(self.is_altstack_different_from_main_stack(stderr))
 
-        self.assertTrue(self.is_altstack_different_from_main_stack(stderr))
+            # Exception Handling (Div-by-Zero)
+            self.assertIn('Arithmetic Exception Handler 1', stderr)
+            self.assertIn('Arithmetic Exception Handler 2', stderr)
 
-        # Exception Handling (Div-by-Zero)
-        self.assertIn('Arithmetic Exception Handler', stderr)
+            # Exception Handling (Red zone)
+            self.assertIn('Arithmetic Exception Handler 3', stderr)
+            self.assertIn('Red zone test ok.', stderr)
 
-        # Exception Handling (Memory Fault)
-        self.assertIn('Memory Fault Exception Handler', stderr)
+            if not HAS_SGX or HAS_EDMM:
+                # Exception Handling (Memory Fault)
+                self.assertIn('Memory Fault Exception Handler', stderr)
+                self.assertNotIn('Faulting address not match', stderr)
 
-        # Exception Handler Swap
-        self.assertIn('Arithmetic Exception Handler 1', stderr)
-        self.assertIn('Arithmetic Exception Handler 2', stderr)
-
-        # Exception Handling (Set Context)
-        self.assertIn('Arithmetic Exception Handler 1', stderr)
-
-        # Exception Handling (Red zone)
-        self.assertIn('Red zone test ok.', stderr)
 
 class TC_20_SingleProcess(RegressionTestCase):
     def test_000_exit_code(self):
