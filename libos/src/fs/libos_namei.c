5b6c9e90cdaff76fb8f45d0e7e15708877862e08
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-09-27 06:44:46 -0700

    fixup! [LibOS,PAL] Refactor LibOS's `/dev/tty` and PAL's corresponding `dev:tty`
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- libos/src/fs/libos_namei.c
+++ libos/src/fs/libos_namei.c
@@ -407,9 +407,7 @@ int dentry_open(struct libos_handle* hdl, struct libos_dentry* dent, int flags) 
 
     /* truncate regular writable file if O_TRUNC is given */
     if ((flags & O_TRUNC) && ((flags & O_RDWR) | (flags & O_WRONLY))
-            && (dent->inode->type != S_IFCHR)
-            && (dent->inode->type != S_IFDIR)
-            && (dent->inode->type != S_IFLNK)) {
+            && (dent->inode->type == S_IFREG)) {
         if (!(fs->fs_ops && fs->fs_ops->truncate))
             return -EINVAL;
 
