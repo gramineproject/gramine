0db1053bf4019b446706de1e72235518ee74b3a2
Kailun Qin <kailun.qin@intel.com>
2024-02-01 02:47:31 -0500

    [LibOS] Re-register async event when a periodic itimer expires
    Previously, the async event was registered only once on `setitimer()`
    syscall and the event's callback blindly reset the interval to 0 when
    an interval timer expired. This broke the functionality of a periodic
    itimer which fired once rather than periodically.
    
    This commit fixes the above issue by re-regitering an async event in the
    itimer event callback if periodic. A LibOS regression test is added.
    
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

---
+++ libos/test/regression/itimer.c
@@ -0,0 +1,60 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2024 Intel Corporation
+ *                    Kailun Qin <kailun.qin@intel.com>
+ */
+
+/* Test for syscalls that gets/sets value of an interval timer (`getitimer()`, `setitimer()`). */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#include "common.h"
+
+#define EXPECTED_ITIMER_COUNT 5
+
+int g_itimer_count = 0;
+
+static void itimer_handler(int signum) {
+    ++g_itimer_count;
+
+    if (g_itimer_count >= EXPECTED_ITIMER_COUNT) {
+        struct itimerval timer = {0};
+        CHECK(setitimer(ITIMER_REAL, &timer, NULL));
+    }
+}
+
+int main(void) {
+    struct sigaction sa = {0};
+    sa.sa_handler = itimer_handler;
+    CHECK(sigaction(SIGALRM, &sa, NULL));
+
+    /* configure the timer to expire after 1 sec, and then every 1 sec */
+    struct itimerval timer = {
+        .it_value.tv_sec     = 1,
+        .it_value.tv_usec    = 0,
+        .it_interval.tv_sec  = 1,
+        .it_interval.tv_usec = 0,
+    };
+
+    setitimer(ITIMER_REAL, &timer, NULL);
+
+    struct itimerval current_timer;
+    getitimer(ITIMER_REAL, &current_timer);
+    if (current_timer.it_interval.tv_sec != 1 || current_timer.it_interval.tv_usec != 0)
+        errx(1, "getitimer: unexpected values");
+
+    while (1) {
+        if (g_itimer_count >= EXPECTED_ITIMER_COUNT) {
+            break;
+        }
+    }
+
+    if (g_itimer_count != EXPECTED_ITIMER_COUNT)
+        errx(1, "expected itimer count = %d, but got only %d", EXPECTED_ITIMER_COUNT,
+             g_itimer_count);
+
+    puts("TEST OK");
+    return 0;
+}
--- libos/test/regression/test_libos.py
+++ libos/test/regression/test_libos.py
@@ -992,6 +992,10 @@ class TC_30_Syscall(RegressionTestCase):
                 os.remove('tmp/flock_file2')
         self.assertIn('TEST OK', stdout)
 
+    def test_150_itimer(self):
+        stdout, _ = self.run_binary(['itimer'])
+        self.assertIn("TEST OK", stdout)
+
 class TC_31_Syscall(RegressionTestCase):
     def test_000_syscall_redirect(self):
         stdout, _ = self.run_binary(['syscall'])
