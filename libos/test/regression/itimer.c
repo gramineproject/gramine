18b3590a3f5e523e864b09a0b4f40f1b7f43cfc3
Kailun Qin <kailun.qin@intel.com>
2024-02-20 23:12:37 -0500

    squash! [LibOS] Re-register async event when a periodic itimer expires
    !TODO: use below commit msg
    
    [LibOS] Re-register async event when a periodic itimer expires
    
    Previously, the async event was registered only once on `setitimer()`
    syscall and the event's callback blindly reset the interval to 0 when
    an interval timer expired. This broke the functionality of a periodic
    itimer: the timer was incorrectly fired only once rather than
    periodically.
    
    This commit fixes the above issue by re-registering an async event in
    the itimer event callback if periodic. A LibOS regression test is added.
    
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- libos/test/regression/itimer.c
+++ libos/test/regression/itimer.c
@@ -13,9 +13,10 @@
 
 #include "common.h"
 
-#define EXPECTED_ITIMER_COUNT 5
+#define TEST_MIN_ITIMER_COUNT 5
 #define TEST_PERIODIC_INTERVAL_MS 100
 #define TIME_US_IN_MS 1000ul
+#define TIME_US_IN_S  1000000ul
 
 static int g_itimer_count = 0;
 
@@ -31,24 +32,21 @@ int main(void) {
      * arrival of each time interval */
     suseconds_t periodic_interval_usec = TEST_PERIODIC_INTERVAL_MS * TIME_US_IN_MS;
     struct itimerval timer = {
-        .it_value.tv_sec     = 0,
-        .it_value.tv_usec    = periodic_interval_usec,
-        .it_interval.tv_sec  = 0,
-        .it_interval.tv_usec = periodic_interval_usec,
+        .it_value.tv_sec     = periodic_interval_usec / TIME_US_IN_S,
+        .it_value.tv_usec    = periodic_interval_usec % TIME_US_IN_S,
+        .it_interval.tv_sec  = periodic_interval_usec / TIME_US_IN_S,
+        .it_interval.tv_usec = periodic_interval_usec % TIME_US_IN_S,
     };
 
     CHECK(setitimer(ITIMER_REAL, &timer, NULL));
 
-    while (__atomic_load_n(&g_itimer_count, __ATOMIC_RELAXED) < EXPECTED_ITIMER_COUNT)
+    while (__atomic_load_n(&g_itimer_count, __ATOMIC_RELAXED) < TEST_MIN_ITIMER_COUNT)
         ;
-
-    if (g_itimer_count != EXPECTED_ITIMER_COUNT)
-        errx(1, "expected itimer count = %d, but got %d", EXPECTED_ITIMER_COUNT, g_itimer_count);
 
     struct itimerval current_timer = {0};
     CHECK(getitimer(ITIMER_REAL, &current_timer));
-    if (current_timer.it_interval.tv_sec != 0 ||
-        current_timer.it_interval.tv_usec != periodic_interval_usec)
+    if (current_timer.it_interval.tv_sec != (time_t)(periodic_interval_usec / TIME_US_IN_S) ||
+        current_timer.it_interval.tv_usec != (suseconds_t)(periodic_interval_usec % TIME_US_IN_S))
         errx(1, "getitimer: unexpected values");
 
     puts("TEST OK");
