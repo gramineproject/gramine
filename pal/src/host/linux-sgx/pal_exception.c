5a51f890384af891ba53a749603ac82cab69df3d
Kailun Qin <kailun.qin@intel.com>
2024-04-04 05:54:10 -0400

    fixup! [LibOS,PAL/Linux-SGX] Add EDMM lazy allocation support
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- pal/src/host/linux-sgx/pal_exception.c
+++ pal/src/host/linux-sgx/pal_exception.c
@@ -462,11 +462,11 @@ void _PalExceptionHandler(uint32_t trusted_exit_info_,
             prot_flags &= ~PAL_PROT_LAZYALLOC;
 
             assert(ctx.err);
-            if (((ctx.err & ERRCD_W) && !(prot_flags & PAL_PROT_WRITE)) ||
+            if ((!(prot_flags & PAL_PROT_READ) && !(prot_flags & PAL_PROT_WRITE) &&
+                 !(prot_flags & PAL_PROT_EXEC)) || /* check if prot_flags is NONE */
+                ((ctx.err & ERRCD_W) && !(prot_flags & PAL_PROT_WRITE)) ||
                 ((ctx.err & ERRCD_I) && !(prot_flags & PAL_PROT_EXEC)) ||
-                /* insufficient read access, e.g., read on a `PRTO_NONE` page */
-                (!(ctx.err & ERRCD_W) && !(ctx.err & ERRCD_I) && !(prot_flags & PAL_PROT_READ)) ||
-                 (ctx.err & ERRCD_PK) || (ctx.err & ERRCD_SS)) {
+                (ctx.err & ERRCD_PK) || (ctx.err & ERRCD_SS)) {
                 /* the memfault can be caused by e.g. insufficient access rights rather than page
                  * not existing, which should be propagated in this case */
                 goto propagate_memfault;
