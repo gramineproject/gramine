09d74f292f2d415d6cb75571ab83d4143fb8cdf7
Mariusz Zaborski <oshogbo@invisiblethingslab.com>
2024-04-08 13:27:32 +0200

    [build] Enable C23 when avilable and nodiscard feature
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>

---
+++ common/include/nodiscard.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* Copyright (C) 2024 Intel Corporation
+ *                    Mariusz Zaborski <oshogbo@invisiblethingslab.com>
+ */
+
+/*
+ * This file contains a definition for NODISCARD, a macro that wraps
+ * the nodiscard attribute introduced in C23. However, because Gramine supports
+ * older systems that might not have support for C23, we have to wrap it on our
+ * own and change it to a no-op on systems that don't support it.
+ */
+
+#pragma once
+
+#if defined(__has_c_attribute)
+#if __has_c_attribute(nodiscard)
+#define NODISCARD [[nodiscard]]
+#endif
+#endif
+
+#ifndef NODISCARD
+#define NODISCARD
+#endif
