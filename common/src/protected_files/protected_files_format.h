ae14b3db790b8a7ea0f0c290ab6436100f11bc02
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2024-04-11 05:56:43 -0700

    [common] Refactor Protected Files slightly
    This commit refactors PF code without changing functionality:
    - Many renames of data structs, variable names, func names
    - More readable comments (and fixing incorrect comments)
    - Removed unused `pf_get_handle()`
    - Removed unused `g_pf_mrenclave_key`, `g_pf_wrap_key`, etc.
    - Removed all mentions of `list`/`LIST` operations (were unused)
    
    This commit contains changes (renames) in `pf_tamper.c`, but this file
    and corresponding tool are not tested and most probably broken. We did
    the changes purely to survive compilation.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- common/src/protected_files/protected_files_format.h
+++ common/src/protected_files/protected_files_format.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#pragma pack(push, 1)
+
 #ifdef USE_STDLIB
 #include <assert.h>
 #else
@@ -14,7 +16,6 @@
 
 #include <limits.h>
 
-#include "list.h"
 #include "protected_files.h"
 
 #define PF_FILE_ID       0x46505f5346415247 /* GRAFS_PF */
@@ -23,117 +24,102 @@
 
 #define METADATA_KEY_NAME "SGX-PROTECTED-FS-METADATA-KEY"
 #define MAX_LABEL_SIZE    64
-
 static_assert(sizeof(METADATA_KEY_NAME) <= MAX_LABEL_SIZE, "label too long");
-
-#pragma pack(push, 1)
-
-typedef struct _metadata_plain {
-    uint64_t   file_id;
-    uint8_t    major_version;
-    uint8_t    minor_version;
-    pf_keyid_t metadata_key_id;
-    pf_mac_t   metadata_gmac; /* GCM mac */
-} metadata_plain_t;
 
 #define PATH_MAX_SIZE (260 + 512)
 
-// these are all defined as relative to node size, so we can decrease node size in tests
-// and have deeper tree
-#define MD_USER_DATA_SIZE (PF_NODE_SIZE * 3 / 4) // 3072
+#define MD_USER_DATA_SIZE (PF_NODE_SIZE * 3 / 4)
 static_assert(MD_USER_DATA_SIZE == 3072, "bad struct size");
 
-typedef struct _metadata_encrypted {
-    char     path[PATH_MAX_SIZE];
-    uint64_t size;
-    pf_key_t mht_key;
-    pf_mac_t mht_gmac;
-    uint8_t  data[MD_USER_DATA_SIZE];
-} metadata_encrypted_t;
+#define MAX_PAGES_IN_CACHE 48
 
-typedef uint8_t metadata_encrypted_blob_t[sizeof(metadata_encrypted_t)];
+enum {
+    FILE_MHT_NODE_TYPE  = 1,
+    FILE_DATA_NODE_TYPE = 2,
+};
 
-#define METADATA_NODE_SIZE PF_NODE_SIZE
-
-typedef uint8_t metadata_padding_t[METADATA_NODE_SIZE -
-                                   (sizeof(metadata_plain_t) + sizeof(metadata_encrypted_blob_t))];
-
-typedef struct _metadata_node {
-    metadata_plain_t          plain_part;
-    metadata_encrypted_blob_t encrypted_part;
-    metadata_padding_t        padding;
-} metadata_node_t;
-
-static_assert(sizeof(metadata_node_t) == PF_NODE_SIZE, "sizeof(metadata_node_t)");
-
-typedef struct _data_node_crypto {
+typedef struct _gcm_crypto_data {
     pf_key_t key;
     pf_mac_t gmac;
 } gcm_crypto_data_t;
 
-// for PF_NODE_SIZE == 4096, we have 96 attached data nodes and 32 mht child nodes
-// for PF_NODE_SIZE == 2048, we have 48 attached data nodes and 16 mht child nodes
-// for PF_NODE_SIZE == 1024, we have 24 attached data nodes and 8 mht child nodes
-// 3/4 of the node size is dedicated to data nodes
+// for PF_NODE_SIZE == 4096, we have 96 attached data nodes and 32 MHT child nodes
+// 3/4 of the node is dedicated to data nodes, 1/4 to MHT nodes
 #define ATTACHED_DATA_NODES_COUNT ((PF_NODE_SIZE / sizeof(gcm_crypto_data_t)) * 3 / 4)
+#define CHILD_MHT_NODES_COUNT     ((PF_NODE_SIZE / sizeof(gcm_crypto_data_t)) * 1 / 4)
 static_assert(ATTACHED_DATA_NODES_COUNT == 96, "ATTACHED_DATA_NODES_COUNT");
-// 1/4 of the node size is dedicated to child mht nodes
-#define CHILD_MHT_NODES_COUNT ((PF_NODE_SIZE / sizeof(gcm_crypto_data_t)) * 1 / 4)
 static_assert(CHILD_MHT_NODES_COUNT == 32, "CHILD_MHT_NODES_COUNT");
+
+typedef struct _metadata_plaintext_header {
+    uint64_t   file_id;
+    uint8_t    major_version;
+    uint8_t    minor_version;
+    pf_salt_t  metadata_key_salt;
+    pf_mac_t   metadata_gmac; /* GCM mac */
+} metadata_plaintext_header_t;
+
+typedef struct _metadata_decrypted_header {
+    char     file_path[PATH_MAX_SIZE];
+    uint64_t file_size;
+    pf_key_t root_mht_node_key;
+    pf_mac_t root_mht_node_gmac;
+    uint8_t  file_data[MD_USER_DATA_SIZE];
+} metadata_decrypted_header_t;
+
+typedef uint8_t metadata_encrypted_blob_t[sizeof(metadata_decrypted_header_t)];
+
+typedef uint8_t metadata_padding_t[PF_NODE_SIZE -
+                                   (sizeof(metadata_plaintext_header_t) +
+                                    sizeof(metadata_encrypted_blob_t))];
+
+typedef struct _metadata_node {
+    metadata_plaintext_header_t plaintext_header;
+    metadata_encrypted_blob_t   encrypted_blob;
+    metadata_padding_t          padding;
+} metadata_node_t;
+static_assert(sizeof(metadata_node_t) == PF_NODE_SIZE, "sizeof(metadata_node_t)");
 
 typedef struct _mht_node {
     gcm_crypto_data_t data_nodes_crypto[ATTACHED_DATA_NODES_COUNT];
     gcm_crypto_data_t mht_nodes_crypto[CHILD_MHT_NODES_COUNT];
 } mht_node_t;
-
 static_assert(sizeof(mht_node_t) == PF_NODE_SIZE, "sizeof(mht_node_t)");
 
 typedef struct _data_node {
-    uint8_t data[PF_NODE_SIZE];
+    uint8_t bytes[PF_NODE_SIZE];
 } data_node_t;
-
 static_assert(sizeof(data_node_t) == PF_NODE_SIZE, "sizeof(data_node_t)");
 
 typedef struct _encrypted_node {
-    uint8_t cipher[PF_NODE_SIZE];
+    uint8_t bytes[PF_NODE_SIZE];
 } encrypted_node_t;
-
 static_assert(sizeof(encrypted_node_t) == PF_NODE_SIZE, "sizeof(encrypted_node_t)");
 
-#define MAX_PAGES_IN_CACHE 48
+static_assert(sizeof(mht_node_t) == sizeof(data_node_t), "sizes of MHT and data nodes differ");
 
-typedef enum {
-    FILE_MHT_NODE_TYPE  = 1,
-    FILE_DATA_NODE_TYPE = 2,
-} mht_node_type_e;
-
-// make sure these are the same size
-static_assert(sizeof(mht_node_t) == sizeof(data_node_t),
-              "sizeof(mht_node_t) == sizeof(data_node_t)");
-
-DEFINE_LIST(_file_node);
+// Data struct that wraps the 4KB encrypted-node buffer (bounce buffer) and the corresponding 4KB
+// decrypted-data buffer (plain buffer), plus additional fields. This data struct is used for both
+// Data and MHT nodes (but not for Metadata node).
 typedef struct _file_node {
-    LIST_TYPE(_file_node) list;
     uint8_t type;
-    uint64_t node_number;
-    struct _file_node* parent;
     bool need_writing;
-    struct {
-        uint64_t physical_node_number;
-        encrypted_node_t encrypted; // the actual data from the disk
-    };
-    union { // decrypted data
+    struct _file_node* parent;
+
+    uint64_t logical_node_number;
+    uint64_t physical_node_number;
+
+    encrypted_node_t encrypted; // encrypted data from storage (bounce buffer)
+    union {                     // decrypted data in private memory (plain buffer)
         mht_node_t mht;
         data_node_t data;
     } decrypted;
 } file_node_t;
-DEFINE_LISTP(_file_node);
 
 typedef struct {
-    uint32_t index;
+    uint32_t index;             // always "1"; FIXME: what's the point of this?
     char label[MAX_LABEL_SIZE]; // must be NULL terminated
-    pf_keyid_t nonce;
-    uint32_t output_len; // in bits
+    pf_salt_t key_salt;         // salt for key derivation from KDK, stored in metadata node
+    uint32_t output_len;        // in bits; always 128; FIXME: what's the point of this?
 } kdf_input_t;
 
 #pragma pack(pop)
