bd8006eb8efc9531dc88c575c4511c5ec2355573
g2flyer <michael.steiner@intel.com>
2024-06-18 15:59:42 -0700

    fixup! [LibOS] Test-cases for SPLRB (2)
    Signed-off-by: g2flyer <michael.steiner@intel.com>

--- libos/test/fs/gdb_helper.py
+++ libos/test/fs/gdb_helper.py
@@ -1,6 +1,5 @@
 import gdb
 import re
-import shutil
 
 def adversary_do(cmd):
     # extract interesting info from context
@@ -9,10 +8,18 @@ def adversary_do(cmd):
     internal_path=gdb.selected_frame().read_var('path').string()
     external_path=re.sub(r'/tmp/enc_input/', './tmp/enc_input/', internal_path)
     external_path_saved=external_path+"._saved_"
+    try:
+        internal_path2=gdb.selected_frame().read_var('path2').string()
+        external_path2=re.sub(r'/tmp/enc_input/', './tmp/enc_input/', internal_path2)
+        opt_arg=f",{internal_path2}"
+    except ValueError:
+        internal_path2=""
+        external_path2=""
+        opt_arg=""
 
     # execute and report result for pytest digestion
     try:
-        cmd(external_path, external_path_saved)
-        print(f"OK: {test_function} in {operation}({internal_path})")
+        cmd(external_path, external_path_saved, external_path2)
+        print(f"OK: {test_function} in {operation}({internal_path}{opt_arg}])")
     except:
-        print(f"FAIL: {test_function} in {operation}({internal_path})")
+        print(f"FAIL: {test_function} in {operation}({internal_path}{opt_arg})")
