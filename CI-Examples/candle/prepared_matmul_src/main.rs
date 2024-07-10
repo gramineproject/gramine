18d0dbb83622001006afa1d5fbf893298f19b7cb
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2024-07-10 07:57:17 -0700

    [CI-Examples] Add Candle ML framework example
    Candle is a minimalist ML framework for Rust with a focus on performance
    and ease of use. This commit adds two examples with Candle: simple
    matrix multiplication (to quickly test functionality) and Quantized
    LLaMA (to test performance).
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

---
+++ CI-Examples/candle/prepared_matmul_src/main.rs
@@ -0,0 +1,14 @@
+// example taken from https://github.com/huggingface/candle/tree/0.6.0?tab=readme-ov-file#get-started
+
+use candle_core::{Device, Tensor};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let device = Device::Cpu;
+
+    let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
+    let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
+
+    let c = a.matmul(&b)?;
+    println!("{c}");
+    Ok(())
+}
