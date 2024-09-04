82387163f3dd16c117aea43dc818a7dfa44f0939
adarshan-intel <adarsh.anand@intel.com>
2024-09-04 12:13:26 +0530

    fixup! Add log_trace for raw syscalls
    Signed-off-by: adarshan-intel <adarsh.anand@intel.com>

--- pal/src/host/linux-sgx/pal_exception.c
+++ pal/src/host/linux-sgx/pal_exception.c
@@ -231,7 +231,7 @@ static bool handle_ud(sgx_cpu_context_t* uc, int* out_event_num) {
         }
         char buf[LOCATION_BUF_SIZE];
         pal_describe_location(uc->rip, buf, sizeof(buf));
-        log_trace("Emulating raw syscall instruction with number %lu and address is %s", uc->rax,
+        log_trace("Emulating raw syscall instruction with number %lu at address %s", uc->rax,
                   buf);
         return false;
     } else if (is_in_out(instr) && !has_lock_prefix(instr)) {
