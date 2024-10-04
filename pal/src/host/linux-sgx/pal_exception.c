b239fbb5258b55db61eff5094db0262c949bb6e0
Adarsh Anand <adarsh.anand@intel.com>
2024-10-04 15:53:06 +0530

    Removing buffer and describe location functions
    Signed-off-by: Adarsh Anand <adarsh.anand@intel.com>

--- pal/src/host/linux-sgx/pal_exception.c
+++ pal/src/host/linux-sgx/pal_exception.c
@@ -232,9 +232,8 @@ static bool handle_ud(sgx_cpu_context_t* uc, int* out_event_num) {
             log_always("Emulating a raw syscall instruction. This degrades performance, consider"
                        " patching your application to use Gramine syscall API.");
         }
-        char buf[LOCATION_BUF_SIZE];
-        pal_describe_location(uc->rip, buf, sizeof(buf));
-        log_trace("Emulating raw syscall instruction with number %lu at address %s", uc->rax, buf);
+        log_trace("Emulating raw syscall instruction with number %lu at address %p",
+                  uc->rax, (void*)uc->rip);
         return false;
     } else if (is_in_out(instr) && !has_lock_prefix(instr)) {
         /*
