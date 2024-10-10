3968717c86ef69089a9c58e29bb3206f9ce180c2
Kailun Qin <kailun.qin@intel.com>
2024-10-10 00:29:23 -0400

    [PAL/Linux] Log a message on `mmap()` returning `-ENOMEM`
    Linux can return `-ENOMEM` if a request to `mmap()` exceeds the
    process's maximum number of mappings. After commit c0a2765
    "[LibOS,PAL/Linux-SGX] Add EDMM lazy allocation support", this can occur
    during the checkpoint/restore on the Linux PAL. In such case, each
    committed page is sent separately and Linux creates a mapping for each
    one, which can result in a large number of mappings.
    
    This commit makes this failure more visible and provides a hint to the
    user on what can be done on the host.
    
    Signed-off-by: Kailun Qin <kailun.qin@intel.com>

--- pal/src/host/linux/pal_memory.c
+++ pal/src/host/linux/pal_memory.c
@@ -38,7 +38,14 @@ int _PalVirtualMemoryAlloc(void* addr, size_t size, pal_prot_flags_t prot) {
     void* res_addr = (void*)DO_SYSCALL(mmap, addr, size, linux_prot, flags, -1, 0);
 
     if (IS_PTR_ERR(res_addr)) {
-        return unix_to_pal_error(PTR_TO_ERR(res_addr));
+        int ret = PTR_TO_ERR(res_addr);
+        if (ret == -ENOMEM && FIRST_TIME()) {
+            log_error("Host Linux returned -ENOMEM on mmap(); this may happen because process's "
+                      "maximum number of mappings is exceeded. Gramine cannot handle this case. "
+                      "You may want to increase the value in /proc/sys/vm/max_map_count.");
+        }
+
+        return unix_to_pal_error(ret);
     }
 
     assert(res_addr == addr);
