gramineproject/gramine

commit c9dca72ffe888e7e0a936bc15b2b2e448db075eb

[Docs] quickstart: Add specific checkout to stable versions
Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

Documentation/conf.py
@@ -78,9 +78,28 @@ pygments_style = None
 highlight_language = 'c'
 primary_domain = 'c'
 
-rst_prolog = '''
+rst_stable_checkout = '\\'
+if 'READTHEDOCS' in os.environ:
+    rtd_current_version = os.environ['READTHEDOCS_VERSION']
+
+    if rtd_current_version.startswith('v'):
+        rst_stable_checkout = rtd_current_version
+
+    elif rtd_current_version == 'stable':
+        # we don't have a version, we may be able to check from git
+        try:
+            tags = [tag for tag in subprocess.check_output(
+                    ['git', 'tag', '--points-at']).decode().strip().split()
+                if tag.startswith('v')]
+            rst_stable_checkout = tags.pop()
+        except (subprocess.CalledProcessError, IndexError):
+            pass
+
+rst_prolog = f'''
 .. |~| unicode:: 0xa0
    :trim:
+
+.. |stable-checkout| replace:: {rst_stable_checkout}
 '''
 
 breathe_projects = {p: '_build/doxygen-{}/xml'.format(p)

commit c3a23a1fb3534f192600e925d041817191368351

[Docs] Fix `--branch` magic to not collide variable with sphinx tags
A mistake was made in commit c9dca72ffe888e7e0a936bc15b2b2e448db075eb
("[Docs] quickstart: Add specific checkout to stable versions") that the
variable name (`tags`) was chosen to collide with Sphinx' special
configuration variable:
https://www.sphinx-doc.org/en/master/usage/configuration.html#project-tags
This went unnoticed, because at the time we were not using project tags.
Later, in commit 6ebc605de8c07a9227afab12e227219781404c16 ("[Docs]
Update sphinx to 5.3"), we started using this feature. The mistake was
still unnoticed, because the stable checkout was under condition that is
true only rarely.

Fixes: c9dca72ffe88 ("[Docs] quickstart: Add specific checkout to stable versions")
Fixes: 6ebc605de8c0 ("[Docs] Update sphinx to 5.3")
Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

Documentation/conf.py
@@ -89,10 +89,10 @@ if 'READTHEDOCS' in os.environ:
     elif rtd_current_version == 'stable':
         # we don't have a version, we may be able to check from git
         try:
-            tags = [tag for tag in subprocess.check_output(
+            git_tags = [tag for tag in subprocess.check_output(
                     ['git', 'tag', '--points-at']).decode().strip().split()
                 if tag.startswith('v')]
-            rst_stable_checkout = f'--branch {tags.pop()}'
+            rst_stable_checkout = f'--branch {git_tags.pop()}'
         except (subprocess.CalledProcessError, IndexError):
             pass
 